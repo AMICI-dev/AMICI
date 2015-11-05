c5c17d78c48c28c7d123faf72697b61babd436c1
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2015-11-05 19:04:52 +0100

    updated documentation

--- mtoc/makeDocumentation.m
+++ mtoc/makeDocumentation.m
@@ -1,5 +1,5 @@
 setenv('PATH', [getenv('PATH') ':/Library/TeX/texbin/'])
 setenv('PATH', [getenv('PATH') ':/usr/local/bin'])
 MatlabDocMaker.create('latex',true)
-
+[wrap_path,~,~] = fileparts(which('amiwrap.m'));
 movefile(fullfile(wrap_path,'doc','latex','refman.pdf'),fullfile(wrap_path,'AMICI_guide.pdf'))
