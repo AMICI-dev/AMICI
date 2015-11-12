88941ed56ddc9c5eb59dd16ef7ab1ddae0a3a82a
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2015-11-12 15:03:20 +0100

    fix installation file

---
+++ installAMICI.m
@@ -0,0 +1,5 @@
+amipath = fileparts('installAMICI.m');
+addpath(amipath)
+addpath(fullfile(amipath,'auxiliary'))
+addpath(fullfile(amipath,'auxiliary','datahash'))
+addpath(fullfile(amipath,'symbolic'))
