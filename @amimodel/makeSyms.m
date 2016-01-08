6766496581f7cd15b809bcab6928198ca7b7a7c2
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-01-08 13:04:36 +0100

    transform all input into symbolic variables

--- @amimodel/makeSyms.m
+++ @amimodel/makeSyms.m
@@ -8,8 +8,12 @@ function [ this ] = makeSyms( this )
     % check whether sym is properly defined
     if(~isfield(this.sym,'xdot'))
         if(isfield(this.sym,'f'))
-            this.sym.xdot = this.sym.f(:);
-            this.sym = rmfield(this.sym,'f');
+            try
+                this.sym.xdot = sym(this.sym.f(:));
+                this.sym = rmfield(this.sym,'f');
+            catch
+                error('Could not transform model.sym.f into a symbolic variable, please check the definition!')
+            end
         else
             error('field xdot/f is missing in model definition')
         end
@@ -19,30 +23,50 @@ function [ this ] = makeSyms( this )
                 error('Model this contains conflicting definitions sym.f and sym.xdot of DE right hand side');
             end
         else
-            this.sym.xdot = this.sym.xdot(:);
+            try
+                this.sym.xdot = sym(this.sym.xdot(:));
+            catch
+                error('Could not transform model.sym.xdot into a symbolic variable, please check the definition!')
+            end
         end
     end
     
     if(~isfield(this.sym,'x'))
         error('Model this is missing the definition of the state vector x (.sym.x)!')
     else
-        this.sym.x = this.sym.x(:);
+        try
+            this.sym.x = sym(this.sym.x(:));
+        catch
+            error('Could not transform model.sym.x into a symbolic variable, please check the definition!')
+        end
     end
     
     if(~isfield(this.sym,'p'))
         error('Model this is missing the definition of the parameter vector p (.sym.p)!')
     else
-        this.sym.p = this.sym.p(:);
+        try
+            this.sym.p = sym(this.sym.p(:));
+        catch
+            error('Could not transform model.sym.y into a symbolic variable, please check the definition!')
+        end
     end
     if(~isfield(this.sym,'x0'))
         error('Model this is missing the definition of the vector of initial conditions x0 (.sym.x0)!')
     else
-        this.sym.x0 = this.sym.x0(:);
+        try
+            this.sym.x0 = sym(this.sym.x0(:));
+        catch
+            error('Could not transform model.sym.x0 into a symbolic variable, please check the definition!')
+        end
     end
     if(~isfield(this.sym,'y'))
         error('Model this is missing the definition of the vector of observables y (.sym.y)!')
     else
-        this.sym.y = this.sym.y(:);
+        try
+        this.sym.y = sym(this.sym.y(:));
+                catch
+            error('Could not transform model.sym.y into a symbolic variable, please check the definition!')
+        end
     end
     if(~all([size(this.sym.x,2)==size(this.sym.xdot,2),size(this.sym.xdot,2)==size(this.sym.x0,2)]))
         error('Sizes of x0, xdot and x do not agree!')
@@ -50,20 +74,20 @@ function [ this ] = makeSyms( this )
     
     % complete optional fields
     if(~isfield(this.sym,'u'))
-        this.sym.u = sym.empty(0,0);
+        this.sym.u = sym(zeros(0,0));
     end
     if(~isfield(this.sym,'k'))
-        this.sym.k = sym.empty(0,0);
+        this.sym.k = sym(zeros(0,0));
     end
         
     if(isfield(this.sym,'root'))
         error('The definition of events via a root function is deprecated and no longer supported. Please update the model definition syntax!')
     end
     if(~isfield(this.sym,'sigma_y'))
-        this.sym.sigma_y = sym.ones(size(this.sym.y));
+        this.sym.sigma_y = sym(ones(size(this.sym.y)));
     end
     if(numel(this.sym.sigma_y) == 1)
-        this.sym.sigma_y = this.sym.sigma_y*sym.ones(size(this.sym.y));
+        this.sym.sigma_y = this.sym.sigma_y*sym(ones(size(this.sym.y)));
     end
     
     if(any(ismember(this.sym.k,this.sym.p)))
