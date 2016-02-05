ea038fd85864c742aae40e7b3ba4f1f1e4814e9d
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-02-05 13:50:37 +0100

    fixes amidata class

--- @amidata/amidata.m
+++ @amidata/amidata.m
@@ -2,17 +2,17 @@ classdef amidata < handle
     %AMIDATA provides a data container to pass experimental data do the
     %simulation routine for likelihood computation. the 
     
-    properties (Dependent) 
+    properties
         % number of timepoints
-        nt=@double;
+        nt=0;
         % number of observables
-        ny=@double;
+        ny=0;
         % number of event observables
-        nz=@double;
+        nz=0;
         % number of events
-        ne=@double;
+        ne=0;
         % number of conditions/constants
-        nk=@double;
+        nk=0;
         % timepoints of observations
         t@double;
         % observations
@@ -36,7 +36,7 @@ classdef amidata < handle
         
         function set.nt(this,nt)
             this.nt = nt;
-            this.t = NaN(nt,1);
+            this.t = 1:nt;
             this.Y = NaN;
             this.Sigma_Y = NaN;
         end
@@ -56,71 +56,72 @@ classdef amidata < handle
         function set.t(this,value)
             assert(isnumeric(value),'AMICI:amimodel:t:numeric','t must have a numeric value!')
             assert(ismatrix(value),'AMICI:amimodel:t:ndims','t must be a two dimensional matrix!')
-            assert(numel(values)==this.nt,'AMICI:amimodel:t:ndims',['t must have ' num2str(this.nt) ' elements!'])
+            assert(numel(value)==this.nt,'AMICI:amimodel:t:ndims',['t must have ' num2str(this.nt) ' (D.nt) elements!'])
             this.t = double(value(:));
         end
         
         function set.Y(this,value)
-            assert(isnumeric(value),'AMICI:amimodel:Y:numeric','Y must have a numeric value!')
             assert(ismatrix(value),'AMICI:amimodel:Y:ndims','Y must be a two dimensional matrix!')
-            if(size(all(size(value)==[this.nt this.ny])))
+            assert(all(all(or(isnumeric(value),isnan(value)))),'AMICI:amimodel:Y:numeric','Y must have a numeric value!')
+            
+            if(all(size(value)==[this.nt this.ny]))
                 this.Y = double(value);
-            elseif(size(all(size(value)==[this.nt 1])))
+            elseif(all(size(value)==[this.nt 1]))
                 this.Y = repmat(double(value),[1,this.ny]);
-            elseif(size(all(size(value)==[1 this.ny])))
+            elseif(all(size(value)==[1 this.ny]))
                 this.Y = repmat(double(value),[this.nt,1]);
-            elseif(size(all(size(value)==[1 1])))
+            elseif(all(size(value)==[1 1]))
                 this.Y = repmat(double(value),[this.nt,this.ny]);
             else
-                error('AMICI:amimodel:Y:size',['Y must have size [' num2str(this.nt) ',' num2str(this.ny) '] !'])
+                error('AMICI:amimodel:Y:size',['Y must have size [' num2str(this.nt) ',' num2str(this.ny) '] ([D.nt,D.ny])!'])
             end
         end
         
         function set.Sigma_Y(this,value)
-            assert(isnumeric(value),'AMICI:amimodel:Sigma_Y:numeric','Sigma_Y must have a numeric value!')
             assert(ismatrix(value),'AMICI:amimodel:Sigma_Y:ndims','Sigma_Y must be a two dimensional matrix!')
-            if(size(all(size(value)==[this.nt this.ny])))
+            assert(all(all(or(isnumeric(value),isnan(value)))),'AMICI:amimodel:Sigma_Y:numeric','Sigma_Y must have a numeric value!')
+            if(all(size(value)==[this.nt this.ny]))
                 this.Sigma_Y = double(value);
-            elseif(size(all(size(value)==[this.nt 1])))
+            elseif(all(size(value)==[this.nt 1]))
                 this.Sigma_Y = repmat(double(value),[1,this.ny]);
-            elseif(size(all(size(value)==[1 this.ny])))
+            elseif(all(size(value)==[1 this.ny]))
                 this.Sigma_Y = repmat(double(value),[this.nt,1]);
-            elseif(size(all(size(value)==[1 1])))
+            elseif(all(size(value)==[1 1]))
                 this.Sigma_Y = repmat(double(value),[this.nt,this.ny]);
             else
-                error('AMICI:amimodel:Sigma_Y:size',['Sigma_Y must have size [' num2str(this.nt) ',' num2str(this.ny) '] !'])
+                error('AMICI:amimodel:Sigma_Y:size',['Sigma_Y must have size [' num2str(this.nt) ',' num2str(this.ny) '] ([D.nt,D.ny])!'])
             end
         end
         
         function set.Z(this,value)
-            assert(isnumeric(value),'AMICI:amimodel:Z:numeric','Z must have a numeric value!')
             assert(ismatrix(value),'AMICI:amimodel:Z:ndims','Z must be a two dimensional matrix!')
-            if(size(all(size(value)==[this.ne this.nz])))
+            assert(all(all(or(isnumeric(value),isnan(value)))),'AMICI:amimodel:Z:numeric','Z must have a numeric value!')
+            if(all(size(value)==[this.ne this.nz]))
                 this.Z = double(value);
-            elseif(size(all(size(value)==[this.ne 1])))
+            elseif(all(size(value)==[this.ne 1]))
                 this.Z = repmat(double(value),[1,this.nz]);
-            elseif(size(all(size(value)==[1 this.nz])))
+            elseif(all(size(value)==[1 this.nz]))
                 this.Z = repmat(double(value),[this.ne,1]);
-            elseif(size(all(size(value)==[1 1])))
+            elseif(all(size(value)==[1 1]))
                 this.Z = repmat(double(value),[this.ne,this.nz]);
             else
-                error('AMICI:amimodel:Z:size',['Z must have size [' num2str(this.ne) ',' num2str(this.nz) '] !'])
+                error('AMICI:amimodel:Z:size',['Z must have size [' num2str(this.ne) ',' num2str(this.nz) '] ([D.ne,D.nz])!'])
             end
         end
         
         function set.Sigma_Z(this,value)
-            assert(isnumeric(value),'AMICI:amimodel:Sigma_Z:numeric','Sigma_Z must have a numeric value!')
             assert(ismatrix(value),'AMICI:amimodel:Sigma_Z:ndims','Sigma_Z must be a two dimensional matrix!')
-            if(size(all(size(value)==[this.ne this.nz])))
+            assert(all(all(or(isnumeric(value),isnan(value)))),'AMICI:amimodel:Sigma_Z:numeric','Sigma_Z must have a numeric value!')
+            if(all(size(value)==[this.ne this.nz]))
                 this.Sigma_Z = double(value);
-            elseif(size(all(size(value)==[this.ne 1])))
+            elseif(all(size(value)==[this.ne 1]))
                 this.Sigma_Z = repmat(double(value),[1,this.nz]);
-            elseif(size(all(size(value)==[1 this.nz])))
+            elseif(all(size(value)==[1 this.nz]))
                 this.Sigma_Z = repmat(double(value),[this.ne,1]);
-            elseif(size(all(size(value)==[1 1])))
+            elseif(all(size(value)==[1 1]))
                 this.Sigma_Z = repmat(double(value),[this.ne,this.nz]);
             else
-                error('AMICI:amimodel:Sigma_Z:size',['Sigma_Z must have size [' num2str(this.ne) ',' num2str(this.nz) '] !'])
+                error('AMICI:amimodel:Sigma_Z:size',['Sigma_Z must have size [' num2str(this.ne) ',' num2str(this.nz) '] ([D.ne,D.nz])!'])
             end
         end
     end
