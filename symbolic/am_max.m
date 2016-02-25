b9493c321701bb54d1a9cabbbd2aa306ca4ae114
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-02-25 14:42:53 +0100

    fix min/man

--- symbolic/am_max.m
+++ symbolic/am_max.m
@@ -2,6 +2,6 @@ function fun = am_max(a, b)
 % syms x y
 % f = symfun(sym('am_max(x,y)'),[x y]);
 % fun = f(a,b);
-fun = sym(['am_max(' char(a) ',' char(b) ')'])
+fun = sym(['am_max(' char(vpa(a)) ',' char(vpa(b)) ')']);
 % fun = b + heaviside(a-b)*(a-b);
 end
