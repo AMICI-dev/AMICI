0ae52c0dc2c0dc09c22427bf748fcffaccc766f6
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-08-08 14:29:53 +0200

    fix min and max

--- symbolic/am_min.m
+++ symbolic/am_min.m
@@ -2,6 +2,6 @@ function fun = am_min(a, b)
 % syms x y
 % f = symfun(sym('am_min(x,y)'),[x y]);
 % fun = f(a,b);
-% fun = sym(['am_min(' char(vpa(a)) ',' char(vpa(b)) ')']);
-fun = b + heaviside(b-a)*(a-b);
+fun = sym(['am_min(' char(vpa(a)) ',' char(vpa(b)) ',0.0)']);
+% fun = b + heaviside(b-a)*(a-b);
 end
