97db7d16122e03f970a0c501373a22d6fd5381ef
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2016-08-10 12:51:20 +0200

    added some more outputs for the case of second order adjoints

--- @amimodel/generateM.m
+++ @amimodel/generateM.m
@@ -366,15 +366,21 @@ if(o2flag)
     fprintf(fid,'        sol.sllh = sol.sllh.*theta(options_ami.sens_ind);\n');
     switch(o2flag)
         case 1
-            fprintf(fid,'        sol.s2llh = sol.s2llh.*(theta(options_ami.sens_ind)*transpose(theta(options_ami.sens_ind))) + diag(sol.sllh);\n');
+            fprintf(fid, '        sol.s2llh = sol.s2llh.*(theta(options_ami.sens_ind)*transpose(theta(options_ami.sens_ind))) + diag(sol.sllh);\n');
             fprintf(fid,['        sol.sx = permute(reshape(transpose(sol.x(:,', num2str(nxtrue), '+1:end)), ', num2str(nxtrue), ', ', num2str(np), ', data.nt), [3,1,2]);\n']);
+            fprintf(fid, '        sol.sx = bsxfun(@times,sol.sx,permute(theta(options_ami.sens_ind),[3,2,1]));\n');
             fprintf(fid,['        sol.sy = permute(reshape(transpose(sol.y(:,', num2str(nytrue), '+1:end)), ', num2str(nytrue), ', ', num2str(np), ', data.nt), [3,1,2]);\n']);
+            fprintf(fid, '        sol.sy = bsxfun(@times,sol.sy,permute(theta(options_ami.sens_ind),[3,2,1]));\n');
+            fprintf(fid,['        sol.sz = permute(reshape(transpose(sol.z(:,', num2str(nztrue), '+1:end)), ', num2str(nztrue), ', ', num2str(np), ', data.ne), [3,1,2]);\n']);
+            fprintf(fid, '        sol.sz = bsxfun(@times,sol.sz,permute(theta(options_ami.sens_ind),[3,2,1]));\n');
+            fprintf(fid, '        sol.ssigmay = bsxfun(@times,sol.ssigmay,permute(theta(options_ami.sens_ind),[3,2,1]));\n');
+            fprintf(fid, '        sol.ssigmaz = bsxfun(@times,sol.ssigmaz,permute(theta(options_ami.sens_ind),[3,2,1]));\n');
         case 2
             fprintf(fid,'        sol.s2llh = sol.s2llh.*theta(options_ami.sens_ind) + (sol.sllh).^2;\n');
     end
     fprintf(fid,['        sol.x = sol.x(:,1:' num2str(nxtrue) ');\n']);
     fprintf(fid,['        sol.y = sol.y(:,1:' num2str(nytrue) ');\n']);
-%     fprintf(fid,['        sol.z = sol.y(:,1:' num2str(nztrue) ');\n']);
+    fprintf(fid,['        sol.z = sol.z(:,1:' num2str(nztrue) ');\n']);
     fprintf(fid, '    else\n');
     fprintf(fid,['        sx = sol.sx(:,1:' num2str(nxtrue) ',:);\n']);
     fprintf(fid,['        sy = sol.sy(:,1:' num2str(nytrue) ',:);\n']);
