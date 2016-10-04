c3ff10ae50026bc29c848f7ad6a104972ff3147f
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-10-04 17:22:11 +0200

    first try to get the cmake running

--- scripts/run-tests.ps1
+++ scripts/run-tests.ps1
@@ -28,22 +28,21 @@ mingw32-make library -e CC="gcc"
 mkdir .\..\..\sundials\build\
 cd .\..\..\sundials\build\
 
-cmake -DCMAKE_INSTALL_PREFIX=".\..\..\build\sundials" \
--DBUILD_ARKODE=OFF \
--DBUILD_CVODE=OFF \
--DBUILD_IDA=OFF \
--DBUILD_KINSOL=OFF \
--DBUILD_SHARED_LIBS=ON \
--DBUILD_STATIC_LIBS=OFF \
--DEXAMPLES_ENABLE=OFF \
--DEXAMPLES_INSTALL=OFF \
--DKLU_ENABLE=ON \
--DKLU_LIBRARY_DIR="$.\..\..\SuiteSparse\lib" \
--DKLU_INCLUDE_DIR="$.\..\..\SuiteSparse\include" \
-.\.. 
+cmake .. -DCMAKE_INSTALL_PREFIX=".\..\..\build\sundials" `
+-DBUILD_ARKODE=OFF `
+-DBUILD_CVODE=OFF `
+-DBUILD_IDA=OFF `
+-DBUILD_KINSOL=OFF `
+-DBUILD_SHARED_LIBS=ON `
+-DBUILD_STATIC_LIBS=OFF `
+-DEXAMPLES_ENABLE=OFF `
+-DEXAMPLES_INSTALL=OFF `
+-DKLU_ENABLE=ON `
+-DKLU_LIBRARY_DIR="$.\..\..\SuiteSparse\lib" `
+-DKLU_INCLUDE_DIR="$.\..\..\SuiteSparse\include" `
 
 mingw32-make -e CC="gcc" LDFLAGS="$LDFLAGS -shared" 
-make install
+mingw32-make install
 
 cd ..\..\
 
