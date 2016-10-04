73d0b358afb27180b2e5f9c24138503f45993db3
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-10-04 18:23:08 +0200

    lets just got with absolute paths ...

--- scripts/run-tests.ps1
+++ scripts/run-tests.ps1
@@ -28,7 +28,7 @@ mingw32-make library -e CC="gcc"
 mkdir .\..\..\sundials\build\
 cd .\..\..\sundials\build\
 
-cmake .. -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\build\sundials" `
+cmake .. -DCMAKE_INSTALL_PREFIX="C:/projects/amici/build/sundials" `
 -DBUILD_ARKODE=OFF `
 -DBUILD_CVODE=OFF `
 -DBUILD_IDA=OFF `
@@ -38,8 +38,8 @@ cmake .. -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\build\sundials" `
 -DEXAMPLES_ENABLE=OFF `
 -DEXAMPLES_INSTALL=OFF `
 -DKLU_ENABLE=ON `
--DKLU_LIBRARY_DIR="$PSScriptRoot\..\SuiteSparse\lib" `
--DKLU_INCLUDE_DIR="$PSScriptRoot\..\SuiteSparse\include" `
+-DKLU_LIBRARY_DIR="C:/projects/amici/SuiteSparse/lib" `
+-DKLU_INCLUDE_DIR="C:/projects/amici/SuiteSparse/include" `
 
 msbuild ALL BUILD.vcxproj
 msbuild INSTALL.vcxproj
