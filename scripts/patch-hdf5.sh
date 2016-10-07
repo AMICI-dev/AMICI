74f31bef5b700e6d6c83bc31faaef78931fc0cd7
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-10-07 23:51:12 +0200

    where the dlls at?

--- scripts/patch-hdf5.sh
+++ scripts/patch-hdf5.sh
@@ -5,4 +5,4 @@ for dll in 'ls dll/*dll'; do
       pexports $dll > $def_file
       dlltool -d $def_file -l lib/$lib_file
 done 
-patch -p1 -d C:/Program Files (x86)/HDF_Group\HDF5\1.8.17\include < C:/projects/amici/scripts/hdf5-1.8.7-mingw.patch
+patch -p1 -d 'C:/Program Files (x86)/HDF_Group/HDF5/1.8.17/include' < C:/projects/amici/scripts/hdf5-1.8.7-mingw.patch
