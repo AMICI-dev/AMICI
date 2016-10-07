56626556fec7c3b491f632bf2f8986bb4db3c3df
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-10-08 01:35:31 +0200

    fix patch bash script

--- scripts/patch-hdf5.sh
+++ scripts/patch-hdf5.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 cd 'C:/Program Files (x86)/HDF_Group/HDF5/1.8.17/bin/'
 for dll in *.dll; do
-	  echo "$dll"
 	  base_def=$(basename $dll .dll)
-	  echo "$base_def"
       def_file="${base_def}.def"
       base_lib=$(basename $dll dll.dll)
-      echo "$base_lib"
       lib_file="lib${base_lib}.a"
       pexports $dll > $def_file
-      dlltool -d $def_file -l lib/$lib_file
+      dlltool -d $def_file -l $lib_file
 done 
 patch -p1 -d 'C:/Program Files (x86)/HDF_Group/HDF5/1.8.17/' < C:/projects/amici/scripts/hdf5-1.8.7-mingw.patch
