337dcdd114fc9dd448ad6ef0a26ec89ae18d7ac5
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-10-08 01:11:21 +0200

    now I know where the dlls are, apply the patch

--- scripts/patch-hdf5.sh
+++ scripts/patch-hdf5.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#for dll in 'ls dll/*dll'; do
-#      def_file='basename $dll .dll'.def
-#      lib_file=lib'basename $dll dll.dll'.a
-#      pexports $dll > $def_file
-#      dlltool -d $def_file -l lib/$lib_file
-#done 
+cd C:/Program Files (x86)/HDF_Group/HDF5/1.8.17/bin/
+for dll in 'ls dll/*dll'; do
+      def_file='basename $dll .dll'.def
+      lib_file=lib'basename $dll dll.dll'.a
+      pexports $dll > $def_file
+      dlltool -d $def_file -l lib/$lib_file
+done 
 patch -p1 -d 'C:/Program Files (x86)/HDF_Group/HDF5/1.8.17/' < C:/projects/amici/scripts/hdf5-1.8.7-mingw.patch
