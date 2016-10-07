68e0835dce6b9b6c33ce9c001a5794277463cbfd
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2016-10-07 23:58:09 +0200

    there actually are no dll files, just lib, maybe skip this and try to get the .h patch working

--- scripts/patch-hdf5.sh
+++ scripts/patch-hdf5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for dll in 'ls dll/*dll'; do
-      def_file='basename $dll .dll'.def
-      lib_file=lib'basename $dll dll.dll'.a
-      pexports $dll > $def_file
-      dlltool -d $def_file -l lib/$lib_file
-done 
-patch -p1 -d 'C:/Program Files (x86)/HDF_Group/HDF5/1.8.17/include' < C:/projects/amici/scripts/hdf5-1.8.7-mingw.patch
+#for dll in 'ls dll/*dll'; do
+#      def_file='basename $dll .dll'.def
+#      lib_file=lib'basename $dll dll.dll'.a
+#      pexports $dll > $def_file
+#      dlltool -d $def_file -l lib/$lib_file
+#done 
+patch -p1 -d 'C:/Program Files (x86)/HDF_Group/HDF5/1.8.17/' < C:/projects/amici/scripts/hdf5-1.8.7-mingw.patch
