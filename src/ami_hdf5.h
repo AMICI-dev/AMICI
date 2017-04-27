4621c3a8dcf234d0fe91f0daa26985b19b540dde
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-04-27 11:25:43 +0200

    Clean up and documentation of stand-alone code and HDF5 I/O

--- src/ami_hdf5.h
+++ src/ami_hdf5.h
@@ -13,17 +13,46 @@
 #define EXTERNC
 #endif
 
-EXTERNC void storeSimulation(const char* fileName, ReturnData *rdata);
-EXTERNC UserData *readSimulationUserData(const char* fileName);
-EXTERNC ExpData *readSimulationExpData(const char* hdffile, UserData *udata);
-EXTERNC void writeReturnData(const char* hdffile, ReturnData *rdata, UserData *udata);
-EXTERNC void createAndWriteDouble2DAttribute(hid_t dataset, const char *attributeName, const double *buffer, hsize_t m, hsize_t n);
-EXTERNC void createAndWriteDouble3DAttribute(hid_t dataset, const char *attributeName, const double *buffer, hsize_t m, hsize_t n, hsize_t o);
-EXTERNC double getDoubleScalarAttribute(hid_t file_id, const char* optionsObject, const char* attributeName);
-EXTERNC int getIntScalarAttribute(hid_t file_id, const char* optionsObject, const char* attributeName);
-EXTERNC void getDoubleArrayAttribute(hid_t file_id, const char* optionsObject, const char* attributeName, double **destination, hsize_t *length);
-EXTERNC void getIntArrayAttribute(hid_t file_id, const char* optionsObject, const char* attributeName, int **destination, hsize_t *length);
-EXTERNC void getDoubleArrayAttribute2D(hid_t file_id, const char* optionsObject, const char* attributeName, double **destination, hsize_t *m, hsize_t *n);
-EXTERNC void setAttributeIntFromDouble(hid_t file_id, const char *obj_name, const char *attr_name, const double *bufferDouble, size_t size );
+/* Functions for reading and writing AMICI data to/from HDF5 files. */
+
+/**
+  * @brief AMI_HDF5_writeReturnData writes ReturnData struct to attributes of an HDF5 dataset
+  * @param rdata Data to write
+  * @param udata UserData for model from which rdata was obtained.
+  * @param hdffile Filename of HDF5 file
+  * @param datasetPath Full dataset path inside the HDF5 file (will be created)
+  */
+
+EXTERNC void AMI_HDF5_writeReturnData(const ReturnData *rdata, const UserData *udata, const char* hdffile, const char* datasetPath);
+
+/**
+ * @brief AMI_HDF5_readSimulationUserDataFromFileName reads AMICI simulation options from HDF5 file.
+ * @param fileName Name of HDF5 file
+ * @param datasetPath Path inside the HDF5 file
+ * @return
+ */
+EXTERNC UserData *AMI_HDF5_readSimulationUserDataFromFileName(const char* fileName, const char *datasetPath);
+
+EXTERNC UserData *AMI_HDF5_readSimulationUserDataFromFileObject(hid_t fileId, const char *datasetPath);
+
+EXTERNC ExpData *AMI_HDF5_readSimulationExpData(const char* hdffile, UserData *udata);
+
+// Helper functions to reading and writing HDF5 attributes:
+
+EXTERNC herr_t AMI_HDF5_createAndWriteDouble2DAttribute(hid_t dataset, const char *attributeName, const double *buffer, hsize_t m, hsize_t n);
+
+EXTERNC herr_t AMI_HDF5_createAndWriteDouble3DAttribute(hid_t dataset, const char *attributeName, const double *buffer, hsize_t m, hsize_t n, hsize_t o);
+
+EXTERNC double AMI_HDF5_getDoubleScalarAttribute(hid_t file_id, const char* optionsObject, const char* attributeName);
+
+EXTERNC int AMI_HDF5_getIntScalarAttribute(hid_t file_id, const char* optionsObject, const char* attributeName);
+
+EXTERNC void AMI_HDF5_getDoubleArrayAttribute(hid_t file_id, const char* optionsObject, const char* attributeName, double **destination, hsize_t *length);
+
+EXTERNC void AMI_HDF5_getIntArrayAttribute(hid_t file_id, const char* optionsObject, const char* attributeName, int **destination, hsize_t *length);
+
+EXTERNC void AMI_HDF5_getDoubleArrayAttribute2D(hid_t file_id, const char* optionsObject, const char* attributeName, double **destination, hsize_t *m, hsize_t *n);
+
+EXTERNC void AMI_HDF5_setAttributeIntFromDouble(hid_t file_id, const char *obj_name, const char *attr_name, const double *bufferDouble, size_t size );
 
 #endif
--- src/main.template.cpp
+++ src/main.template.cpp
@@ -3,39 +3,64 @@
 #include <string.h>
 #include <assert.h>
 #include <math.h>
-#include "wrapfunctions.h" /* user functions */
-#include "include/symbolic_functions.h"
-#include <include/amici.h> /* amici functions */
-#include <src/ami_hdf5.h>
-#include <include/udata_accessors.h>
-#include <include/rdata_accessors.h>
 
-void processUserData(UserData *udata);
+#include "wrapfunctions.h" /* model specific functions */
+#include <include/amici.h> /* AMICI API */
+#include <src/ami_hdf5.h>  /* AMICI HDF5 I/O functions */
+#include <include/udata_accessors.h> /* Accessor macros for UserData members */
+#include <include/rdata_accessors.h> /* Accessor macros for ReturnData members */
+
+/* This is a scaffold for a stand-alone AMICI simulation executable demonstrating
+ * use of the AMICI C++ API.
+ *
+ * This program reads AMICI options from an HDF5 file, prints some results
+ * and writes additional results to an HDF5 file. The name of the HDF5 file
+ * is expected as single command line argument.
+ *
+ * An initial HDF5 file with the required fields can be generated using MATLAB by adding the following lines
+ * in the simulate_${MODEL_NAME}.m file before the call to .....:
+ *
+ * hdffile = 'mydata.h5'
+ * structToHDF5Attribute(hdffile, '/options', options)
+ * structToHDF5Attribute(hdffile, '/data', data)
+ *
+ * ... and then running a simulation from MATLAB as usual.
+ *
+ */
+
+
+// Function prototypes
 void processReturnData(ReturnData *rdata, UserData *udata);
 void printReturnData(ReturnData *rdata, UserData *udata);
 
+
 int main(int argc, char **argv)
 {  
+    // HDF5 file to read and write data (full path)
     const char *hdffile;
 
+    // Check command line arguments
     if(argc != 2) {
-        fprintf(stderr, "Error: must provide input file as first and only argument.\n");
+        fprintf(stderr, "Error: must provide HDF5 input file as first and only argument.\n");
         return 1;
     } else {
         hdffile = argv[1];
     }
     
-    UserData *udata = readSimulationUserData(hdffile);
+    // Read UserData (AMICI settings and model parameters) from HDF5 file
+    UserData *udata = AMI_HDF5_readSimulationUserDataFromFileName(hdffile, "/options");
     if (udata == NULL) {
         return 1;
     }
 
-    ExpData *edata = readSimulationExpData(hdffile, udata);
+    // Read ExpData (experimental data for model) from HDF5 file
+    ExpData *edata = AMI_HDF5_readSimulationExpData(hdffile, udata);
     if (edata == NULL) {
         freeUserData(udata);
         return 1;
     }
 
+    // Run the simulation
     int status = 0;
     ReturnData *rdata = getSimulationResults(udata, edata, &status);
     if (rdata == NULL) {
@@ -44,9 +69,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // Do something with the simulation results
     processReturnData(rdata, udata);
-    writeReturnData(hdffile, rdata, udata);
 
+    // Save simulation results to HDF5 file
+    AMI_HDF5_writeReturnData(rdata, udata, hdffile, "/solution");
+
+    // Free memory
     freeExpData(edata);
     freeUserData(udata);
     freeReturnData(rdata);
@@ -56,49 +85,51 @@ int main(int argc, char **argv)
 
 
 void processReturnData(ReturnData *rdata, UserData *udata) {
+    // show some the simulation results
     printReturnData(rdata, udata);
 }
 
 void printReturnData(ReturnData *rdata, UserData *udata) {
+    //Print of some the simulation results
 
-    printf("tsdata: ");
+    printf("Timepoints (tsdata): ");
     printArray(tsdata, nt);
 
-
-    printf("\n\nxdata\n");
+    printf("\n\nStates (xdata):\n");
     for(int i = 0; i < nx; ++i) {
         for(int j = 0; j < nt; ++j)
             printf("%e\t", rdata->am_xdata[j +  nt * i]);
         printf("\n");
     }
 
-    printf("\nydata\n");
+    printf("\nObservables (ydata):\n");
     for(int i = 0; i < ny; ++i) {
         for(int j = 0; j < nt; ++j)
             printf("%e\t", rdata->am_ydata[j +  nt * i]);
         printf("\n");
     }
 
-    printf("\n\nxdotdata: ");
+    printf("\n\ndx/dt (xdotdata):\n");
     for(int i = 0; i < nx; ++i)
         printf("%e\t", rdata->am_xdotdata[i]);
 
-
-
-    printf("\njdata\n");
-    for(int i = 0; i < nx; ++i) {
-        for(int j = 0; j < nx; ++j)
-            printf("%e\t", rdata->am_Jdata[i + nx * j]);
-        printf("\n");
-    }
+//    printf("\nJacobian (jdata)\n");
+//    for(int i = 0; i < nx; ++i) {
+//        for(int j = 0; j < nx; ++j)
+//            printf("%e\t", rdata->am_Jdata[i + nx * j]);
+//        printf("\n");
+//    }
 
     printf("\nnumsteps: \t\t");
     printfArray(numstepsdata, nt, "%.0f ");
+
     printf("\nnumrhsevalsdata: \t");
     printfArray(numrhsevalsdata, nt, "%.0f ");
+
     printf("\norder: \t\t");
     printfArray(orderdata, nt, "%.0f ");
+
     printf("\n");
-    printf("llh: %e\n", *llhdata);
+    printf("Loglikelihood (llhdata): %e\n", *llhdata);
 }
 
