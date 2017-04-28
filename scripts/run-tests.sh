d360f06c9a7ecdc0f26419f2261c5eae767fcb1b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-04-28 14:01:37 +0200

    Cpputest dependencies

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -48,6 +48,18 @@ make install
 
 cd ../../
 
+# Cpputest
+mkdir -p ${AMICI_PATH}/ThirdParty
+cd ${AMICI_PATH}/ThirdParty 
+if [ ! -d cpputest-3.8 ]; then
+    if [ ! -e cpputest-3.8.tar.gz ]; then
+        wget https://github.com/cpputest/cpputest/releases/download/v3.8/cpputest-3.8.tar.gz
+    fi
+    tar -xzf cpputest-3.8.tar.gz
+    cd cpputest-3.8/cpputest_build/
+    ../configure && make
+fi
+
 # done building dependencies
 
 # Prepare tests
