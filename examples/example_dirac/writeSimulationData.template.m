6cb08a89c20f5c62b5afd3715dba6e76d8330963
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-04-28 15:46:27 +0200

    Added script for preparation of test data

---
+++ examples/example_dirac/writeSimulationData.template.m
@@ -0,0 +1,18 @@
+if false
+    
+    %% Write data that is passed to AMICI to HDF5
+    global amiHDFfile;
+    global amiHDFprefix;
+    if ~isempty(amiHDFfile)
+        amiHDFOptionPath = [amiHDFprefix '/options'];
+        structToHDF5Attribute(amiHDFfile, amiHDFOptionPath, options_ami);
+        h5writeatt(amiHDFfile, amiHDFOptionPath, 'ts', tout);
+        h5writeatt(amiHDFfile, amiHDFOptionPath, 'nt', numel(tout));
+        h5writeatt(amiHDFfile, amiHDFOptionPath, 'theta', theta);
+        h5writeatt(amiHDFfile, amiHDFOptionPath, 'kappa', kappa);
+        if(~isempty(data))
+            structToHDF5Attribute(amiHDFfile, [amiHDFprefix '/data'], data);
+        end
+        structToHDF5Attribute(amiHDFfile, [amiHDFprefix '/results'], sol);
+    end
+end
