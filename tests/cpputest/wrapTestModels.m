3ffed9a84e8ffeb0a8357e342fba103624d14612
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-05-09 12:53:41 +0200

    Convenience function to wrap models for CI tests

---
+++ tests/cpputest/wrapTestModels.m
@@ -0,0 +1,21 @@
+function [ output_args ] = wrapTestModels( input_args )
+    %WRAPTESTMODELS Summary of this function goes here
+    %   Detailed explanation goes here
+    
+    amiciPath = fileparts(mfilename('fullpath'));
+    amiciPath = [amiciPath '/../..'];
+
+    %% EXAMPLE STEADYSTATE
+
+    cd([amiciPath '/examples/example_steadystate/']);
+
+    [exdir,~,~]=fileparts(which('example_steadystate.m'));
+    amiwrap('model_steadystate','model_steadystate_syms',exdir)
+          
+    %% EXAMPLE DIRAC
+    cd([amiciPath '/examples/example_dirac/']);
+    
+    [exdir,~,~]=fileparts(which('example_dirac.m'));
+    amiwrap('model_dirac','model_dirac_syms',exdir)
+end
+
