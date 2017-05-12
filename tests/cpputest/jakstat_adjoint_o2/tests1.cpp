e531bcc1ee0b21bb924cd986b82bb14e3494e483
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-05-13 00:37:33 +0200

    Add test case for second order

---
+++ tests/cpputest/jakstat_adjoint_o2/tests1.cpp
@@ -0,0 +1,36 @@
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+#include "testfunctions.h"
+
+#include <cstring>
+#include "wrapfunctions.h"
+
+TEST_GROUP(groupJakstatAdjointO2)
+{
+    void setup() {
+
+    }
+
+    void teardown() {
+
+    }
+};
+
+
+TEST(groupJakstatAdjointO2, testSensitivity2) {
+    // read simulation options
+    UserData *udata = AMI_HDF5_readSimulationUserDataFromFileName(HDFFILE, "/model_jakstat_adjoint/sensi2forward/options");
+    ExpData *edata = AMI_HDF5_readSimulationExpData(HDFFILE, udata, "/model_jakstat_adjoint/sensi2forward/data");
+    int status;
+    ReturnData *rdata = getSimulationResults(udata, edata, &status);
+    CHECK_EQUAL(0, status);
+
+//    verifyReturnData("/model_jakstat_adjoint/sensi2forward/results", rdata, udata, TEST_ATOL, TEST_RTOL);
+
+    freeReturnData(rdata);
+    freeExpData(edata);
+    freeUserData(udata);
+}
+
+
