dbb9070ca576b4eaeaebea0e89f80f6677722dfe
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-05-16 10:10:27 +0200

    Update test

--- tests/cpputest/jakstat_adjoint_o2/tests1.cpp
+++ tests/cpputest/jakstat_adjoint_o2/tests1.cpp
@@ -23,10 +23,14 @@ TEST(groupJakstatAdjointO2, testSensitivity2) {
     UserData *udata = AMI_HDF5_readSimulationUserDataFromFileName(HDFFILE, "/model_jakstat_adjoint/sensi2forward/options");
     ExpData *edata = AMI_HDF5_readSimulationExpData(HDFFILE, udata, "/model_jakstat_adjoint/sensi2forward/data");
     int status;
+    CHECK_FALSE(edata == NULL);
+    CHECK_FALSE(udata == NULL);
+
     ReturnData *rdata = getSimulationResults(udata, edata, &status);
+
     CHECK_EQUAL(0, status);
 
-//    verifyReturnData("/model_jakstat_adjoint/sensi2forward/results", rdata, udata, TEST_ATOL, TEST_RTOL);
+    verifyReturnData("/model_jakstat_adjoint/sensi2forward/results", rdata, udata, TEST_ATOL, TEST_RTOL);
 
     freeReturnData(rdata);
     freeExpData(edata);
