c68eedeb872b72d64eacb5d0004d825445bd7de5
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-08-18 08:37:33 +0200

    Separate file for ReturnDataMatlab

--- include/amici_interface_matlab.h
+++ include/amici_interface_matlab.h
@@ -33,50 +33,6 @@ ReturnDataMatlab *setupReturnData(mxArray *plhs[], int nlhs, const UserData *uda
 ExpData *expDataFromMatlabCall(const mxArray *prhs[], const UserData *udata, Model *model);
 
 
-class ReturnDataMatlab : public ReturnData {
-
-public:
-    ReturnDataMatlab(const UserData *udata, const Model *model);
-    ~ReturnDataMatlab() {}
-
-    mxArray *mxsol;
-
-protected:
-    void initFields();
-
-    virtual void initField1(double **fieldPointer, const char *fieldName, int dim);
-
-    /**
-     * @ brief initialise matrix and attach to the field
-     * @ param FIELD name of the field to which the matrix will be attached
-     * @ param D1 number of rows in the matrix
-     * @ param D2 number of columns in the matrix
-     */
-
-    virtual void initField2(double **fieldPointer, const char *fieldName, int dim1, int dim2);
-
-    /**
-     * @ brief initialise 3D tensor and attach to the field
-     * @ param FIELD name of the field to which the tensor will be attached
-     * @ param D1 number of rows in the tensor
-     * @ param D2 number of columns in the tensor
-     * @ param D3 number of elements in the third dimension of the tensor
-     */
-
-    virtual void initField3(double **fieldPointer, const char *fieldName, int dim1, int dim2, int dim3);
-
-    /**
-     * @ brief initialise 4D tensor and attach to the field
-     * @ param FIELD name of the field to which the tensor will be attached
-     * @ param D1 number of rows in the tensor
-     * @ param D2 number of columns in the tensor
-     * @ param D3 number of elements in the third dimension of the tensor
-     * @ param D4 number of elements in the fourth dimension of the tensor
-     */
-
-    virtual void initField4(double **fieldPointer, const char *fieldName, int dim1, int dim2, int dim3, int dim4);
-
-};
 
 void amici_dgemv(AMICI_BLAS_LAYOUT layout,
                  AMICI_BLAS_TRANSPOSE TransA, const int M, const int N,
