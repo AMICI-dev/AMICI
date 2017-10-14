504eafca35407e70cf2d035058277f1e410a5838
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-10-14 14:55:19 +0200

    Boost::serialization support for UserData and ReturnData and corresponding tests

--- include/amici_misc.h
+++ include/amici_misc.h
@@ -1,16 +1,11 @@
 #ifndef AMICI_MISC_H
 #define AMICI_MISC_H
 
-#ifdef __cplusplus
-#define EXTERNC extern "C"
-#else
-#define EXTERNC
-#endif
-
-EXTERNC void zeros(double *destination, int count);
-EXTERNC void fillArray(double *destination, int count, double value);
-EXTERNC void printArray(double const *array, int numElements);
-EXTERNC void printfArray(double const *array, int numElements,
+#include<algorithm>
+void zeros(double *destination, int count);
+void fillArray(double *destination, int count, double value);
+void printArray(double const *array, int numElements);
+void printfArray(double const *array, int numElements,
                          char const *format);
 
 #endif // AMICI_MISC_H
