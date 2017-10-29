80d2868aae429bc5c9ecae9f1a935503fb70cbe8
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2017-10-29 13:50:10 +0100

    remove most error codes

--- include/amici_defines.h
+++ include/amici_defines.h
@@ -8,25 +8,10 @@ namespace amici {
 #define AMICI_ONEOUTPUT   5
 
 /* Return codes */
-#define AMICI_ERROR_UDATA              -99
-#define AMICI_ERROR_EDATA              -98
-#define AMICI_ERROR_RDATA              -97
-#define AMICI_ERROR_TDATA              -96
-#define AMICI_ERROR_SETUP              -95
-#define AMICI_ERROR_SETUPB             -94
-#define AMICI_ERROR_NOTHINGTODO        -93
-#define AMICI_ERROR_FSA                -92
-#define AMICI_ERROR_ASA                -91
-#define AMICI_ERROR_SA                 -90
+#define AMICI_FAILURE            -99
 #define AMICI_ERROR_SS_SENSIS          -89
-#define AMICI_ERROR_DATA               -88
-#define AMICI_ERROR_EVENT              -87
-#define AMICI_ERROR_SIMULATION         -86
 #define AMICI_ERROR_NEWTONSOLVER       -85
 #define AMICI_ERROR_NEWTON_LINSOLVER   -84
-#define AMICI_ERROR_NOT_IMPLEMENTED    -83
-#define AMICI_ERROR_MODEL              -82
-#define AMICI_ERROR_OTHER              -81
 #define AMICI_ERROR_SIM2STEADYSTATE    -80
 #define AMICI_ERROR_PREEQUILIBRATION   -79
 #define AMICI_SUCCESS                    0
