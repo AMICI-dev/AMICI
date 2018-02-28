3006ec67ed0b51f5cb0c55c3ee07ef737a8dd4a4
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2018-02-28 17:11:48 +0100

    updated hdf5 i/o and adapted tests

--- tests/testModels.m
+++ tests/testModels.m
@@ -9,8 +9,8 @@ function testModels()
     info = h5info(hdf5file);
     for imodel = 1:length(info.Groups)
         for itest = 1:length(info.Groups(imodel).Groups)
-            [results,options,data] = readDataFromHDF5(info.Groups(imodel).Groups(itest),hdf5file);
-            sol = getResults(info.Groups(imodel).Name(2:end),options,data);
+            [results,options,data,t,theta,kappa] = readDataFromHDF5(info.Groups(imodel).Groups(itest),hdf5file);
+            sol = getResults(info.Groups(imodel).Name(2:end),options,data,t,theta,kappa);
             compareResults(sol,results);
         end 
     end
@@ -18,22 +18,37 @@ function testModels()
     
 end
 
-function [results,options,data] = readDataFromHDF5(group,hdf5file);
+function [results,options,data,t,theta,kappa] = readDataFromHDF5(groups,hdf5file);
     data = [];
-    for idataset = 1:length(group.Datasets)
-        dataset = group.Datasets(idataset);
-        eval([dataset.Name ' =  hdf2struct(dataset,''' group.Name '/' dataset.Name ''',hdf5file);']);
+    t = [];
+    theta = [];
+    kappa = [];
+    for igroup = 1:length(groups.Groups)
+        group = groups.Groups(igroup);
+        [~,name] = fileparts(group.Name);
+        eval([name ' =  hdf2struct(group,''' group.Name ''',hdf5file);']);
     end
 end
 
-function s = hdf2struct(dataset,hdf5path,hdf5file)
+function s = hdf2struct(group,hdf5path,hdf5file)
     s = struct;
-    for attr = {dataset.Attributes.Name};
-        s.(attr{1}) = h5readatt(hdf5file,hdf5path,attr{1});
+    if(~isempty(group.Attributes))
+        for attr = {group.Attributes.Name};
+            s.(attr{1}) = h5readatt(hdf5file,hdf5path,attr{1});
+        end
+    end
+    if(~isempty(group.Datasets))
+        for data = {group.Datasets.Name};
+            s.(data{1}) = h5read(hdf5file,[hdf5path '/' data{1}]);
+        end
+    end
+    for igroup = 1:length(group.Groups);
+        [~,name] = fileparts(group.Groups(igroup).Name);
+        s.(name) = hdf2struct(group.Groups(igroup),[hdf5path '/' name],hdf5file);
     end
 end
 
-function sol = getResults(modelname,options,data)
+function sol = getResults(modelname,options,data,t,theta,kappa)
     theta = options.theta;
     options = rmfield(options,'theta');
     kappa = options.kappa;
@@ -63,39 +78,41 @@ function compareResults(sol,results)
     for ifield = transpose(fieldnames(sol))
         if(strcmp(ifield{1},'diagnosis'))
             for jfield = transpose(fieldnames(sol.diagnosis))
-                if(~ismember(jfield,{'J','newton_numsteps'})) % stuff that was previously missing/wrong
-                    checkAgreement(sol.diagnosis.(jfield{1}),results.(jfield{1}),0,1);
-                end
+                checkAgreement(sol.diagnosis,results.diagnosis,jfield{1},0,1);
             end
         else
-            if(~ismember(ifield,{'chi2','x0','sx0','ssigmay','s2x','s2y','s2sigmay'})) % stuff that was previously missing/wrong
-                if(ismember(ifield,{'s2llh'}))
-                    checkAgreement(sol.(ifield{1}),results.(ifield{1}),1e-4,1e-5)
-                else
-                    checkAgreement(sol.(ifield{1}),results.(ifield{1}))
-                end
+            if(ismember(ifield,{'s2llh'}))
+                checkAgreement(sol,results,ifield{1},1e-4,1e-5)
+            else
+                checkAgreement(sol,results,ifield{1})
             end
         end
     end
 end
 
-function checkAgreement(actual,expected,atol,rtol)
-   if(nargin<3)
-       atol = 1e-10;
-   end
-   if(nargin<4)
-       rtol = 1e-5;
-   end
-   if(~isempty(expected))
-       assert(~isempty(actual));
-       actual = actual(:);
-       expected = expected(:);
-       assert(all(isnan(actual)==isnan(expected)));
-       actual = actual(~isnan(actual));
-       expected = expected(~isnan(actual));
-       assert(all(isinf(actual)==isinf(expected)));
-       actual = actual(~isinf(actual));
-       expected = expected(~isinf(actual));
-       assert(all(abs(expected - actual) <= atol) || all(abs((expected - actual) ./ (rtol + expected)) <= rtol));
-   end
+function checkAgreement(sol,results,fieldname,atol,rtol)
+    if(~isfield(results,fieldname))
+        assert(isempty(sol.(fieldname)))
+        return
+    end
+    expected = results.(fieldname);
+    actual = sol.(fieldname);
+    if(nargin<4)
+        atol = 1e-10;
+    end
+    if(nargin<5)
+        rtol = 1e-5;
+    end
+    if(~isempty(expected))
+        assert(~isempty(actual));
+        actual = actual(:);
+        expected = expected(:);
+        assert(all(isnan(actual)==isnan(expected)));
+        actual = actual(~isnan(actual));
+        expected = expected(~isnan(actual));
+        assert(all(isinf(actual)==isinf(expected)));
+        actual = actual(~isinf(actual));
+        expected = expected(~isinf(actual));
+        assert(all(abs(expected - actual) <= atol) || all(abs((expected - actual) ./ (rtol + expected)) <= rtol));
+    end
 end
