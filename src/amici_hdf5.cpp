e8739b87a43692cbcc3ae297f44a26f7795cbe6f
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-03-02 14:22:05 +0100

    Deal with empty attributes. Empty sens_ind means empty sens_ind.

--- src/amici_hdf5.cpp
+++ src/amici_hdf5.cpp
@@ -426,7 +426,8 @@ std::vector<int> getIntArrayAttribute(H5::H5File const&file,
 #endif
 
     std::vector<int> buffer(length);
-    status = H5LTget_attribute_int(file.getId(), optionsObject.c_str(), attributeName.c_str(),
+    if(length)
+        status = H5LTget_attribute_int(file.getId(), optionsObject.c_str(), attributeName.c_str(),
                                    buffer.data());
     if (status < 0)
         throw AmiException("Error in getIntArrayAttribute: Cannot read "
@@ -622,15 +623,11 @@ void readModelDataFromHDF5(const H5::H5File &file, Model &model, const std::stri
 
     if(attributeExists(file, datasetPath, "sens_ind")) {
         auto sensInd = getIntArrayAttribute(file, datasetPath, "sens_ind");
-        if (sensInd.size() > 0) {
-            // currently base 1 indices are written
-            for (int i = 0; (unsigned)i < sensInd.size(); ++i) {
-                sensInd[i] -= 1;
-            }
-            model.setParameterList(sensInd);
-        } else {
-            model.requireSensitivitiesForAllParameters();
+        // currently base 1 indices are written
+        for (int i = 0; (unsigned)i < sensInd.size(); ++i) {
+            sensInd[i] -= 1;
         }
+        model.setParameterList(sensInd);
     }
 
     if(attributeExists(file, datasetPath, "x0")) {
