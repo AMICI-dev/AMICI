b534d9a1b507ac02b9d57102934bacb5a2c65ea8
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-03-14 13:48:34 +0100

    ReturnData::invalidate : access sx,sy only if they are non-empty. Fixes #285

--- src/rdata.cpp
+++ src/rdata.cpp
@@ -116,11 +116,16 @@ void ReturnData::invalidate(const realtype t) {
             x.at(ix * nt + it) = getNaN();
         for (int iy = 0; iy < ny; iy++)
             y.at(iy * nt + it) = getNaN();
-        for (int ip = 0; ip < nplist; ip++) {
-            for (int ix = 0; ix < nx; ix++)
-                sx.at((ip*nx + ix) * nt + it) = getNaN();
-            for (int iy = 0; iy < ny; iy++)
-                sy.at((ip*ny + iy) * nt + it) = getNaN();
+    }
+
+    if (sensi_meth == AMICI_SENSI_FSA || sensi >= AMICI_SENSI_ORDER_SECOND) {
+        for (int it = it_start; it < nt; it++){
+            for (int ip = 0; ip < nplist; ip++) {
+                for (int ix = 0; ix < nx; ix++)
+                    sx.at((ip*nx + ix) * nt + it) = getNaN();
+                for (int iy = 0; iy < ny; iy++)
+                    sy.at((ip*ny + iy) * nt + it) = getNaN();
+            }
         }
     }
 }
