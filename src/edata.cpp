e37f0fc8304f98be91db69163412787d0c690a82
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-04-06 20:37:52 +0200

    Cleanup

--- src/edata.cpp
+++ src/edata.cpp
@@ -39,15 +39,6 @@ ExpData::ExpData(const ExpData &other)
     : ExpData(other.nytrue, other.nztrue, other.nt, other.nmaxevent,
               other.my, other.sigmay, other.mz, other.sigmaz)
 {
-//    std::vector<realtype> my;
-//    /** standard deviation of observed data (dimension: nt x nytrue, row-major) */
-//    std::vector<realtype> sigmay;
-
-//    /** observed events (dimension: nmaxevents x nztrue, row-major) */
-//    std::vector<realtype> mz;
-//    /** standard deviation of observed events/roots
-//     * (dimension: nmaxevents x nztrue, row-major)*/
-//    std::vector<realtype> sigmaz;
 }
 
 void ExpData::setObservedData(const double *observedData) {
