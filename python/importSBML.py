d4dc85c0979455834e71118e5f23be7641a9c721
fabian.froehlich <fabian.froehlich@helmholtz-muenchen.de>
2018-04-27 17:40:57 +0200

    extract SBML test settings

--- python/importSBML.py
+++ python/importSBML.py
@@ -4,6 +4,7 @@ from model import Model
 import os
 import sys
 import importlib
+import numpy as np
 sys.path.insert(0, "../build/swig/python/")
 import amici
 
@@ -13,16 +14,30 @@ test_path = os.path.join(amici_path,'tests','sbml-semantic-test-cases','cases','
 
 def runTest(testId):
     try:
-        sbmlfile = os.path.join(test_path, testId, testId + '-sbml-l3v2.xml')
-        wrapper = Model(sbmlfile,'SBMLTest' + testId)
+        current_test_path = os.path.join(test_path, testId)
+        sbmlFile = os.path.join(current_test_path, testId + '-sbml-l3v2.xml')
+        settingsFile = os.path.join(current_test_path, testId + '-settings.txt')
+        resultsFile = os.path.join(current_test_path, testId + '-settings.txt')
+        wrapper = Model(sbmlFile,'SBMLTest' + testId)
         wrapper.wrapModel()
         sys.path.insert(0,os.path.join(wrapper.model_path,'build','swig'))
 
-        settings
+        settings = {}
+        with open(settingsFile) as f:
+            for line in f:
+                if not line == '\n':
+                    (key, val) = line.split(':')
+                    settings[key] = val
+        ts = np.linspace(float(settings['start']),float(settings['start'])+float(settings['duration']),int(settings['steps']))
+        atol = float(settings['absolute'])
+        rtol = float(settings['relative'])
 
         mod = importlib.import_module(wrapper.modelname)
         model = mod.getModel()
+        model.setTimepoints(mod.amici.DoubleVector(ts))
         solver = model.getSolver()
+        solver.setRelativeTolerance(rtol)
+        solver.setAbsoluteTolerance(atol)
         rdata = amici.runAmiciSimulation(solver.get(),None,model.get())
         print(rdata)
 
