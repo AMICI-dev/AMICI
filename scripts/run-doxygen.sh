cb87a40f05a13ad3ac4af51f09e014658e712df3
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-05-07 15:47:54 +0200

    refactor(doc) Fix mtoc paths. Cleanup.

--- scripts/run-doxygen.sh
+++ scripts/run-doxygen.sh
@@ -33,10 +33,11 @@ chmod +x ${MTOC_CONFIG_PATH}/mtocpp_filter.sh
 # generate doxyfile
 DOXYFILE=${MTOC_CONFIG_PATH}/Doxyfile
 cp ${MTOC_CONFIG_PATH}/Doxyfile.template ${DOXYFILE}
+DOXY_WARNING_FILE=${AMICI_PATH}/matlab/mtoc/warnings.log
 
 sed -i -e "s#_OutputDir_#$AMICI_PATH/doc#g" ${DOXYFILE}
 sed -i -e "s#_SourceDir_#$AMICI_PATH#g" ${DOXYFILE}
-sed -i -e "s#_ConfDir_#$AMICI_PATH/mtoc/config#g" ${DOXYFILE}
+sed -i -e "s#_ConfDir_#${MTOC_CONFIG_PATH}#g" ${DOXYFILE}
 sed -i -e "s#_ProjectName_#AMICI#g" ${DOXYFILE}
 sed -i -e "s#_ProjectDescription_#Advanced Matlab Interface for CVODES and IDAS#g" ${DOXYFILE}
 sed -i -e "s#_ProjectLogo_##g" ${DOXYFILE}
@@ -45,12 +46,12 @@ sed -i -e "s#_MTOCFILTER_#${MTOC_CONFIG_PATH}/mtocpp_filter.sh#g" ${DOXYFILE}
 sed -i -e "s#_LatexExtras_#${MTOC_CONFIG_PATH}/latexextras#g" ${DOXYFILE}
 sed -i -e "s#_GenLatex_#YES#g" ${DOXYFILE}
 sed -i -e "s#_HaveDot_#YES#g" ${DOXYFILE}
-sed -i -e "s#WARN_LOGFILE      =#WARN_LOGFILE      =$AMICI_PATH/mtoc/warnings.log#g" ${DOXYFILE}
+sed -i -e "s#WARN_LOGFILE      =#WARN_LOGFILE      =${DOXY_WARNING_FILE}#g" ${DOXYFILE}
 
 # generate latexextras
 
 cp ${MTOC_CONFIG_PATH}/latexextras.template ${MTOC_CONFIG_PATH}/latexextras.sty
-sed -i -e "s#_ConfDir_#$AMICI_PATH/mtoc/config#g" ${MTOC_CONFIG_PATH}/latexextras.sty
+sed -i -e "s#_ConfDir_#${MTOC_CONFIG_PATH}#g" ${MTOC_CONFIG_PATH}/latexextras.sty
 
 doxygen "${DOXYFILE}"
 doxygen "${DOXYFILE}"
@@ -62,12 +63,12 @@ rm ${DOXYFILE}
 rm ${MTOC_CONFIG_PATH}/mtocpp_filter.sh
 
 # check if warnings log was created
-if [ -f ${AMICI_PATH}/matlab/mtoc/warnings.log  ]; then
+if [ -f ${DOXY_WARNING_FILE}  ]; then
     # check if warnings log is empty
-if [ -s ${AMICI_PATH}/matlab/mtoc/warnings.log ]; then
+    if [ -s ${DOXY_WARNING_FILE} ]; then
         echo "DOXYGEN failed:"
-        cat ${AMICI_PATH}/matlab/mtoc/warnings.log
-        rm ${AMICI_PATH}/matlab/mtoc/warnings.log
+        cat ${DOXY_WARNING_FILE}
+        rm ${DOXY_WARNING_FILE}
         exit 1
     else
         exit 0
