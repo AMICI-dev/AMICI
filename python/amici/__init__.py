3fba43341bcd45f659d02662148d9e06c1183ecc
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-05-16 21:43:42 +0200

    Fix: import * only allowed at module level

--- python/amici/__init__.py
+++ python/amici/__init__.py
@@ -38,15 +38,13 @@ def setAmiciPaths():
     amiciSrcPath = os.path.join(amici_path, 'src')
     amiciModulePath = os.path.dirname(__file__)
 
-def importAmiciSwig():
-    # If this file is inside the amici package, import swig interface, 
-    # otherwise we are inside the git repository, then don't
-    dirname = os.path.dirname(__file__)
-    if os.path.isfile(os.path.join(dirname, 'amici.py')):
-        from .amici import *
+# If this file is inside the amici package, import swig interface, 
+# otherwise we are inside the git repository, then don't
+dirname = os.path.dirname(__file__)
+if os.path.isfile(os.path.join(dirname, 'amici.py')):
+    from .amici import *
 
 setAmiciPaths()
-importAmiciSwig()
 
 from .sbml_import import *
 
