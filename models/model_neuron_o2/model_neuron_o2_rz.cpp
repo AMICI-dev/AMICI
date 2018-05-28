f0b5f325e4cb38ac31ca650475f3877e370a8ea9
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-05-28 13:06:18 +0200

    (cmake) Add option to disable swig and python targets. Closes #315

--- models/model_neuron_o2/model_neuron_o2_rz.cpp
+++ models/model_neuron_o2/model_neuron_o2_rz.cpp
@@ -7,10 +7,16 @@ typedef amici::realtype realtype;
 using namespace amici;
 
 void rz_model_neuron_o2(double *rz, const int ie, const realtype t, const realtype *x, const realtype *p, const realtype *k, const realtype *h) {
+    switch(ie) { 
+        case 0: {
   rz[0] = x[0]-3.0E1;
   rz[1] = x[2];
   rz[2] = x[4];
   rz[3] = x[6];
   rz[4] = x[8];
+
+        } break;
+
+    } 
 }
 
