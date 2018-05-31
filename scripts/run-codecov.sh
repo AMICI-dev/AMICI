047aff5cb03e485a80f5509831b31879a0bd3845
Fabian Fröhlich <fabian@schaluck.com>
2018-05-31 12:07:58 +0200

    fix(codecov) fix lcov command

--- scripts/run-codecov.sh
+++ scripts/run-codecov.sh
@@ -8,10 +8,9 @@ lcov --base-directory ${AMICI_PATH} --directory ${AMICI_PATH} --zerocounters -q
 
 cd ${AMICI_PATH}/build
 ctest -V
-rm ${AMICI_PATH}/tests/cpputest/writeResults.h5
 cd ${AMICI_PATH}
 
-lcov --compat-libtool --no-external --directory ${AMICI_PATH}/build/CMakeFiles/amici.dir/src  --capture --output-file coverage.info
+lcov --compat-libtool --no-external --directory ${AMICI_PATH}/build/CMakeFiles/amici.dir/src --base-directory ${AMICI_PATH} --capture --output-file coverage.info
 
 wget http://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
 
