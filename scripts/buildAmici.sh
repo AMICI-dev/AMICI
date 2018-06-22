b20226368952bac96f3c6744ab40a37ac49bb3d3
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-06-22 16:16:12 +0200

    ci(python) Install amici package to virtual environment and run tests there (Closes #342)

--- scripts/buildAmici.sh
+++ scripts/buildAmici.sh
@@ -13,4 +13,6 @@ CPPUTEST_BUILD_DIR=${AMICI_PATH}/ThirdParty/cpputest-master/build/
 CppUTest_DIR=${CPPUTEST_BUILD_DIR} cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 make python-wheel
-pip3 install --user --prefix= `ls -t ${AMICI_PATH}/build/python/amici-*.whl | head -1`
+
+python3 -m venv ${AMICI_PATH}/build/venv --clear
+${AMICI_PATH}/build/venv/bin/pip3 install --prefix= `ls -t ${AMICI_PATH}/build/python/amici-*.whl | head -1`
---
+++ scripts/run-python-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Test python model wrapping inside virtual environment
+
+AMICI_PATH="`dirname \"$0\"`"
+AMICI_PATH="`( cd \"$AMICI_PATH/..\" && pwd )`"
+
+cd ${AMICI_PATH}/tests
+
+${AMICI_PATH}/build/venv/bin/pip3 install scipy
+${AMICI_PATH}/build/venv/bin/python3 testModels.py
