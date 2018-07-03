f991ce69469258fee245c7277ae593f2f1faee7c
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-07-03 13:51:35 +0200

    ci(python) no --user in venv; install wheel

--- scripts/buildAmici.sh
+++ scripts/buildAmici.sh
@@ -18,8 +18,9 @@ make
 #pip3 install --user --prefix= `ls -t ${AMICI_PATH}/build/python/amici-*.whl | head -1`
 
 make python-sdist
-
+set -x
 python3 -m venv ${AMICI_PATH}/build/venv --clear
 source ${AMICI_PATH}/build/venv/bin/activate
-pip3 install  --user pkgconfig
-pip3 install `ls -t ${AMICI_PATH}/build/python/amici-*.tar.gz | head -1`
+pip3 install --upgrade pip
+pip3 install pkgconfig wheel
+pip3 install --verbose `ls -t ${AMICI_PATH}/build/python/amici-*.tar.gz | head -1`
--- scripts/run-python-tests.sh
+++ scripts/run-python-tests.sh
@@ -7,6 +7,5 @@ AMICI_PATH="`( cd \"$AMICI_PATH/..\" && pwd )`"
 set -e
 cd ${AMICI_PATH}/tests
 source ${AMICI_PATH}/build/venv/bin/activate
-pip3 install --upgrade pip
-pip3 install --user scipy h5py
+pip3 install scipy h5py
 python3 testModels.py
