d1f5824cc5b3865aa6778e763711e344cb9baf91
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-07-03 12:57:45 +0200

    ci (python) venv activate

--- scripts/run-python-tests.sh
+++ scripts/run-python-tests.sh
@@ -5,6 +5,7 @@ AMICI_PATH="`dirname \"$0\"`"
 AMICI_PATH="`( cd \"$AMICI_PATH/..\" && pwd )`"
 
 cd ${AMICI_PATH}/tests
-
-${AMICI_PATH}/build/venv/bin/pip3 install --user scipy h5py
-${AMICI_PATH}/build/venv/bin/python3 testModels.py
+source ${AMICI_PATH}/build/venv/activate
+pip3 install --upgrade pip
+pip3 install --user scipy h5py
+python3 testModels.py
