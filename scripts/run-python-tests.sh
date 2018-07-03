039ebb0de9eafe1dffcf12d12da628cfa911d7d9
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-07-03 13:11:55 +0200

    ci (python) venv activate

--- scripts/run-python-tests.sh
+++ scripts/run-python-tests.sh
@@ -4,8 +4,9 @@
 AMICI_PATH="`dirname \"$0\"`"
 AMICI_PATH="`( cd \"$AMICI_PATH/..\" && pwd )`"
 
+set -e
 cd ${AMICI_PATH}/tests
-source ${AMICI_PATH}/build/venv/activate
+source ${AMICI_PATH}/build/venv/bin/activate
 pip3 install --upgrade pip
 pip3 install --user scipy h5py
 python3 testModels.py
