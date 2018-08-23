c68d6c5ef1684aa59ab3588412b2618d89229c29
FFroehlich <fabian@schaluck.com>
2018-08-23 10:16:47 -0400

    fix(tests) fix codacy issues

--- scripts/run-SBMLTestsuite.sh
+++ scripts/run-SBMLTestsuite.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Check test suite with valgrind
-# Note: CppuTest memcheck should be disabled
-# Note: Consider using ctest -T memcheck instead
-
-SCRIPT_PATH=$(dirname $BASH_SOURCE)
-AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 
 set -e
 
