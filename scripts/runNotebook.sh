7116da7c8fadb0b3e9e6871f0d7ee0e7589d2245
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-10-25 10:38:56 +0200

    Fix(ci) Fail CI on notebook setup error (Fixes #452)

--- scripts/runNotebook.sh
+++ scripts/runNotebook.sh
@@ -2,16 +2,20 @@
 # Run jupyter notebooks as given on command line, show output only on error. 
 # If a directory is provided, run all contained notebooks non-recursively.
 set -x
+set -e
+
 SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 
 runNotebook () {
+    set +e
     tempfile=$(mktemp)
     jupyter nbconvert --debug --stdout --execute --to markdown $@ &> $tempfile
     ret=$?
     if [[ $ret != 0 ]]; then cat $tempfile; fi
     rm $tempfile
     exit $ret
+    set -e
 }
 
 if [ $# -eq 0 ]; then
