224e0982a822420073461d1226d1d05a547cd8b2
FFroehlich <fabian@schaluck.com>
2018-11-17 20:35:59 -0500

    fix(style) fix codacy unused variable issue

--- tests/testPandas.py
+++ tests/testPandas.py
@@ -43,7 +43,7 @@ class TestAmiciPandasImportExport(unittest.TestCase):
         rdata = amici.runAmiciSimulation(self.model, self.solver)
         self.edata = [amici.ExpData(rdata, 0.01, 0)]
         # test copy constructor
-        ExpDataCopy = amici.ExpData(self.edata[0])
+        self.edata_copy = amici.ExpData(self.edata[0])
 
     def tearDown(self):
         os.chdir(self.resetdir)
