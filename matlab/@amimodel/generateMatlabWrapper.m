fca6043c000952e284522e8b592a6cdf48c037bb
FFroehlich <fabian@schaluck.com>
2018-12-12 20:50:12 -0500

    fix(matlab) fixes #491

--- matlab/@amimodel/generateMatlabWrapper.m
+++ matlab/@amimodel/generateMatlabWrapper.m
@@ -224,7 +224,7 @@ function generateMatlabWrapper(nx, ny, np, nk, nz, o2flag, amimodelo2, wrapperFi
         fprintf(fid,'if(nargin>=6)\n');
         fprintf(fid,'    chainRuleFactor = getChainRuleFactors(options_ami.pscale, theta, options_ami.sens_ind);\n');
         fprintf(fid,'    v = varargin{6};\n');
-        fprintf(fid,'    v = v(:).*chainRuleFactor;\n');
+        fprintf(fid,'    v = v(:).*chainRuleFactor(:);\n');
         fprintf(fid,'else\n');
         fprintf(fid,'    if(options_ami.sensi==2)\n');
         fprintf(fid,'        error(''6th argument (multiplication vector is missing'');\n');
