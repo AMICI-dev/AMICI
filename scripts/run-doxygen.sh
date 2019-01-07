bdede05e9087b263832c8a75b9b9e822f3697904
Fabian Fröhlich <fabian@schaluck.com>
2019-01-07 11:16:17 -0500

    fix(CI) ignore multiple @param sections warning for the time being, fixes #504

--- scripts/run-doxygen.sh
+++ scripts/run-doxygen.sh
@@ -71,6 +71,10 @@ cp ./refman.pdf ${AMICI_PATH}/AMICI_guide.pdf
 grep -v "warning: Unexpected html tag <img> found within <a href=...> context" ${DOXY_WARNING_FILE} > ${DOXY_WARNING_FILE}_tmp
 mv ${DOXY_WARNING_FILE}_tmp ${DOXY_WARNING_FILE}
 
+# suppress doxygen warnings about multiple param sections
+grep -v "has muliple @param documentation sections" ${DOXY_WARNING_FILE} > ${DOXY_WARNING_FILE}_tmp
+mv ${DOXY_WARNING_FILE}_tmp ${DOXY_WARNING_FILE}
+
 # check if warnings log was created
 if [ -f ${DOXY_WARNING_FILE}  ]; then
     # check if warnings log is empty
