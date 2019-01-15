d4c201a81b510c1512efc8e5da4d410949fa7ea2
FFroehlich <fabian@schaluck.com>
2019-01-15 17:42:15 -0500

    silence buildCpputest (0.7k lines)

--- scripts/buildCpputest.sh
+++ scripts/buildCpputest.sh
@@ -14,15 +14,15 @@ export CPPUTEST_BUILD_DIR=${AMICI_PATH}/ThirdParty/cpputest-master/
 
 if [ ! -d "cpputest-master" ]; then
     if [ ! -e "cpputest-master.zip" ]; then
-        wget -O cpputest-master.zip https://codeload.github.com/cpputest/cpputest/zip/master
+        wget -q -O cpputest-master.zip https://codeload.github.com/cpputest/cpputest/zip/master
     fi
-    unzip cpputest-master.zip
+    unzip -q cpputest-master.zip
     #cd cpputest-master/ && ./autogen.sh && ./configure && make
 fi
 
 cd cpputest-master
 mkdir -p build
 cd build
-cmake -DTESTS=OFF -DBUILD_TESTING=OFF -DC++11=ON ..
+cmake -DTESTS=OFF -DBUILD_TESTING=OFF -DC++11=ON .. -quiet
 make -j4
 
