c99c8a5dc7de249d6588dcb77976f943fec66a3f
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-02-03 19:45:36 +0100

    feature(python) Check for matching AMICI versions when importing model (Closes #556). Set exact AMICI version as model package requirement.

--- python/amici/__init__.template.py
+++ python/amici/__init__.template.py
@@ -1,3 +1,16 @@
 """AMICI-generated module for model TPL_MODELNAME"""
 
+import amici
+
+# Ensure we are binary-compatible, see #556
+if 'TPL_AMICI_VERSION' != amici.__version__:
+    raise RuntimeError('Cannot use model TPL_MODELNAME, generated with AMICI '
+                       'version TPL_AMICI_VERSION, together with AMICI version'
+                       f' {amici.__version__} which is present in your '
+                       'PYTHONPATH. Install the AMICI package matching the '
+                       'model version or regenerate the model with the AMICI '
+                       'currently in your path.')
+
 from TPL_MODELNAME.TPL_MODELNAME import *
+
+__version__ = 'TPL_PACKAGE_VERSION'
