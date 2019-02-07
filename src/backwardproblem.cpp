5338b0a741e163cd581d59d67ac56fdcfc51b40d
Daniel Weindl <dweindl@users.noreply.github.com>
2019-02-07 16:37:19 +0100

    fix(core) Get solver diagnostics for first(last) timepoint (#588) (Closes #586)
    * fix(core) Get solver diagnostics for first(last) timepoint. Cleanup. Doc.
    
    * doc(core) doxygen

--- src/backwardproblem.cpp
+++ src/backwardproblem.cpp
@@ -10,11 +10,7 @@
 #include <cstring>
 
 namespace amici {
-    
-/** Construct backward problem from forward problem
-  * @param fwd pointer to corresponding forward problem
-  * @return new BackwardProblem instance
-  */
+
 BackwardProblem::BackwardProblem(const ForwardProblem *fwd) :
     llhS0(static_cast<decltype(llhS0)::size_type>(fwd->model->nJ * fwd->model->nplist()), 0.0),
     xB(fwd->model->nx_solver),
@@ -40,18 +36,14 @@ BackwardProblem::BackwardProblem(const ForwardProblem *fwd) :
 
 
 void BackwardProblem::workBackwardProblem() {
-    /**
-     * workBackwardProblem solves the backward problem. if adjoint
-     * sensitivities are enabled this will also compute sensitivies
-     * workForwardProblem should be called before this is function is called
-     */
+
 
     if (model->nx_solver <= 0 || solver->getSensitivityOrder() < SensitivityOrder::first ||
         solver->getSensitivityMethod() != SensitivityMethod::adjoint || model->nplist() == 0) {
         return;
     }
-    
-    solver->setupAMIB(this, model);
+
+    solver->setupB(this, model);
 
     int it = rdata->nt - 2;
     --iroot;
@@ -65,10 +57,10 @@ void BackwardProblem::workBackwardProblem() {
             solver->solveB(tnext, AMICI_NORMAL);
             solver->getB(which, &t, &xB, &dxB);
             solver->getQuadB(which, &t, &xQB);
+            solver->getDiagnosisB(it, rdata, this->which);
         }
 
         /* handle discontinuity */
-
         if (model->ne > 0 && rdata->nmaxevent > 0 && iroot >= 0) {
             if (tnext == discs.at(iroot)) {
                 handleEventB(iroot);
@@ -90,61 +82,18 @@ void BackwardProblem::workBackwardProblem() {
 
     /* we still need to integrate from first datapoint to tstart */
     if (t > model->t0()) {
-        if (model->nx_solver > 0) {
-            /* solve for backward problems */
-            solver->solveB(model->t0(), AMICI_NORMAL);
-            solver->getQuadB(which, &(t), &xQB);
-            solver->getB(which, &(t), &xB, &dxB);
-        }
-    }
-    
-
-    for (int iJ = 0; iJ < model->nJ; iJ++) {
-        if (iJ == 0) {
-            for (int ip = 0; ip < model->nplist(); ++ip) {
-                llhS0[ip] = 0.0;
-                for (int ix = 0; ix < model->nxtrue_solver; ++ix) {
-                    llhS0[ip] += xB[ix] * sx.at(ix,ip);
-                }
-            }
-        } else {
-            for (int ip = 0; ip < model->nplist(); ++ip) {
-                llhS0[ip + iJ * model->nplist()] = 0.0;
-                for (int ix = 0; ix < model->nxtrue_solver; ++ix) {
-                    llhS0[ip + iJ * model->nplist()] +=
-                        xB[ix + iJ * model->nxtrue_solver] * sx.at(ix,ip)+
-                        xB[ix] * sx.at(ix + iJ * model->nxtrue_solver,ip);
-                }
-            }
-        }
+        /* solve for backward problems */
+        solver->solveB(model->t0(), AMICI_NORMAL);
+        solver->getQuadB(which, &(t), &xQB);
+        solver->getB(which, &(t), &xB, &dxB);
+        solver->getDiagnosisB(it, rdata, this->which);
     }
 
-    for (int iJ = 0; iJ < model->nJ; iJ++) {
-        for (int ip = 0; ip < model->nplist(); ip++) {
-            if (iJ == 0) {
-                rdata->sllh.at(ip) -= llhS0[ip] + xQB[ip*model->nJ];
-            } else {
-                rdata->s2llh.at(iJ - 1 + ip * (model->nJ - 1)) -=
-                    llhS0[ip + iJ * model->nplist()] +
-                    xQB[iJ + ip*model->nJ];
-            }
-        }
-    }
-
+    computeLikelihoodSensitivities();
 }
 
-/* ------------------------------------------------------------------------ */
-/* ------------------------------------------------------------------------ */
-/* ------------------------------------------------------------------------ */
 
 void BackwardProblem::handleEventB(const int iroot) {
-    /**
-     * handleEventB executes everything necessary for the handling of events
-     * for the backward problem
-     *
-     * @param iroot index of event @type int
-     */
-
     for (int ie = 0; ie < model->ne; ie++) {
 
         if (rootidx[iroot * model->ne + ie] == 0) {
@@ -178,12 +127,7 @@ void BackwardProblem::handleEventB(const int iroot) {
     model->updateHeavisideB(&rootidx[iroot * model->ne]);
 }
 
-/**
- * handleDataPoint executes everything necessary for the handling of data
- * points for the backward problems
- *
- * @param it index of data point @type int
- */
+
 void BackwardProblem::handleDataPointB(const int it) {
     for (int ix = 0; ix < model->nxtrue_solver; ix++) {
         for (int iJ = 0; iJ < model->nJ; iJ++)
@@ -191,29 +135,53 @@ void BackwardProblem::handleDataPointB(const int it) {
             xB[ix + iJ * model->nxtrue_solver] +=
                 dJydx[iJ + ( ix + it * model->nx_solver ) * model->nJ];
     }
-    solver->getDiagnosisB(it, rdata, this->which);
 }
-    
-/**
- * getTnext computes the next timepoint to integrate to. This is the maximum
- * of tdata and troot but also takes into account if it<0 or iroot<0 where
- * these expressions
- * do not necessarily make sense
- *
- * @param troot timepoint of next event @type realtype
- * @param iroot index of next event @type int
- * @param it index of next data point @type int
- * @param model pointer to model specification object @type Model
- * @return tnext next timepoint @type realtype
- */
-realtype BackwardProblem::getTnext(std::vector<realtype> const& troot, const int iroot,
-                                   const int it) {
+
+
+realtype BackwardProblem::getTnext(std::vector<realtype> const& troot,
+                                   const int iroot, const int it) {
     if (it < 0
             || (iroot >= 0 && model->ne > 0 && troot.at(iroot) > rdata->ts[it])) {
         return troot.at(iroot);
     }
 
     return rdata->ts[it];
+}
+
+
+void BackwardProblem::computeLikelihoodSensitivities()
+{
+    for (int iJ = 0; iJ < model->nJ; iJ++) {
+        if (iJ == 0) {
+            for (int ip = 0; ip < model->nplist(); ++ip) {
+                llhS0[ip] = 0.0;
+                for (int ix = 0; ix < model->nxtrue_solver; ++ix) {
+                    llhS0[ip] += xB[ix] * sx.at(ix,ip);
+                }
+            }
+        } else {
+            for (int ip = 0; ip < model->nplist(); ++ip) {
+                llhS0[ip + iJ * model->nplist()] = 0.0;
+                for (int ix = 0; ix < model->nxtrue_solver; ++ix) {
+                    llhS0[ip + iJ * model->nplist()] +=
+                        xB[ix + iJ * model->nxtrue_solver] * sx.at(ix,ip)+
+                        xB[ix] * sx.at(ix + iJ * model->nxtrue_solver,ip);
+                }
+            }
+        }
+    }
+
+    for (int iJ = 0; iJ < model->nJ; iJ++) {
+        for (int ip = 0; ip < model->nplist(); ip++) {
+            if (iJ == 0) {
+                rdata->sllh.at(ip) -= llhS0[ip] + xQB[ip*model->nJ];
+            } else {
+                rdata->s2llh.at(iJ - 1 + ip * (model->nJ - 1)) -=
+                    llhS0[ip + iJ * model->nplist()] +
+                    xQB[iJ + ip*model->nJ];
+            }
+        }
+    }
 }
 
 } // namespace amici
