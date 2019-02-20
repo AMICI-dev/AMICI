6d5c423a15730368437b4d37704f659391f2bd56
FFroehlich <fabian@schaluck.com>
2019-02-20 16:16:50 -0500

    Revert "Try previous Cpputest version (_dl_runtime_resolve_xsave)"
    This reverts commit ae8f021dfed2ce816bb7cb848e51f6ff17fe1214.

--- scripts/buildCpputest.sh
+++ scripts/buildCpputest.sh
@@ -14,7 +14,7 @@ export CPPUTEST_BUILD_DIR=${AMICI_PATH}/ThirdParty/cpputest-master/
 
 if [ ! -d "cpputest-master" ]; then
     if [ ! -e "cpputest-master.zip" ]; then
-        wget -q -O cpputest-master.zip https://codeload.github.com/cpputest/cpputest/zip/23a8ec83bd129b0afe4d9e38509e9bdb489fce55 # https://codeload.github.com/cpputest/cpputest/zip/master
+        wget -q -O cpputest-master.zip https://codeload.github.com/cpputest/cpputest/zip/master
     fi
     unzip -q cpputest-master.zip
     #cd cpputest-master/ && ./autogen.sh && ./configure && make
