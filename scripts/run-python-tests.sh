1c584fc4e3c944c43960f23a11a1226d78215271
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-03-10 18:13:18 +0100

    Set BNGPATH for users with no default BNGPATH set

--- scripts/run-python-tests.sh
+++ scripts/run-python-tests.sh
@@ -5,6 +5,11 @@ SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 
 set -e
+
+if [[ -z "${BNGPATH}" ]]; then
+    export BNGPATH=${AMICI_PATH}/ThirdParty/BioNetGen-2.3.2
+fi
+
 cd ${AMICI_PATH}/tests
 source ${AMICI_PATH}/build/venv/bin/activate
 pip install scipy h5py
