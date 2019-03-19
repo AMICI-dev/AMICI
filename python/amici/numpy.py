6a8e3f6955d4ba2d4361aa70637283773c0c3d3b
FFroehlich <fabian@schaluck.com>
2019-03-19 14:18:28 -0400

    bugfixes and add documentation

--- python/amici/numpy.py
+++ python/amici/numpy.py
@@ -1,17 +1,41 @@
 import numpy as np
 import copy
 import collections
+from . import ExpDataPtr, ReturnDataPtr, ExpData, ReturnData
 
 
-class SwigClassView(collections.abc.Mapping):
+class SwigPtrView(collections.abc.Mapping):
+    """ Interface class to expose std::vector<double> and scalar members of
+    swig wrapped C++ objects as numpy array attributes and fields. This
+    class is memory efficient as copies of the underlying C++ objects is
+    only created when respective fields are accessed for the first time.
+    Cached copies are used for all subsequent calls.
+
+    Attributes:
+        _swigptr: pointer to the c++ object
+            @type SwigPtr
+        _field_names: names of members that will be exposed as numpy arrays
+            @type list
+        _field_dimensions: dimensions of numpy arrays @type list
+        _cache: dictionary with cached values @type dict
+    """
+
     _swigptr = None
     _field_names = []
-    _field_dimensions = []
-    _cache = dict()
+    _field_dimensions = dict()
 
     def __getitem__(self, item):
-        if len(self._field_names) == 0 or len(self._field_dimensions) == 0 or \
-                self._swigptr is None:
+        """Access to field names, copies data from C++ object into numpy
+        array, reshapes according to field dimensions and stores values in
+        cache.
+
+        Arguments:
+            item: field name
+
+        Returns:
+            value @type numpy.array or float
+        """
+        if self._swigptr is None:
             raise NotImplementedError('Cannot get items from abstract class.')
 
         if item == 'ptr':
@@ -20,77 +44,129 @@ class SwigClassView(collections.abc.Mapping):
         if item in self._cache:
             return self._cache[item]
 
-        if item not in self._field_names or item not in self._field_dimensions:
+        if item not in self._field_names:
             self.__missing__(item)
 
         value = fieldAsNumpy(
-            self._field_dimensions[item], self._field_names[item], self._swigptr
+            self._field_dimensions, item, self._swigptr
         )
         self._cache[item] = value
         return value
 
     def __missing__(self, key):
+        """Default behaviour for missing keys
+
+        Arguments:
+            key: field name
+
+        Returns:
+
+        Raises:
+            KeyError
+        """
         raise KeyError(f'Unknown field name {key}.')
 
     def __getattr__(self, item):
+        """Attribute accessor for field names
+
+        Arguments:
+            item: field name
+
+        Returns:
+            value @type numpy.array or float
+        """
         return self.__getitem__(item)
 
     def __init__(self, swigptr):
+        """Constructor
+
+        Arguments:
+            swigptr: pointer to the C++ object
+
+        Returns:
+            SwigPtrView instance @type SwigPtrView
+        """
         self._swigptr = swigptr
-        super(SwigClassView, self).__init__()
+        self._cache = dict()
+        super(SwigPtrView, self).__init__()
 
     def __len__(self):
+        """Returns the number of available keys/fields
+
+        Returns:
+            length of _field_names @type int
+        """
         return len(self._field_names)
 
     def __iter__(self):
+        """Create an iterator of the keys/fields
+
+        Returns:
+            iterator over _field_names @type iter
+        """
         return iter(self._field_names)
 
     def __copy__(self):
-        other = SwigClassView(self._swigptr)
+        """Create a shallow copy
+
+        Arguments:
+
+        Returns:
+            SwigPtrView shallow copy @type SwigPtrView
+        """
+        other = SwigPtrView(self._swigptr)
         other._field_names = self._field_names
         other._field_dimensions = self._field_dimensions
         other._cache = self._cache
         return other
 
+    def __contains__(self, item):
+        """Faster implementation of __contains__ that avoids copy of the field
+
+        Arguments:
+
+        Returns:
+            whether item is available as key @type bool
+        """
+        return item in self._field_names
+
     def __deepcopy__(self):
-        other = SwigClassView(self._swigptr)
+        """Create a deep copy
+
+        Arguments:
+
+        Returns:
+            SwigPtrView deep copy @type SwigPtrView
+        """
+        other = SwigPtrView(self._swigptr)
         other._field_names = copy.deepcopy(self._field_names)
         other._field_dimensions = copy.deepcopy(self._field_dimensions)
         other._cache = copy.deepcopy(self._cache)
         return other
 
 
-class ReturnDataView(SwigClassView):
+class ReturnDataView(SwigPtrView):
     """ Interface class for C++ Return Data objects that avoids possibly costly
     copies of member data.
     """
 
-    _field_names = ['t', 'x', 'x0', 'x_ss', 'sx', 'sx0', 'sx_ss', 'y', 'sigmay',
-                   'sy', 'ssigmay', 'z', 'rz', 'sigmaz', 'sz', 'srz',
-                   'ssigmaz', 'sllh', 's2llh', 'J', 'xdot', 'status', 'llh',
-                   'chi2', 'res', 'sres', 'FIM', 'wrms_steadystate',
-                   't_steadystate', 'newton_numlinsteps', 'newton_numsteps',
-                   'numsteps', 'numrhsevals', 'numerrtestfails',
-                   'numnonlinsolvconvfails', 'order', 'numstepsB',
-                    'numrhsevalsB', 'numerrtestfailsB',
-                    'numnonlinsolvconvfailsB']
+    _field_names = [
+        'ts', 'x', 'x0', 'x_ss', 'sx', 'sx0', 'sx_ss', 'y', 'sigmay',
+        'sy', 'ssigmay', 'z', 'rz', 'sigmaz', 'sz', 'srz',
+        'ssigmaz', 'sllh', 's2llh', 'J', 'xdot', 'status', 'llh',
+        'chi2', 'res', 'sres', 'FIM', 'wrms_steadystate',
+        't_steadystate', 'newton_numlinsteps', 'newton_numsteps',
+        'numsteps', 'numrhsevals', 'numerrtestfails',
+        'numnonlinsolvconvfails', 'order', 'numstepsB',
+        'numrhsevalsB', 'numerrtestfailsB',
+        'numnonlinsolvconvfailsB'
+    ]
 
     def __init__(self, rdata):
-        self.nt = rdata.nt
-        self.nx = rdata.nx
-        self.nxtrue = rdata.nxtrue
-        self.nxsolver = rdata.nx_solver
-        self.ny = rdata.ny
-        self.nytrue = rdata.nytrue
-        self.nz = rdata.nz
-        self.nztrue = rdata.nztrue
-        self.np = rdata.np
-        self.nplist = rdata.nplist
-        self.nJ = rdata.nJ
-        self.nmaxevent = rdata.nmaxevent
-        self.newton_maxsteps = self.newton_maxsteps
-
-        self.field_dimensions = {
+        if not isinstance(rdata, (ReturnDataPtr, ReturnData)):
+            raise TypeError(f'Unsupported pointer {type(rdata)}, must be'
+                            f'amici.ExpDataPtr!')
+        self._field_dimensions = {
             'ts': [rdata.nt],
             'x': [rdata.nt, rdata.nx],
             'x0': [rdata.nx],
@@ -141,21 +217,26 @@ class ReturnDataView(SwigClassView):
     def __getitem__(self, item):
         if item == 't':
             item = 'ts'
-        super(ReturnDataView, self).__init__(item)
+        return super(ReturnDataView, self).__getitem__(item)
 
 
-class ExpDataView(SwigClassView):
+class ExpDataView(SwigPtrView):
     """ Interface class for C++ Exp Data objects that avoids possibly costly
     copies of member data.
     """
 
-    _field_names = ['observedData', 'observedDataStdDev', 'observedEvents',
-                   'observedEventsStdDev', 'fixedParameters',
-                   'fixedParametersPreequilibration',
-                   'fixedParametersPresimulation']
+    _field_names = [
+        'observedData', 'observedDataStdDev', 'observedEvents',
+        'observedEventsStdDev', 'fixedParameters',
+        'fixedParametersPreequilibration',
+        'fixedParametersPresimulation'
+    ]
 
     def __init__(self, edata):
-        self.field_dimensions = {  # observables
+        if not isinstance(edata, (ExpDataPtr, ExpData)):
+            raise TypeError(f'Unsupported pointer {type(edata)}, must be'
+                            f'amici.ExpDataPtr!')
+        self._field_dimensions = {  # observables
             'observedData': [edata.nt(), edata.nytrue()],
             'observedDataStdDev': [edata.nt(), edata.nytrue()],
 
@@ -175,11 +256,6 @@ class ExpDataView(SwigClassView):
         edata.observedEvents = edata.getObservedEvents()
         edata.observedEventsStdDev = edata.getObservedEventsStdDev()
         super(ExpDataView, self).__init__(edata)
-
-    def __getitem__(self, item):
-        if item == 't':
-            item = 'ts'
-        super(ExpDataView, self).__init__(item)
 
 
 def fieldAsNumpy(field_dimensions, field, data):
