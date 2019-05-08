50bcbc74f397a2814a32162363e9f3aebecdc66c
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-05-08 18:00:26 +0200

    For CMake, get AMICI version from version.txt to avoid issues of unmerged tagged branches and building from non-git directory

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -2,7 +2,11 @@ find_package(Git)
 if(Git_FOUND)
     execute_process(COMMAND sh -c "${GIT_EXECUTABLE} describe --abbrev=4 --dirty=-dirty --always --tags  | cut -c 2- | tr -d '\n' | sed -E s/-/./"
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        OUTPUT_VARIABLE PROJECT_VERSION
+        OUTPUT_VARIABLE PROJECT_VERSION_GIT
         )
 endif()
 
+execute_process(COMMAND sh -c "cat version.txt"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE PROJECT_VERSION
+    )
