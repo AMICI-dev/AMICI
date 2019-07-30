99854c49f4556647e1aced82b4d6d7df459f8852
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-07-30 19:22:58 +0200

    fix(python) Workaround for missing ensurepip during venv creation [ci skip]

--- scripts/installAmiciSource.sh
+++ scripts/installAmiciSource.sh
@@ -20,8 +20,21 @@ rm -rf ${AMICI_PATH}/python/sdist/build/
 
 
 # test install from setup.py
+set +e
 python3 -m venv ${AMICI_PATH}/build/venv --clear
-source ${AMICI_PATH}/build/venv/bin/activate
+# in case this fails (usually due to missing ensurepip, try getting pip
+# manually
+if [[ $? ]]; then
+    set -e
+    python3 -m venv ${AMICI_PATH}/build/venv --clear --without-pip
+    source ${AMICI_PATH}/build/venv/bin/activate
+    curl https://bootstrap.pypa.io/get-pip.py -o ${AMICI_PATH}/build/get-pip.py
+    python ${AMICI_PATH}/build/get-pip.py
+else
+    set -e
+    source ${AMICI_PATH}/build/venv/bin/activate
+fi
+
 # install wheel separately to prevent build_wheel fail in next step
 pip install --upgrade wheel
 pip install --upgrade pip setuptools pkgconfig scipy matplotlib pysb coverage
