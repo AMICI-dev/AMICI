63db79b9e6f2ad05bb2a626fa44e25a3c9c5509b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-08-29 15:57:31 +0200

    Undo unnecessary error handling

--- tests/testSBMLSuite.py
+++ tests/testSBMLSuite.py
@@ -24,36 +24,12 @@ from typing import List
 test_path = os.path.join(os.path.dirname(__file__), 'sbml-test-suite', 'cases',
                          'semantic')
 
-EXPECTED_TO_WORK = {
-    *list(range(1, 26)), 27, *list(range(34, 38)), *list(range(42, 51)),
-    *list(range(54, 66)), *list(range(75, 78)), *list(range(95, 104)),
-    *list(range(107, 122)), *list(range(125, 129)), *list(range(132, 136)),
-    *list(range(145, 151)), *list(range(186, 196)), 199,
-    *list(range(202, 269)), *list(range(270, 276)), 277,
-    *list(range(280, 286)), *list(range(462, 469)),
-    *list(range(471, 477)), *list(range(480, 516)), *list(range(522, 528)),
-    *list(range(529, 531)), *list(range(577, 607)), *list(range(616, 619)),
-    *list(range(631, 634)), *list(range(676, 679)), 688, 697, 698, 706,
-    *list(range(781, 789)),
-    *list(range(794, 800)), *list(range(801, 827)), *list(range(830, 839)),
-    *list(range(851, 870)), 877, 878, 882, *list(range(894, 898)),
-    *list(range(969, 972)),
-    *list(range(975, 978)), *list(range(1001, 1008)), *list(range(1009, 1014)),
-    *list(range(1018, 1027)), *list(range(1030, 1039)),
-    *list(range(1055, 1071)), *list(range(1077, 1083)),
-    *list(range(1096, 1103)), 1104, 1107, *list(range(1109, 1112)), 1225, 1232,
-    1245, 1246, 1288, 1307, 1341, 1342, *list(range(1420, 1435)), 1436, 1438,
-    1440, 1442, 1465, 1552, 1554, 1555, 1557, 1574, *list(range(1631, 1641)),
-    *list(range(1645, 1651)), 1722, *list(range(1724, 1727)),
-    *list(range(1730, 1736)), *list(range(1739, 1742)), 1744, 1746, 1748, 1750,
-    1752, 1753, 1760, 1762, 1764, 1767, 1773, 1774
-}
+
 ALL_TESTS = set(range(1, 1781))
 
 
 class TestAmiciSBMLTestSuite(unittest.TestCase):
     SBML_TEST_IDS = ALL_TESTS
-    SBML_TESTS_PASSED = set()
 
     def setUp(self):
         self.resetdir = os.getcwd()
@@ -135,8 +111,6 @@ class TestAmiciSBMLTestSuite(unittest.TestCase):
                 np.isclose(simulated_x, test_x, atol, rtol).all()
             )
             print(f'TestCase {test_id} passed.')
-            TestAmiciSBMLTestSuite.SBML_TESTS_PASSED |= {int(test_id)}
-
 
         except amici.sbml_import.SBMLException as err:
             print(f'TestCase {test_id} was skipped: {err}')
@@ -253,62 +227,9 @@ def parse_selection(selection_str: str) -> List[int]:
     return indices
 
 
-def print_stats():
-    """Print test statistics"""
-
-    percent_passed = (len(TestAmiciSBMLTestSuite.SBML_TESTS_PASSED)
-                     / len(TestAmiciSBMLTestSuite.SBML_TEST_IDS))
-    known_to_fail = TestAmiciSBMLTestSuite.SBML_TEST_IDS - EXPECTED_TO_WORK
-    failed = (TestAmiciSBMLTestSuite.SBML_TEST_IDS
-              - TestAmiciSBMLTestSuite.SBML_TESTS_PASSED)
-    unexpectedly_failed = failed - known_to_fail
-    unexpectedly_succeeded = (TestAmiciSBMLTestSuite.SBML_TESTS_PASSED
-                              - EXPECTED_TO_WORK)
-
-    print()
-    print('Tests executed:', len(TestAmiciSBMLTestSuite.SBML_TEST_IDS))
-    print('Tests passed:', len(TestAmiciSBMLTestSuite.SBML_TESTS_PASSED),
-          f'({percent_passed}% of tests run)')
-
-    if unexpectedly_failed:
-        print()
-        print('ERROR:', len(unexpectedly_failed),
-              'tests failed which were expected to work:', unexpectedly_failed)
-
-    if unexpectedly_succeeded:
-        print()
-        print('Congratulations:', len(unexpectedly_succeeded),
-              'tests passed which did not pass before:',
-              unexpectedly_succeeded)
-        print('This tests still fails until you update '
-              f' {__file__}.EXPECTED_TO_WORK accordingly.')
-
-
-def get_return_code() -> int:
-    """
-    We return success if we pass at least all tests which were known to work
-    before.
-
-    Returns:
-        exit code
-    """
-    known_to_fail = TestAmiciSBMLTestSuite.SBML_TEST_IDS - EXPECTED_TO_WORK
-    failed = (TestAmiciSBMLTestSuite.SBML_TEST_IDS
-              - TestAmiciSBMLTestSuite.SBML_TESTS_PASSED)
-    unexpectedly_failed = failed - known_to_fail
-    unexpectedly_succeeded = (TestAmiciSBMLTestSuite.SBML_TESTS_PASSED
-                              - EXPECTED_TO_WORK)
-
-    return len(unexpectedly_failed) + len(unexpectedly_succeeded)
-
-
 if __name__ == '__main__':
     TestAmiciSBMLTestSuite.SBML_TEST_IDS = set(parse_selection(sys.argv.pop()))
 
     suite = unittest.TestSuite()
     suite.addTest(TestAmiciSBMLTestSuite())
-    unittest.main(buffer=False, exit=False)
-
-    print_stats()
-
-    sys.exit(get_return_code())
+    unittest.main()
