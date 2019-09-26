aaf8e36d1d6a2f283e1438a8d1b35f9d9e7fbb57
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-09-26 21:12:39 +0200

    Formatting

--- scripts/run-cppcheck.sh
+++ scripts/run-cppcheck.sh
@@ -3,10 +3,13 @@
 # Note: CppuTest memcheck should be disabled
 # Note: Consider using ctest -T memcheck instead
 
-SCRIPT_PATH=$(dirname $BASH_SOURCE)
-AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
+SCRIPT_PATH=$(dirname "$BASH_SOURCE")
+AMICI_PATH=$(cd "$SCRIPT_PATH"/.. && pwd)
 
 cd ${AMICI_PATH}
 
-cppcheck -i${AMICI_PATH}/src/doc ${AMICI_PATH}/src  -I${AMICI_PATH}/include/ --enable=style --exitcode-suppressions=${AMICI_PATH}/.cppcheck-exitcode-suppressions
+cppcheck -i"${AMICI_PATH}"/src/doc "${AMICI_PATH}"/src \
+         -I$"{AMICI_PATH}"/include/ \
+         --enable=style \
+         --exitcode-suppressions="${AMICI_PATH}"/.cppcheck-exitcode-suppressions
 
