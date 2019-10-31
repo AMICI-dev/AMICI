290e5986c89f29e9c1128ad2cd4c69bf08d9542c
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-10-31 07:45:28 +0100

    test

--- tests/performance/test.py
+++ tests/performance/test.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python3
 
 import amici
+import sys
 
 import CS_Signalling_ERBB_RAS_AKT_petab as model_module
 
 
 def main():
+    arg = sys.argv[1]
+
     model = model_module.getModel()
     solver = model.getSolver()
-    rdata = amici.runAmiciSimulation(model, solver)
+    # TODO
+    edata = amici.ExpData(model)
+    edata.setTimepoints([1e8])
+    edata.setObservedData([1.0])
+
+    if arg == 'forward_simulation':
+        solver.setSensitivityMethod(amici.SensitivityMethod_none)
+        solver.setSensitivityOrder(amici.SensitivityOrder_none)
+    elif arg == 'forward_sensitivities':
+        solver.setSensitivityMethod(amici.SensitivityMethod_forward)
+        solver.setSensitivityOrder(amici.SensitivityOrder_first)
+    elif arg == 'adjoint_sensitivities':
+        solver.setSensitivityMethod(amici.SensitivityMethod_adjoint)
+        solver.setSensitivityOrder(amici.SensitivityOrder_first)
+    else:
+        print("Unknown argument:", arg)
+        sys.exit(1)
+    rdata = amici.runAmiciSimulation(model, solver, edata)
     assert rdata['status'] == amici.AMICI_SUCCESS
 
 
