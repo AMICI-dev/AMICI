7030cd0a0b248d6074185df8cf234789cf191562
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-12-17 19:50:07 +0100

    Why python not found? New CMake on Travis?

--- scripts/buildAmici.sh
+++ scripts/buildAmici.sh
@@ -12,7 +12,8 @@ AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 mkdir -p ${AMICI_PATH}/build
 cd ${AMICI_PATH}/build
 CPPUTEST_BUILD_DIR=${AMICI_PATH}/ThirdParty/cpputest-master/build/
-CppUTest_DIR=${CPPUTEST_BUILD_DIR} ${CMAKE} -DCMAKE_BUILD_TYPE=Debug ..
+CppUTest_DIR=${CPPUTEST_BUILD_DIR} \
+  ${CMAKE} -DCMAKE_BUILD_TYPE=Debug -DPython_EXECUTABLE=$(which python3) ..
 ${MAKE}
 
 ${MAKE} python-sdist
