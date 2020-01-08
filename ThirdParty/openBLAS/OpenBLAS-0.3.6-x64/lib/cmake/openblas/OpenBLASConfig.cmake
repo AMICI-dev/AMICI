656b3565bde7df52824f337adba6b80bf54173db
Thomas S. Ligon <tom@thomassligon.info>
2020-01-08 19:13:11 +0100

    test BLAS
    ...using ThirdParty directory

---
+++ ThirdParty/openBLAS/OpenBLAS-0.3.6-x64/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.6")
+SET(OpenBLAS_INCLUDE_DIRS /home/martin/MXE-neu/OpenBLAS-0.3.6/inst/include)
+SET(OpenBLAS_LIBRARIES /home/martin/MXE-neu/OpenBLAS-0.3.6/inst/bin/libopenblas.dll)
