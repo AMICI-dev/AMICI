7d9fe6deebaa30089d3f57b55d7ec4938035f9fa
Fabian Fröhlich <fabian@schaluck.com>
2020-01-10 22:37:04 -0500

    Python 3.8, fixes #897 (#898)
    * refactor travis, implement testcases for python 3.7 and 3.8
    
    * increment version requirement
    
    * update workflow image
    
    * finish refactor travis
    
    * improve install logic travis
    
    * fix coverage python
    
    * fix travis
    
    * fix travis
    
    * fix lcov
    
    * fix libhdf5
    
    * fix travis
    
    * fix travis
    
    * fix travis
    
    * fix lcov
    
    * remove debug code
    
    * reorganize travis

--- scripts/run-codecov.sh
+++ scripts/run-codecov.sh
@@ -15,7 +15,4 @@ python ./tests/testCoverage.py
 ret=$?
 if [[ $ret != 0 ]]; then exit $ret; fi
 
-
-lcov --compat-libtool --no-external --directory ${AMICI_PATH}/build/CMakeFiles/amici.dir/src --base-directory ${AMICI_PATH} --capture --output-file coverage.info
-
 rm -rf ./test_model_steadystate_scaled
