d16d6126bf76ed8e914fa245deff0f762a227708
Thomas S. Ligon <tom@thomassligon.info>
2020-01-13 20:43:56 +0100

    test BLAS

--- scripts/installOpenBLAS.ps1
+++ scripts/installOpenBLAS.ps1
@@ -1,6 +1,5 @@
 Write-Host 'script installOpenBLAS.ps1 started'
 New-Item -Path 'C:\BLAS' -ItemType Directory -Force
-# New-Item -Path 'C:\BLAS\OpenBLAS-0.3.6-x64' -ItemType Directory -Force
 # Set-Location 'C:\BLAS'
 # Copy-Item -Path "$BASE_DIR\ThirdParty\BLAS\OpenBLAS-0.3.6-x64.zip" -Destination "C:\BLAS" -Force
 $uri = 'https://sourceforge.net/projects/openblas/files/v0.3.6/OpenBLAS-0.3.6-x64.zip/download'
@@ -9,6 +8,6 @@ $output = 'C:\BLAS\OpenBLAS-0.3.6-x64.zip'
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($uri,"$output")
 Expand-Archive -Path 'C:\BLAS\OpenBLAS-0.3.6-x64.zip' -DestinationPath 'C:\BLAS\OpenBLAS-0.3.6-x64' -Force
-New-Item -Path Env:BLAS_LIBS -Value "/LIBPATH:C:\BLAS\OpenBLAS-0.3.6-x64\lib" -Force
+New-Item -Path Env:BLAS_LIBS -Value "/LIBPATH:C:\BLAS\OpenBLAS-0.3.6-x64\lib libopenblas.lib" -Force
 New-Item -Path Env:BLAS_CFLAGS -Value "/IC:\BLAS\OpenBLAS-0.3.6-x64\include" -Force
 Write-Host 'script installOpenBLAS.ps1 completed'
