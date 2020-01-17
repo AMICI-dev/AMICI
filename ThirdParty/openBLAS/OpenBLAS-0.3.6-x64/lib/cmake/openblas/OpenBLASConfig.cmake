f46103f950084933fdedc1a89674a57a6d3a3e27
Thomas S. Ligon <tom@thomassligon.info>
2020-01-17 14:32:33 +0100

    cleanup after BLAS installation

--- ThirdParty/openBLAS/OpenBLAS-0.3.6-x64/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.3.6")
-SET(OpenBLAS_INCLUDE_DIRS /home/martin/MXE-neu/OpenBLAS-0.3.6/inst/include)
-SET(OpenBLAS_LIBRARIES /home/martin/MXE-neu/OpenBLAS-0.3.6/inst/bin/libopenblas.dll)
