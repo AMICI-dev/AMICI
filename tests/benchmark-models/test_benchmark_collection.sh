8ea98334b8fc0a746bfb4bca79caa0ed34123377
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-01-24 16:29:01 +0100

    Use benchmark collection models for CI
    * Add reference values
    * Add script for simulating and comparing to reference
    * Add github action

---
+++ tests/benchmark-models/test_benchmark_collection.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Import and run selected benchmark models with nominal parameters and check
+# agreement with reference values
+#
+# Expects environment variable BENCHMARK_COLLECTION to provide path to
+# benchmark collection model directory
+
+# Confirmed to be working
+models="
+Boehm_JProteomeRes2014
+Borghans_BiophysChem1997
+Elowitz_Nature2000
+Schwen_PONE2014
+Chen_MSB2009
+Fujita_SciSignal2010
+Sneyd_PNAS2002
+Zheng_PNAS2012"
+
+# Not matching reference for unclear reasons
+# Lucarelli_CellSystems2018
+# Weber_BMC2015
+#
+# PEtab needs fixing: Bachmann_MSB2011
+#
+# Unsupported:
+#
+# Becker_Science2010: multiple models
+#
+# no reference value:
+# Alkan_SciSignal2018
+# Beer_MolBioSystems2014
+# Blasi_CellSystems2016
+# Crauste_CellSystems2017
+# Hass_PONE2017
+# Korkut_eLIFE2015
+# Perelson_Science1996
+# Bruno_JExpBio2016
+#
+# Timepoint-specific parameter overrides
+# Fiedler_BMC2016
+# Brannmark_JBC2010
+# Isensee_JCB2018
+# Sobotta_Frontiers2017
+#
+# yaml missing:
+# Casaletto_PNAS2019
+#
+# Model missing:
+# Merkle_PCB2016
+#
+# SBML extensions:
+# Parmar_PCB2019
+#
+# Events:
+# Swameye_PNAS2003
+#
+# state-dependent sigmas:
+# Raia_CancerResearch2011
+
+set -e
+
+script_path=$(dirname "$BASH_SOURCE")
+script_path=$(cd "$script_path" && pwd)
+
+for model in $models; do
+    printf '=%.0s' {1..40}
+    printf "   %s   " "${model}"
+    printf '=%.0s' {1..40}
+    echo
+    yaml="${BENCHMARK_COLLECTION}"/"${model}"/"${model}".yaml
+    amici_model_dir=test_bmc/"${model}"
+    mkdir -p "$amici_model_dir"
+    amici_import_petab --verbose -y "${yaml}" -o "${amici_model_dir}" -n "${model}"
+    "$script_path"/test_petab_model.py --verbose -y "${yaml}" -d "${amici_model_dir}" -m "${model}" -c
+    printf '=%.0s' {1..100}
+    echo
+    echo
+done
+
---
+++ tests/benchmark-models/test_petab_model.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+"""
+Simulate a PEtab problem and compare results to reference values
+"""
+
+import argparse
+import importlib
+import logging
+import os
+import sys
+
+import petab
+import yaml
+from amici.logging import get_logger
+from amici.petab_objective import simulate_petab, rdatas_to_measurement_df
+from petab.visualize import plot_petab_problem
+
+logger = get_logger(f"amici.{__name__}", logging.WARNING)
+
+def parse_cli_args():
+    """Parse command line arguments
+
+    Returns:
+        Parsed CLI arguments from ``argparse``.
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Simulate PEtab-format model using AMICI.')
+
+    # General options:
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='More verbose output')
+    parser.add_argument('-c', '--check', dest='check', action='store_true',
+                        help='Compare to reference value')
+    parser.add_argument('-p', '--plot', dest='plot', action='store_true',
+                        help='Plot measurement and simulation results')
+
+    # PEtab problem
+    parser.add_argument('-y', '--yaml', dest='yaml_file_name',
+                        required=True,
+                        help='PEtab YAML problem filename')
+
+    # Corresponding AMICI model
+    parser.add_argument('-m', '--model-name', dest='model_name',
+                        help='Name of the AMICI module of the model to '
+                             'simulate.', required=True)
+    parser.add_argument('-d', '--model-dir', dest='model_directory',
+                        help='Directory containing the AMICI module of the '
+                             'model to simulate. Required if model is not '
+                             'in python path.')
+
+    parser.add_argument('-o', '--simulation-file', dest='simulation_file',
+                        help='File to write simulation result to, in PEtab'
+                        'measurement table format.')
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    """Simulate the model specified on the command line"""
+
+
+    args = parse_cli_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    logger.info(f"Simulating '{args.model_name}' "
+                f"({args.model_directory}) using PEtab data from "
+                f"{args.yaml_file_name}")
+
+    # load PEtab files
+    problem = petab.Problem.from_yaml(args.yaml_file_name)
+
+    # load model
+    if args.model_directory:
+        sys.path.insert(0, args.model_directory)
+    model_module = importlib.import_module(args.model_name)
+    amici_model = model_module.getModel()
+
+    llh, rdatas = simulate_petab(petab_problem=problem,
+                                 amici_model=amici_model,
+                                 log_level=logging.DEBUG)
+
+    # create simulation PEtab table
+    sim_df = rdatas_to_measurement_df(rdatas=rdatas, model=amici_model,
+                                      measurement_df=problem.measurement_df)
+    sim_df.rename(columns={petab.MEASUREMENT: petab.SIMULATION}, inplace=True)
+
+    if args.simulation_file:
+        sim_df.to_csv(index=False, sep="\t")
+
+    if args.plot:
+        try:
+            # visualize fit
+            ax = plot_petab_problem(petab_problem=problem, sim_data=sim_df)
+
+            # save figure
+            fig_path = os.path.join(args.model_directory,
+                                    args.model_name + "_vis.png")
+            logger.info(f"Saving figure to {fig_path}")
+            ax.flatten()[0].get_figure().savefig(fig_path, dpi=150)
+
+        except NotImplementedError:
+            pass
+
+    if args.check:
+        references_yaml = os.path.join(os.path.dirname(__file__),
+                                       "benchmark_models.yaml")
+        with open(references_yaml) as f:
+            refs = yaml.full_load(f)
+
+        try:
+            ref_llh = refs[args.model_name]["llh"]
+            logger.info(f"Reference llh: {ref_llh}")
+
+            if abs(ref_llh - llh) < 1e-3:
+                logger.info(f"Computed llh {llh} matches reference "
+                            f"{ref_llh}. Absolute difference is "
+                            f"{ref_llh - llh}.")
+            else:
+                logger.error(f"Computed llh {llh} does not match reference "
+                             f"{ref_llh}. Absolute difference is "
+                             f"{ref_llh - llh}."
+                             f" Relative difference is {llh / ref_llh}")
+                sys.exit(1)
+        except KeyError:
+            logger.error("No reference likelihood found for "
+                         f"{args.model_name} in {references_yaml}")
+
+
+if __name__ == "__main__":
+    main()
