0b2d6e9f75912f8f767935d519a4e161ac09abba
Daniel Weindl <dweindl@users.noreply.github.com>
2020-01-25 18:37:21 +0100

    Update python/amici/petab_import.py
    Co-Authored-By: Yannik Schälte <31767307+yannikschaelte@users.noreply.github.com>

--- python/amici/petab_import.py
+++ python/amici/petab_import.py
@@ -269,7 +269,7 @@ def import_model(sbml_model: Union[str, 'libsbml.Model'],
     sbml_model = sbml_importer.sbml
 
     logger.info(f"Model name is '{model_name}'. "
-                f"Writing model code to '{model_output_dir}'")
+                f"Writing model code to '{model_output_dir}'.")
     show_model_info(sbml_model)
 
     # Read PEtab observables and sigmas
