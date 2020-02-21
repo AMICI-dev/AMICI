AMICI-dev/AMICI

commit 971995457ab3c7ef8c61580c945dc0737fb16f31

More informative errors on SWIG interface import failures (#959)
* More informative errors on SWIG interface import failures

* consider conda hdf5 includes

python/amici/__init__.py
@@ -77,17 +77,32 @@ hdf5_enabled = False
 has_clibs = False
 
 try:
+    # Try importing AMICI SWIG-interface with HDF5 enabled
     from . import amici
     from .amici import *
     hdf5_enabled = True
     has_clibs = True
-except (ImportError, ModuleNotFoundError, AttributeError):  # pragma: no cover
-    try:
-        from . import amici_without_hdf5 as amici
-        from .amici_without_hdf5 import *
-        has_clibs = True
-    except (ImportError, ModuleNotFoundError, AttributeError):
-        pass
+except ImportError as e:
+    # No such module exists or there are some dynamic linking problems
+    if "cannot import name" in str(e):
+        # No such module exists,
+        # try importing AMICI SWIG-interface without HDF5
+        try:
+            from . import amici_without_hdf5 as amici
+            from .amici_without_hdf5 import *
+            has_clibs = True
+        except ImportError as e:
+            if "cannot import name" in str(e):
+                # No such module exists
+                # this probably means, the model was imported during setuptools
+                # `setup` or after an installation with `--no-clibs`.
+                pass
+        else:
+            # Probably some linking problem that we don't want to hide
+            raise e
+    else:
+        # Probably some linking problem that we don't want to hide
+        raise e
 
 # Initialize AMICI paths
 amici_path = _get_amici_path()

python/amici/petab_import.py
@@ -220,7 +220,7 @@ def import_petab_problem(
         model_output_dir: str = None,
         model_name: str = None,
         force_compile: bool = False,
-        **kwargs) -> amici.Model:
+        **kwargs) -> 'amici.Model':
     """
     Import model from petab problem.
 

python/amici/setuptools.py
@@ -134,6 +134,15 @@ def getHdf5Config():
         hdf5_library_dir_hints.insert(
             0, os.path.join(os.environ['HDF5_BASE'], 'lib'))
 
+    # special treatment for conda environments
+    # as the conda library dir is provided first, we should also check for
+    # conda header files first
+    if 'CONDA_DIR' in os.environ:
+        hdf5_include_dir_hints.insert(
+            0, os.path.join(os.environ['CONDA_DIR'], 'include'))
+        hdf5_library_dir_hints.insert(
+            0, os.path.join(os.environ['CONDA_DIR'], 'lib'))
+
     for hdf5_include_dir_hint in hdf5_include_dir_hints:
         hdf5_include_dir_found = os.path.isfile(
             os.path.join(hdf5_include_dir_hint, 'hdf5.h'))

scripts/buildSundials.sh
@@ -4,6 +4,7 @@
 #
 set -e
 
+# shellcheck disable=SC2128
 SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd "$SCRIPT_PATH"/.. && pwd)
 

commit 59aacea4643d2dcbaeaaf03c60d50c859d54ae9b

Handle ModuleNotFound problem introduced in #959

python/amici/__init__.py
@@ -82,6 +82,9 @@ try:
     from .amici import *
     hdf5_enabled = True
     has_clibs = True
+except ModuleNotFoundError:
+    # import from setuptools or installation with `--no-clibs`
+    pass
 except ImportError as e:
     # No such module exists or there are some dynamic linking problems
     if "cannot import name" in str(e):
@@ -91,6 +94,9 @@ except ImportError as e:
             from . import amici_without_hdf5 as amici
             from .amici_without_hdf5 import *
             has_clibs = True
+        except ModuleNotFoundError:
+            # import from setuptools or installation with `--no-clibs`
+            pass
         except ImportError as e:
             if "cannot import name" in str(e):
                 # No such module exists