AMICI-dev/AMICI

commit 971995457ab3c7ef8c61580c945dc0737fb16f31

More informative errors on SWIG interface import failures (#959)
* More informative errors on SWIG interface import failures

* consider conda hdf5 includes

python/amici/__init__.py
@@ -77,17 +77,32 @@ hdf5_enabled = False
 has_clibs = False
 
 try:
+    # Try importing AMICI SWIG-interface with HDF5 enabled
     from . import amici
     from .amici import *
     hdf5_enabled = True
     has_clibs = True
-except (ImportError, ModuleNotFoundError, AttributeError):  # pragma: no cover
-    try:
-        from . import amici_without_hdf5 as amici
-        from .amici_without_hdf5 import *
-        has_clibs = True
-    except (ImportError, ModuleNotFoundError, AttributeError):
-        pass
+except ImportError as e:
+    # No such module exists or there are some dynamic linking problems
+    if "cannot import name" in str(e):
+        # No such module exists,
+        # try importing AMICI SWIG-interface without HDF5
+        try:
+            from . import amici_without_hdf5 as amici
+            from .amici_without_hdf5 import *
+            has_clibs = True
+        except ImportError as e:
+            if "cannot import name" in str(e):
+                # No such module exists
+                # this probably means, the model was imported during setuptools
+                # `setup` or after an installation with `--no-clibs`.
+                pass
+        else:
+            # Probably some linking problem that we don't want to hide
+            raise e
+    else:
+        # Probably some linking problem that we don't want to hide
+        raise e
 
 # Initialize AMICI paths
 amici_path = _get_amici_path()

python/amici/petab_import.py
@@ -220,7 +220,7 @@ def import_petab_problem(
         model_output_dir: str = None,
         model_name: str = None,
         force_compile: bool = False,
-        **kwargs) -> amici.Model:
+        **kwargs) -> 'amici.Model':
     """
     Import model from petab problem.
 

python/amici/setuptools.py
@@ -134,6 +134,15 @@ def getHdf5Config():
         hdf5_library_dir_hints.insert(
             0, os.path.join(os.environ['HDF5_BASE'], 'lib'))
 
+    # special treatment for conda environments
+    # as the conda library dir is provided first, we should also check for
+    # conda header files first
+    if 'CONDA_DIR' in os.environ:
+        hdf5_include_dir_hints.insert(
+            0, os.path.join(os.environ['CONDA_DIR'], 'include'))
+        hdf5_library_dir_hints.insert(
+            0, os.path.join(os.environ['CONDA_DIR'], 'lib'))
+
     for hdf5_include_dir_hint in hdf5_include_dir_hints:
         hdf5_include_dir_found = os.path.isfile(
             os.path.join(hdf5_include_dir_hint, 'hdf5.h'))

scripts/buildSundials.sh
@@ -4,6 +4,7 @@
 #
 set -e
 
+# shellcheck disable=SC2128
 SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd "$SCRIPT_PATH"/.. && pwd)
 

commit d0ec790e7a27d55b89156ba646243a0634e7a6f4

Fix CI (#969)
* Handle ModuleNotFound problem introduced in #959

* Keep PEtab out of TravisCI to reduce runtimes; test via benchmark collection on github

* Run petab-related unittests on github

* enable action for branch

* swig import handling

* Fix indent

python/amici/__init__.py
@@ -82,24 +82,32 @@ try:
     from .amici import *
     hdf5_enabled = True
     has_clibs = True
-except ImportError as e:
+except ModuleNotFoundError:
+    # import from setuptools or installation with `--no-clibs`
+    pass
+except (ImportError, AttributeError) as e:
     # No such module exists or there are some dynamic linking problems
-    if "cannot import name" in str(e):
-        # No such module exists,
-        # try importing AMICI SWIG-interface without HDF5
+    if isinstance(e, AttributeError) or "cannot import name" in str(e):
+        # No such module exists (ImportError),
+        #  or python tries to import a HDF5 function from the non-hdf5
+        #  swig interface (AttributeError):
+        #  try importing AMICI SWIG-interface without HDF5
         try:
             from . import amici_without_hdf5 as amici
             from .amici_without_hdf5 import *
             has_clibs = True
+        except ModuleNotFoundError:
+            # import from setuptools or installation with `--no-clibs`
+            pass
         except ImportError as e:
             if "cannot import name" in str(e):
                 # No such module exists
                 # this probably means, the model was imported during setuptools
                 # `setup` or after an installation with `--no-clibs`.
                 pass
-        else:
-            # Probably some linking problem that we don't want to hide
-            raise e
+            else:
+                # Probably some linking problem that we don't want to hide
+                raise e
     else:
         # Probably some linking problem that we don't want to hide
         raise e

python/sdist/setup.py
@@ -198,7 +198,7 @@ def main():
         setup_requires=['setuptools>=40.6.3'],
         python_requires='>=3.6',
         extras_require={
-            'petab': ['petab>=0.1.1']
+            'petab': ['petab>=0.1.2']
         },
         package_data={
             'amici': ['amici/include/amici/*',

scripts/installAmiciSource.sh
@@ -30,5 +30,5 @@ fi
 # install wheel separately to prevent build_wheel fail in next step
 pip install --upgrade wheel
 pip install --upgrade pip setuptools pkgconfig scipy matplotlib pysb coverage
-pip install --verbose -e ${AMICI_PATH}/python/sdist[petab]
+pip install --verbose -e ${AMICI_PATH}/python/sdist
 deactivate

.github/workflows/test-benchmark-collection-models.yml
@@ -4,6 +4,7 @@ on:
     branches:
       - develop
       - master
+      - fix_ci
 
   pull_request:
     branches:
@@ -28,7 +29,7 @@ jobs:
       run: |
         pip3 install --upgrade --user wheel \
           && pip3 install --upgrade --user setuptools
-    - run: pip3 install shyaml petab>=0.1.1
+    - run: pip3 install pytest shyaml petab>=0.1.2
     - run: |
         echo ::add-path::${HOME}/.local/bin/
         echo ::add-path::${GITHUB_WORKSPACE}/tests/performance/
@@ -42,6 +43,10 @@ jobs:
         AMICI_PARALLEL_COMPILE=2 check_time.sh \
             install_sdist pip3 install -v --user \
             $(ls -t python/sdist/dist/amici-*.tar.gz | head -1)
+
+    - name: Run PEtab-related unit tests
+      run: |
+        pytest tests/testMisc.py
 
     # retrieve test models
     - name: Download and test benchmark collection

scripts/run-python-tests.sh
@@ -20,5 +20,3 @@ python testCPP.py
 python testPreequilibration.py
 python testMisc.py
 python testPandas.py
-
-./test-petab-import.sh

tests/test-petab-import.sh
@@ -1,20 +1,0 @@
-#!/bin/bash
-SCRIPT_PATH=$(dirname $BASH_SOURCE)
-AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
-
-# avtivate virtual environment
-source ${AMICI_PATH}/build/venv/bin/activate
-
-# test PEtab command line import
-MODEL_NAME="deleteme"
-MODEL_OUTPUT_DIR="${MODEL_NAME}"
-SBML_FILE_NAME="https://raw.githubusercontent.com/LeonardSchmiester/Benchmark-Models/hackathon/hackathon_contributions_new_data_format/Zheng_PNAS2012/model_Zheng_PNAS2012.xml"
-MEASUREMENT_FILE_NAME="https://raw.githubusercontent.com/LeonardSchmiester/Benchmark-Models/hackathon/hackathon_contributions_new_data_format/Zheng_PNAS2012/measurementData_Zheng_PNAS2012.tsv"
-CONDITION_FILE_NAME="https://raw.githubusercontent.com/LeonardSchmiester/Benchmark-Models/hackathon/hackathon_contributions_new_data_format/Zheng_PNAS2012/experimentalCondition_Zheng_PNAS2012.tsv"
-PARAMETER_FILE_NAME="https://raw.githubusercontent.com/LeonardSchmiester/Benchmark-Models/hackathon/hackathon_contributions_new_data_format/Zheng_PNAS2012/parameters_Zheng_PNAS2012.tsv"
-SBML_FILE_NAME2="deleteme_model.xml"
-curl "${SBML_FILE_NAME}" > "${SBML_FILE_NAME2}"
-amici_import_petab.py -o "${MODEL_OUTPUT_DIR}" --no-compile \
-  -s "${SBML_FILE_NAME2}" -m "${MEASUREMENT_FILE_NAME}" \
-  -c "${CONDITION_FILE_NAME}" -p "${PARAMETER_FILE_NAME}" -n "${MODEL_NAME}"
-rm -rf "${MODEL_OUTPUT_DIR}"

tests/testMisc.py
@@ -11,8 +11,6 @@ import sympy as sp
 import libsbml
 from tempfile import TemporaryDirectory
 
-from amici.petab_import import constant_species_to_parameters
-
 
 class TestAmiciMisc(unittest.TestCase):
     """TestCase class various AMICI Python interface functions"""
@@ -140,13 +138,18 @@ class TestAmiciMisc(unittest.TestCase):
             species_ref = r.createModifier()
             species_ref.setSpecies(name)
 
-        constant_species_to_parameters(model)
+        try:
+            from amici.petab_import import constant_species_to_parameters
 
-        assert len(list(model.getListOfParameters())) == 1
-        assert len(list(model.getListOfSpecies())) == 0
-        assert len(list(r.getListOfReactants())) == 0
-        assert len(list(r.getListOfProducts())) == 0
-        assert len(list(r.getListOfModifiers())) == 0
+            constant_species_to_parameters(model)
+
+            assert len(list(model.getListOfParameters())) == 1
+            assert len(list(model.getListOfSpecies())) == 0
+            assert len(list(r.getListOfReactants())) == 0
+            assert len(list(r.getListOfProducts())) == 0
+            assert len(list(r.getListOfModifiers())) == 0
+        except ModuleNotFoundError:
+            pass
 
     def test_hill_function_dwdx(self):
         """Kinetic laws with Hill functions, may lead to NaNs in the Jacobian