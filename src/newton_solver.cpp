9307045437d370952e93271aa19fb002c596b1de
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2020-05-27 00:51:06 +0200

    fixes #1089, #1090 (#1097)
    * enforce minimum number for numlinsteps field in newtonsolver and steadystateproblem
    
    * add member maxsteps to steadystateproblem, resize numlinsteps according to linear solver type
    
    * Newton solver does not hat getLinearSolver...
    
    * no double zeros when using ints
    
    * fix #1090
    
    * also with iterative solver amx Newton steps might still be zero
    
    * fix out of bounds when writing numlinsteps
    
    * always use maxSteps, as this should always have the correct size for numlinsteps
    
    * setting numlinsetps to at least 1 conflicts with rdata
    
    * add error code for singular jacobian
    
    * fix sympy to version 1.5.1 for the moment
    
    * address steadystateproblem,cpp sonarcloud
    
    * unpin sympy
    
    Co-authored-by: Fabian Fröhlich <fabian@schaluck.com>

--- src/newton_solver.cpp
+++ src/newton_solver.cpp
@@ -75,7 +75,8 @@ std::unique_ptr<NewtonSolver> NewtonSolver::getSolver(realtype *t, AmiVector *x,
     solver->dampingFactorMode = simulationSolver.getNewtonDampingFactorMode();
     solver->dampingFactorLowerBound =
         simulationSolver.getNewtonDampingFactorLowerBound();
-    solver->numlinsteps.resize(simulationSolver.getNewtonMaxSteps(), 0.0);
+    if (simulationSolver.getLinearSolver() == LinearSolver::SPBCG)
+        solver->numlinsteps.resize(simulationSolver.getNewtonMaxSteps(), 0);
 
     return solver;
 }
