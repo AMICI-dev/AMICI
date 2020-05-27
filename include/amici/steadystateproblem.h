ed726f9b5e2bcde3a75de9cc66b5d63597b4dafc
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2020-05-27 08:23:12 +0200

    delete double statement

--- include/amici/steadystateproblem.h
+++ include/amici/steadystateproblem.h
@@ -239,9 +239,6 @@ class SteadystateProblem {
     /** maximum number of steps for Newton solver for allocating numlinsteps */
     int maxSteps = 0;
 
-    /** maximum number of steps for Newton solver for allocating numlinsteps */
-    int maxSteps = 0;
-
     /** weighted root-mean-square error */
     realtype wrms = NAN;
 
