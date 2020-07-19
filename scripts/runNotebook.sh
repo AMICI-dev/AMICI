c5d2bb0d8b443cbbbf7d0244febba371813f97d2
FFroehlich <fabian@schaluck.com>
2020-07-19 19:52:28 -0400

    fix python matrix

--- scripts/runNotebook.sh
+++ scripts/runNotebook.sh
@@ -10,11 +10,7 @@ AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 runNotebook () {
     set +e
     tempfile=$(mktemp)
-    if [ -f "${AMICI_PATH}build/venv/bin/activate"]; then
-        jupyter nbconvert --debug --stdout --execute --ExecutePreprocessor.timeout=300 --to markdown $@ &> $tempfile
-    else
-        python3 -m jupyter nbconvert --debug --stdout --execute --ExecutePreprocessor.timeout=300 --to markdown $@ &> $tempfile
-    fi
+    jupyter nbconvert --debug --stdout --execute --ExecutePreprocessor.timeout=300 --to markdown $@ &> $tempfile
     ret=$?
     if [[ $ret != 0 ]]; then
       cat $tempfile
@@ -29,8 +25,8 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-source ${AMICI_PATH}/build/venv/bin/activate || echo "${AMICI_PATH}build/venv/bin/activate not available, running without virtualenv"
-pip3 show ipython || (pip3 install --upgrade jupyter jupyter_contrib_nbextensions nbconvert && python3 -m ipykernel install --user --name amici --display-name "Python (amici)")
+source ${AMICI_PATH}/build/venv/bin/activate
+pip3 show ipython || (pip3 install --upgrade jupyter jupyter_contrib_nbextensions && python3 -m ipykernel install --user --name amici --display-name "Python (amici)")
 
 for arg in "$@"; do
     if [ -d $arg ]; then
