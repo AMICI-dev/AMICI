98b0fb5273434749ab10bdc3fc9d49bd04c8c629
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-08-06 12:20:47 +0200

    md

--- python/amici/ode_export.py
+++ python/amici/ode_export.py
@@ -2255,6 +2255,7 @@ class ODEExporter:
             '#include "amici/symbolic_functions.h"',
             '#include "amici/defines.h"',
             '#include "sundials/sundials_types.h"',
+            '#define _USE_MATH_DEFINES'
             '#include <cmath>',
         ]
 
