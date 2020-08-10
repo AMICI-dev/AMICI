42aaffbad2597dc6ffa9c057390644b51eae83b9
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-08-10 17:05:48 +0200

    ..

--- documentation/conf.py
+++ documentation/conf.py
@@ -86,6 +86,7 @@ autodoc_mock_imports = ['_amici', 'amici._amici']
 for mod_name in autodoc_mock_imports:
     sys.modules[mod_name] = mock.MagicMock()
 
+amici.ExpData = amici.amici.ExpData
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
