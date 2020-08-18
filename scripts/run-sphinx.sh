e823adfdaa09efbd48228d3e8253da2b01bd4245
FFroehlich <fabian@schaluck.com>
2020-08-18 18:21:54 -0400

    Update run-sphinx.sh

--- scripts/run-sphinx.sh
+++ scripts/run-sphinx.sh
@@ -5,11 +5,11 @@ SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 
 cd ${AMICI_PATH}/documentation
-python3 -mvirtualenv ${AMICI_PATH}/doc-venv --clear
+python3 -m venv ${AMICI_PATH}/doc-venv --clear
 source ${AMICI_PATH}/doc-venv/bin/activate
 python -m pip install --upgrade --no-cache-dir pip
 python -m pip install --upgrade --no-cache-dir Pygments==2.3.1 setuptools==41.0.1 docutils==0.14 mock==1.0.1 pillow==5.4.1 alabaster>=0.7,<0.8,!=0.7.5 commonmark==0.8.1 recommonmark==0.5.0 sphinx<2 sphinx-rtd-theme<0.5 readthedocs-sphinx-ext<1.1
-python -m pip install --exists-action=w --no-cache-dir -r documentation/rtd_requirements.txt
+python -m pip install --exists-action=w --no-cache-dir -r ${AMICI_PATH}/documentation/rtd_requirements.txt
 
 sphinx-build -T -E -W --keep-going -b readthedocs -d _build/doctrees-readthedocs -D language=en . _build/html
 ret=$?
