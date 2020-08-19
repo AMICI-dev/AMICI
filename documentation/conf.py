496483049b3d441a54ffeb4885ab353a992a1a3e
FFroehlich <fabian@schaluck.com>
2020-08-19 11:31:15 -0400

    ignore generated?

--- documentation/conf.py
+++ documentation/conf.py
@@ -139,7 +139,8 @@ language = None
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
-                    '**.ipynb_checkpoints', 'numpy.py', 'MATLAB.md', 'gfx']
+                    '**.ipynb_checkpoints', 'numpy.py', 'MATLAB.md', 'gfx',
+                    'generated']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
