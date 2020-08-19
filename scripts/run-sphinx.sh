d2c866ad3422779964276691a8584edb1c0dad29
FFroehlich <fabian@schaluck.com>
2020-08-19 14:27:16 -0400

    fix sphinx path GHA, change exclude patterns

--- scripts/run-sphinx.sh
+++ scripts/run-sphinx.sh
@@ -4,13 +4,15 @@
 SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 
-cd ${AMICI_PATH}/documentation
+cd ${AMICI_PATH}
 python3 -m venv ${AMICI_PATH}/doc-venv --clear
 source ${AMICI_PATH}/doc-venv/bin/activate
 python -m pip install --upgrade --no-cache-dir pip
-python -m pip install --upgrade --no-cache-dir Pygments==2.3.1 setuptools==41.0.1 docutils==0.14 mock==1.0.1 pillow==5.4.1 alabaster>=0.7,<0.8,!=0.7.5 commonmark==0.8.1 recommonmark==0.5.0 sphinx<2 sphinx-rtd-theme<0.5 readthedocs-sphinx-ext<1.1
+python -m pip install --upgrade --no-cache-dir Pygments==2.3.1
+setuptools==41.0.1 docutils==0.14 mock==1.0.1 pillow==5.4.1 alabaster==0.7.12  commonmark==0.8.1 recommonmark==0.5.0 sphinx<2 sphinx-rtd-theme<0.5 readthedocs-sphinx-ext<1.1
 python -m pip install --exists-action=w --no-cache-dir -r ${AMICI_PATH}/documentation/rtd_requirements.txt
 
-sphinx-build -T -E -W --keep-going -b html -d _build/doctrees-readthedocs -D language=en . _build/html
+rm -rf ${AMICI_PATH}/documentation/generated
+sphinx-build -T -E -W --keep-going -b html -d _build/doctrees-readthedocs -D language=en -c documentation . _build/html
 ret=$?
 if [[ $ret != 0 ]]; then exit $ret; fi
