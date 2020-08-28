e64e3a97f0036dc54df24ab06f804aac50412f68
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-08-28 10:06:25 +0200

    Refactor run-doxygen.sh: extract mtocpp provisioning to separate script for reusability

--- scripts/run-doxygen.sh
+++ scripts/run-doxygen.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
-# generate code documentation via doxygen
+# Generate code documentation via doxygen
 set -euo pipefail
 set -x
 
 SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 
-# build mtocpp
-cd ${AMICI_PATH}/ThirdParty
-if [ ! -d "mtocpp-master" ]; then
-    if [ ! -e "mtocpp-master.zip" ]; then
-        wget -O mtocpp-master.zip https://github.com/mdrohmann/mtocpp/archive/master.zip
-    fi
-    unzip mtocpp-master.zip
-    mkdir ./mtocpp-master/build
-    cd ./mtocpp-master/build && cmake .. && make mtocpp mtocpp_post
-    if [ $? -ne 0 ] ; then
-        exit 1
-    fi
-fi
-
+# Set up Matlab filter
 cd ${AMICI_PATH}
+scripts/downloadAndBuildMtocpp.sh
 MTOC_CONFIG_PATH=${AMICI_PATH}/matlab/mtoc/config
-# generate filter
-echo "$AMICI_PATH/ThirdParty/mtocpp-master/build/mtocpp \$1 ${MTOC_CONFIG_PATH}/mtocpp.conf" > ${MTOC_CONFIG_PATH}/mtocpp_filter.sh
 
-chmod +x ${MTOC_CONFIG_PATH}/mtocpp_filter.sh
-
-# generate doxyfile
-DOXYFILE=${MTOC_CONFIG_PATH}/Doxyfile
-cp ${MTOC_CONFIG_PATH}/Doxyfile.template ${DOXYFILE}
+# Generate doxyfile
+DOXYFILE="${MTOC_CONFIG_PATH}/Doxyfile"
+cp "${MTOC_CONFIG_PATH}/Doxyfile.template" "${DOXYFILE}"
 DOXY_WARNING_FILE=${AMICI_PATH}/matlab/mtoc/warnings.log
 
 # Replace some template values
@@ -47,43 +31,41 @@ sed -i -e "s#_HaveDot_#YES#g" ${DOXYFILE}
 # Fail if no replace was made
 sed -i -re "/WARN_LOGFILE(\s*)=.*/{s##WARN_LOGFILE\1= ${DOXY_WARNING_FILE}#g;h};\${x;/./{x;q0};x;q1}" ${DOXYFILE}
 
-# generate latexextras
-
+# Generate latexextras
 cp ${MTOC_CONFIG_PATH}/latexextras.template ${MTOC_CONFIG_PATH}/latexextras.sty
 sed -i -e "s#_ConfDir_#${MTOC_CONFIG_PATH}#g" ${MTOC_CONFIG_PATH}/latexextras.sty
-
 export PATH=/Library/TeX/texbin:$PATH
 
+# Run doxygen
 doxygen "${DOXYFILE}"
 
 #cleanup
 #rm ${AMICI_PATH}/mtoc/config/latexextras.sty
-rm ${DOXYFILE}
-rm ${MTOC_CONFIG_PATH}/mtocpp_filter.sh
+rm "${DOXYFILE}"
+rm "${MTOC_CONFIG_PATH}/mtocpp_filter.sh"
 
-cd ${AMICI_PATH}/doc/latex
-
+# Build pdf
+cd "${AMICI_PATH}/doc/latex"
 make
-
-cp ./refman.pdf ${AMICI_PATH}/AMICI_guide.pdf
+cp ./refman.pdf "${AMICI_PATH}/AMICI_guide.pdf"
 
 # suppress doxygen warnings about status badges
-grep -v "warning: Unexpected html tag <img> found within <a href=...> context" ${DOXY_WARNING_FILE} > ${DOXY_WARNING_FILE}_tmp || [[ $? == 1 ]]
-mv ${DOXY_WARNING_FILE}_tmp ${DOXY_WARNING_FILE}
+grep -v "warning: Unexpected html tag <img> found within <a href=...> context" "${DOXY_WARNING_FILE}" > "${DOXY_WARNING_FILE}_tmp" || [[ $? == 1 ]]
+mv "${DOXY_WARNING_FILE}_tmp" "${DOXY_WARNING_FILE}"
 
 # suppress doxygen warning about unresolved external links (problem unclear)
-grep -v "warning: unable to resolve reference to \`https" ${DOXY_WARNING_FILE} > ${DOXY_WARNING_FILE}_tmp || [[ $? == 1 ]]
-mv ${DOXY_WARNING_FILE}_tmp ${DOXY_WARNING_FILE}
+grep -v "warning: unable to resolve reference to \`https" "${DOXY_WARNING_FILE}" > "${DOXY_WARNING_FILE}_tmp" || [[ $? == 1 ]]
+mv "${DOXY_WARNING_FILE}_tmp" "${DOXY_WARNING_FILE}"
 
-grep -v "error: Problem running ghostscript gs -q -g146x60 -r384x384x -sDEVICE=ppmraw -sOutputFile=_form0.pnm -dNOPAUSE -dBATCH -- _form0.ps. Check your installation!" ${DOXY_WARNING_FILE} > ${DOXY_WARNING_FILE}_tmp || [[ $? == 1 ]]
-mv ${DOXY_WARNING_FILE}_tmp ${DOXY_WARNING_FILE}
+grep -v "error: Problem running ghostscript gs -q -g146x60 -r384x384x -sDEVICE=ppmraw -sOutputFile=_form0.pnm -dNOPAUSE -dBATCH -- _form0.ps. Check your installation!" "${DOXY_WARNING_FILE}" > "${DOXY_WARNING_FILE}_tmp" || [[ $? == 1 ]]
+mv "${DOXY_WARNING_FILE}_tmp" "${DOXY_WARNING_FILE}"
 
 # check if warnings log was created
-if [ -f ${DOXY_WARNING_FILE}  ]; then
+if [ -f "${DOXY_WARNING_FILE}"  ]; then
     # check if warnings log is empty
-    if [ -s ${DOXY_WARNING_FILE} ]; then
+    if [ -s "${DOXY_WARNING_FILE}" ]; then
         echo "DOXYGEN failed:"
-        cat ${DOXY_WARNING_FILE}
+        cat "${DOXY_WARNING_FILE}"
         #rm ${DOXY_WARNING_FILE}
         exit 1
     else
