9becd5668ecb32d0b3848e17bcb51cc8140a211c
paulstapor <paul.stapor@helmholtz-muenchen.de>
2020-09-08 11:04:47 +0200

    actually fnished the new boundary condition and extrapolation implementation except for natural BC and debugging is still missing

--- src/splinefunctions.cpp
+++ src/splinefunctions.cpp
@@ -191,10 +191,10 @@ void HermiteSpline::computeCoefficients() {
 void HermiteSpline::computeCoefficientsExtrapolation() {
     /* Do we want to extrapolate at all? */
     bool needExtrapolationCoefficients =
-        (firstNodeEP_> SplineExtrapolation::constant ||
-         firstNodeEP_> SplineExtrapolation::linear) &&
-        (lastNodeEP_> SplineExtrapolation::constant ||
-         lastNodeEP_> SplineExtrapolation::linear);
+        (firstNodeEP_ == SplineExtrapolation::constant ||
+         firstNodeEP_ == SplineExtrapolation::linear) &&
+        (lastNodeEP_ == SplineExtrapolation::constant ||
+         lastNodeEP_ == SplineExtrapolation::linear);
     if (!needExtrapolationCoefficients)
         return;
 
@@ -284,6 +284,8 @@ void HermiteSpline::computeCoefficientsSensi(int nplist, int spline_offset,
     }
 
     /* We need the coefficients for extrapolating beyond the spline domain */
+    computeCoefficientsExtrapolationSensi(nplist, spline_offset,
+                                          dspline_valuesdp, dspline_slopesdp);
 }
 
 void HermiteSpline::computeCoefficientsExtrapolationSensi(int nplist,
@@ -291,10 +293,10 @@ void HermiteSpline::computeCoefficientsExtrapolationSensi(int nplist,
 
     /* Do we want to extrapolate at all? */
     bool needExtrapolationCoefficients =
-        (firstNodeEP_> SplineExtrapolation::constant ||
-         firstNodeEP_> SplineExtrapolation::linear) &&
-        (lastNodeEP_> SplineExtrapolation::constant ||
-         lastNodeEP_> SplineExtrapolation::linear);
+        (firstNodeEP_ == SplineExtrapolation::constant ||
+         firstNodeEP_ == SplineExtrapolation::linear) &&
+        (lastNodeEP_ == SplineExtrapolation::constant ||
+         lastNodeEP_ == SplineExtrapolation::linear);
     if (!needExtrapolationCoefficients)
         return;
 
@@ -305,63 +307,110 @@ void HermiteSpline::computeCoefficientsExtrapolationSensi(int nplist,
      * ] */
     coefficients_extrapolate_sensi.resize(4 * nplist, 0.0);
 
-    switch (firstNodeEP_) {
-        case SplineExtrapolation::constant:
-            coefficients_extrapolate[0] = node_values_[0];
-            coefficients_extrapolate[1] = 0;
-            break;
-
-        case SplineExtrapolation::linear:
-            coefficients_extrapolate[0] = node_values_[0]
-                - nodes_[0] * node_values_derivative_[0];
-            coefficients_extrapolate[1] = node_values_derivative_[0];
-            break;
-
-        default:
-            /* We don't need specific coefficients in the cases of:
-             * noExtrapolation, polynomial, periodic*/
-            break;
-    }
-
+    realtype sp0;
+    realtype sm0;
     for (int ip = 0; ip < nplist; ip++) {
-        /* Before node[0] */
-        realtype sp0 = dspline_valuesdp[spline_offset + ip];
-        realtype sm0;
-        if (get_node_derivative_by_FD()) {
-            if (firstNodeDerivative == SplineExtrapolation::constant) {
+        sp0 = dspline_valuesdp[spline_offset + ip];
+        switch (firstNodeEP_) {
+            /* This whole switch-case-if-else-if-thing could be moved
+             * outside the loop, I know. Yet, it's at most some thousand
+             * if's done once in the program for saving many lines of code
+             * and getting a much clearer code structure. */
+            case SplineExtrapolation::constant:
                 sm0 = 0;
-            } else if (firstNodeDerivative == SplineExtrapolation::linearFinDiff) {
-                sm0 = (dspline_valuesdp[spline_offset + ip + nplist] - sp0)
-                    / (nodes_[1] - nodes_[0]);
-            } else {
-                throw AmiException("Natural boundary condition for Hermite "
-                                   "splines is not yet implemented.");
-            }
-        } else {
-            sm0 = dspline_slopesdp[spline_offset + ip];
-        }
+                break;
 
-        /* After node [n_nodes() - 1] */
-        realtype sp_end = dspline_valuesdp[spline_offset + ip + (n_nodes() - 1) * nplist];
-        realtype sm_end;
-        if (get_node_derivative_by_FD()) {
-            if (firstNodeDerivative == SplineBoundaryCondition::constant) {
-                sm_end = 0;
-            } else if (firstNodeDerivative == SplineBoundaryCondition::linearFinDiff) {
-                sm_end = (sp_end - dspline_valuesdp[spline_offset + ip + (n_nodes() - 2) * nplist])
-                    / (nodes_[n_nodes() - 1] - nodes_[n_nodes() - 2]);
-            } else {
-                throw AmiException("Natural boundary condition for Hermite splines "
-                                   "is not yet implemented.");
-            }
-        } else {
-            sm_end = dspline_slopesdp[spline_offset + ip + (n_nodes() - 1) * nplist];
-        }
+            case SplineExtrapolation::linear:
+                if (get_node_derivative_by_FD() &&
+                    firstNodeBC_ == SplineBoundaryCondition::given) {
+                    sm0 = (dspline_valuesdp[spline_offset + ip + nplist] - sp0)
+                        / (nodes_[1] - nodes_[0]);
 
+                } else if (get_node_derivative_by_FD() &&
+                           firstNodeBC_ == SplineBoundaryCondition::natural) {
+                    throw AmiException("Natural boundary condition for "
+                        "Hermite splines with linear extrapolation is "
+                        "not yet implemented.");
+
+                } else if (!get_node_derivative_by_FD() &&
+                           firstNodeBC_ == SplineBoundaryCondition::given) {
+                    sm0 = dspline_slopesdp[spline_offset + ip];
+                    throw AmiException("Natural boundary condition for "
+                        "Hermite splines with linear extrapolation is "
+                        "not yet implemented.");
+
+                } else if (!get_node_derivative_by_FD() &&
+                           firstNodeBC_ == SplineBoundaryCondition::natural) {
+
+                } else {
+                    throw AmiException("Some weird combination of spline boundary "
+                        "condition, extrapolation and finite differecnces was "
+                        "passed which should bo ne allowed.");
+                }
+                break;
+
+            default:
+                /* We don't need specific coefficients in the cases of:
+                 * noExtrapolation, polynomial, periodic*/
+                break;
+        }
         /* Write them to the vector */
         coefficients_extrapolate_sensi[4 * ip] = sp0 - sm0 * nodes_[0];
         coefficients_extrapolate_sensi[4 * ip + 1] = sm0;
-        coefficients_extrapolate_sensi[4 * ip + 2] = sp_end - sm_end * nodes_[n_nodes() - 1];
+    }
+
+    realtype sp_end;
+    realtype sm_end;
+    for (int ip = 0; ip < nplist; ip++) {
+        sp_end = dspline_valuesdp[spline_offset + ip + (n_nodes()-1) * nplist];
+        switch (lastNodeEP_) {
+            /* This whole switch-case-if-else-if-thing could be moved
+             * outside the loop, I know. Yet, it's at most some thousand
+             * if's done once in the program for saving many lines of code
+             * and getting a much clearer code structure. */
+            case SplineExtrapolation::constant:
+                sm_end = 0;
+                break;
+
+            case SplineExtrapolation::linear:
+                if (get_node_derivative_by_FD() &&
+                    lastNodeBC_ == SplineBoundaryCondition::given) {
+                    sm_end = (sp_end - dspline_valuesdp[spline_offset +
+                              ip + (n_nodes() - 2) * nplist])
+                             / (nodes_[n_nodes() - 1] - nodes_[n_nodes() - 2]);
+
+                } else if (get_node_derivative_by_FD() &&
+                           lastNodeBC_ == SplineBoundaryCondition::natural) {
+                    throw AmiException("Natural boundary condition for "
+                        "Hermite splines with linear extrapolation is "
+                        "not yet implemented.");
+
+                } else if (!get_node_derivative_by_FD() &&
+                           lastNodeBC_ == SplineBoundaryCondition::given) {
+                    sm_end = dspline_slopesdp[spline_offset + ip +
+                             (n_nodes() - 1) * nplist];
+
+                } else if (!get_node_derivative_by_FD() &&
+                           lastNodeBC_ == SplineBoundaryCondition::natural) {
+                    throw AmiException("Natural boundary condition for "
+                        "Hermite splines with linear extrapolation is "
+                        "not yet implemented.");
+
+                } else {
+                    throw AmiException("Some weird combination of spline boundary "
+                        "condition, extrapolation and finite differecnces was "
+                        "passed which should bo ne allowed.");
+                }
+                break;
+
+            default:
+                /* We don't need specific coefficients in the cases of:
+                 * noExtrapolation, polynomial, periodic*/
+                break;
+        }
+        /* Write them to the vector */
+        coefficients_extrapolate_sensi[4 * ip + 2] = sp_end -
+            sm_end * nodes_[n_nodes() - 1];
         coefficients_extrapolate_sensi[4 * ip + 3] = sm_end;
     }
 }
@@ -376,6 +425,7 @@ void HermiteSpline::getCoeffsSensiLowlevel(int ip, int i_node, int nplist, int n
      * computeCoefficientsSensi() here. See this function for documentation.
      * */
     int node_offset = spline_offset + ip;
+    int last = n_nodes() - 1;
     double spk = dnodesdp[node_offset + i_node * nplist];
     double spk1 = dnodesdp[node_offset + (i_node + 1) * nplist];
     double smk;
@@ -386,43 +436,60 @@ void HermiteSpline::getCoeffsSensiLowlevel(int ip, int i_node, int nplist, int n
     if (get_node_derivative_by_FD()) {
         if (i_node == 0) {
             /* Are we at the fist node? What's the boundary condition? */
-            if (firstNodeDerivative == SplineBoundaryCondition::constant) {
+            if (firstNodeBC_ == SplineBoundaryCondition::zeroDerivative) {
                 smk = 0;
-            } else if (firstNodeDerivative == SplineBoundaryCondition::linearFinDiff) {
+            } else if (firstNodeBC_ == SplineBoundaryCondition::given) {
                 smk = (spk1 - spk) / len_m;
+            } else if (firstNodeBC_ == SplineBoundaryCondition::natural) {
+                throw AmiException("Natural boundary condition for Hermite "
+                   "splines is not yet implemented.");
+            } else if (firstNodeBC_ == SplineBoundaryCondition::periodic) {
+                smk = (spk1 - dnodesdp[node_offset + last * nplist]) /
+                      (len_m + nodes_[last] - nodes_[last - 1]);
             } else {
-                throw AmiException("Natural boundary condition for Hermite splines "
-                                   "is not yet implemented.");
+                /* must be SplineBoundaryCondition::naturalZeroDerivative*/
+                throw AmiException("Natural boundary condition with zero "
+                    "derivative is prohibited for Hermite splines.");
             }
-            //smk1 = (dnodesdp[node_offset + i_node + 2] - spk) / len_p;
             smk1 = (dnodesdp[node_offset + (i_node + 2) * nplist] - spk) / len_p;
 
         } else if (i_node == n_nodes() - 2) {
             /* Are we at the last node? What's the boundary condition? */
-            //smk = (spk1 - dnodesdp[node_offset + i_node - 1]) / len_m;
             smk = (spk1 - dnodesdp[node_offset + (i_node - 1) * nplist]) / len_m;
-            if (lastNodeDerivative == SplineBoundaryCondition::constant) {
+            if (lastNodeBC_ == SplineBoundaryCondition::zeroDerivative) {
                 smk1 = 0;
-            } else if (lastNodeDerivative == SplineBoundaryCondition::linearFinDiff) {
+            } else if (lastNodeBC_ == SplineBoundaryCondition::given) {
                 smk1 = (spk1 - spk) / len_p;
+            } else if (lastNodeBC_ == SplineBoundaryCondition::natural) {
+                throw AmiException("Natural boundary condition for Hermite "
+                   "splines is not yet implemented.");
+            } else if (lastNodeBC_ == SplineBoundaryCondition::periodic) {
+                smk1 = (spk1 - dnodesdp[node_offset]) /
+                       (len_p + nodes_[1] - nodes_[0]);
             } else {
-                throw AmiException("Natural boundary condition for Hermite splines "
-                                   "is not yet implemented.");
+                /* must be SplineBoundaryCondition::naturalZeroDerivative*/
+                throw AmiException("Natural boundary condition with zero "
+                    "derivative is prohibited for Hermite splines.");
             }
+            smk1 = (dnodesdp[node_offset + (i_node + 2) * nplist] - spk) / len_p;
 
         } else {
             /* We're somewhere in between. That's fine. */
-            //smk = (spk1 - dnodesdp[node_offset + i_node - 1]) / len_m;
-            //smk1 = (dnodesdp[node_offset + i_node + 2] - spk) / len_p;
             smk = (spk1 - dnodesdp[node_offset + (i_node - 1) * nplist]) / len_m;
             smk1 = (dnodesdp[node_offset + (i_node + 2) * nplist] - spk) / len_p;
         }
     } else {
         /* The slopes are explicitly given, easiest case... */
-        //smk = dslopesdp[node_offset + i_node];
-        //smk1 = dslopesdp[node_offset + i_node + 1];
         smk = dslopesdp[node_offset + i_node * nplist];
         smk1 = dslopesdp[node_offset + (i_node + 1) * nplist];
+
+        /* For the nodes at the boundary, we have to take care of the bc */
+        if (i_node == 0 &&
+            firstNodeBC_ == SplineBoundaryCondition::zeroDerivative)
+            smk = 0;
+        if (i_node == n_nodes() - 2 &&
+            lastNodeBC_ == SplineBoundaryCondition::zeroDerivative)
+            smk1 = 0;
     }
 
     /* Compute the actual coefficients */
