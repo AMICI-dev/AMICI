149b40145d691b73402441bf5716dcf56ec04613
FFroehlich <fabian@schaluck.com>
2020-09-16 14:25:32 -0400

    fix suppression

--- scripts/run-valgrind-py.sh
+++ scripts/run-valgrind-py.sh
@@ -15,4 +15,4 @@ source "${amici_path}"/build/venv/bin/activate
 pip install scipy h5py pytest
 
 # PEtab tests are run separately
-PYTHONMALLOC=malloc valgrind --suppressions=valgrind-python.supp --show-leak-kinds=definite --error-exitcode=1 --leak-check=full --gen-suppressions=yes -v python -m pytest -vv --ignore-glob=*petab*
+PYTHONMALLOC=malloc valgrind --suppressions=valgrind-python.supp --show-leak-kinds=definite --error-exitcode=1 --leak-check=full --gen-suppressions=all -v python -m pytest -vv --ignore-glob=*petab*
