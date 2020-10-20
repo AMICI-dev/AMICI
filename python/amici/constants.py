c1799168b73f519d14b7ce88158f8c4d47c6b798
FFroehlich <fabian@schaluck.com>
2020-10-20 08:21:16 -0400

    move code comment to doctstring

--- python/amici/constants.py
+++ python/amici/constants.py
@@ -8,14 +8,16 @@ constants that are used in multiple other modules
 import enum
 
 
-# inheriting from str enables direct comparison to strings, which means that
-# the species symbols can be accessed as symbols['species'], which is
-# convenient for debugging and symbols[SymbolId.SPECIES], which is how the
-# field should be accessed programmatically.
 class SymbolId(str, enum.Enum):
     """
     Defines the different fields in the symbol dict to which sbml entities
     get parsed to.
+
+    .. note:: This class inherits from str enabling direct comparison to
+        strings, which means that the species symbols can be accessed as
+        symbols['species'], which is convenient for debugging and symbols[
+        SymbolId.SPECIES], which is how the field should be accessed
+        programmatically.
     """
     SPECIES = 'species'
     PARAMETER = 'parameter'
