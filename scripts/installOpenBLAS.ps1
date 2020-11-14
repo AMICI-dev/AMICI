76b91f1f4046e49b3b97e62259532040d64db1dd
thomassligon <tom@thomassligon.info>
2020-11-14 21:30:05 +0100

    update from 10 to 12
    Update OpenBLAS from v0.3.10 to v0.3.12, among other things because it supports building for double-precision only (v0.3.10 worked, but caused errors when trying to link utest).

--- scripts/installOpenBLAS.ps1
+++ scripts/installOpenBLAS.ps1
@@ -2,16 +2,16 @@ Write-Host 'script installOpenBLAS.ps1 started'
 New-Item -Path 'C:\BLAS' -ItemType Directory -Force # create directory
 # Enforce stronger cryptography
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-$uri = 'https://github.com/xianyi/OpenBLAS/archive/v0.3.10.zip'
-$output = 'C:\BLAS\v0.3.10.zip'
+$uri = 'https://github.com/xianyi/OpenBLAS/archive/v0.3.12.zip'
+$output = 'C:\BLAS\v0.3.12.zip'
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($uri,"$output")
-Expand-Archive -Path 'C:\BLAS\v0.3.10.zip' -DestinationPath 'C:\BLAS\OpenBLAS-v0.3.10' -Force # expand zip file
+Expand-Archive -Path 'C:\BLAS\v0.3.12.zip' -DestinationPath 'C:\BLAS\OpenBLAS-v0.3.12' -Force # expand zip file
 C:\Users\travis\build\AMICI\scripts\compileBLAS.cmd
 New-Item -Path 'C:\BLAS\lib' -ItemType Directory -Force # create directory
-Copy-Item "C:\BLAS\OpenBLAS-v0.3.10\OpenBLAS-0.3.10\lib\Release\openblas.lib" -Destination "C:\BLAS\lib" -Recurse
+Copy-Item "C:\BLAS\OpenBLAS-v0.3.12\OpenBLAS-0.3.12\lib\Release\openblas.lib" -Destination "C:\BLAS\lib" -Recurse
 New-Item -Path 'C:\BLAS\bin' -ItemType Directory -Force # create directory
-Copy-Item "C:\BLAS\OpenBLAS-v0.3.10\OpenBLAS-0.3.10\lib\openblas.dll" -Destination "C:\BLAS\bin" -Recurse
+Copy-Item "C:\BLAS\OpenBLAS-v0.3.12\OpenBLAS-0.3.12\lib\openblas.dll" -Destination "C:\BLAS\bin" -Recurse
 Get-ChildItem -Path "C:\BLAS" -Include "openblas.lib" -Recurse # check for file
 Get-ChildItem -Path "C:\BLAS" -Include "openblas.dll" -Recurse # check for file
 Get-Item -Path Env:BLAS_* # check environment variables
