b32af0a3d4d96f08eba4b6ac4a28fb9dae7ba7f7
thomassligon <tom@thomassligon.info>
2020-11-18 20:35:46 +0100

    use variable for version

--- scripts/compileBLAS.cmd
+++ scripts/compileBLAS.cmd
@@ -1,13 +1,13 @@
-echo compileBLAS.cmd started
-cd "C:\BLAS\OpenBLAS-v0.3.12\OpenBLAS-0.3.12"
+echo compileBLAS.cmd started for openBLAS version %1
+cd "C:\BLAS\OpenBLAS-v%1\OpenBLAS-%1"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 cmake -G "Ninja" ^
     -DBUILD_DOUBLE=1 ^
     -DBUILD_SHARED_LIBS=ON ^
-    -DCMAKE_INSTALL_PREFIX:PATH="C:\BLAS\OpenBLAS-v0.3.12\OpenBLAS-0.3.12\out\install\x64-Release" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="C:\BLAS\OpenBLAS-v%1\OpenBLAS-%1\out\install\x64-Release" ^
     -DCMAKE_C_COMPILER:FILEPATH=cl ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_MAKE_PROGRAM=ninja ^
-    "C:\BLAS\OpenBLAS-v0.3.12\OpenBLAS-0.3.12"
-cmake --build "C:\BLAS\OpenBLAS-v0.3.12\OpenBLAS-0.3.12" --parallel 2
+    "C:\BLAS\OpenBLAS-v%1\OpenBLAS-%1"
+cmake --build "C:\BLAS\OpenBLAS-v%1\OpenBLAS-%1" --parallel 2
 echo compileBLAS.cmd completed
