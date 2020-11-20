ba6d01d5172b478a2cc11c570c6388f431fd988f
thomassligon <tom@thomassligon.info>
2020-11-20 10:38:45 +0100

    fix PS script

--- scripts/installOpenBLAS.ps1
+++ scripts/installOpenBLAS.ps1
@@ -4,10 +4,10 @@ New-Item -Path 'C:\BLAS' -ItemType Directory -Force # create directory
 # Enforce stronger cryptography
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 $uri = "https://github.com/xianyi/OpenBLAS/archive/v$version.zip"
-$output = "C:\BLAS\$version.zip"
+$output = "C:\BLAS\v$version.zip"
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($uri,"$output")
-Expand-Archive -Path "C:\BLAS\$version.zip" -DestinationPath "C:\BLAS\OpenBLAS-$version" -Force # expand zip file
+Expand-Archive -Path "C:\BLAS\v$version.zip" -DestinationPath "C:\BLAS\OpenBLAS-$version" -Force # expand zip file
 cmd /c "C:\Users\travis\build\AMICI\scripts\compileBLAS.cmd $version"
 New-Item -Path 'C:\BLAS\lib' -ItemType Directory -Force # create directory
 Copy-Item "C:\BLAS\OpenBLAS-$version\OpenBLAS-$version\lib\Release\openblas.lib" -Destination "C:\BLAS\lib" -Recurse
