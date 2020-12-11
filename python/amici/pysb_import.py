a9e39b8a8cd2e5856f4ae6e7211ab9b1bb9b33ce
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-12-11 20:41:40 +0100

    Set ODEModel._has_quadratic_nllh

--- python/amici/pysb_import.py
+++ python/amici/pysb_import.py
@@ -201,6 +201,10 @@ def ode_model_from_pysb_importer(
                               noise_distributions)
     _process_pysb_expressions(model, ode, observables, sigmas,
                               noise_distributions)
+    ode._has_quadratic_nllh = all(
+        noise_distr in ['normal', 'lin-normal']
+        for noise_distr in noise_distributions.values()
+    )
 
     ode.generate_basic_variables()
 
