993232fc77d3dc72385e9e6c1f07dd973abff4ae
Daniel Weindl <dweindl@users.noreply.github.com>
2021-01-15 20:29:15 +0100

    Add expression IDs and names to generated models (#1374)
    * Add expression IDs and names to generated models
    
    Closes #1373
    
    * Add test for names/ids

--- src/model.ODE_template.cpp
+++ src/model.ODE_template.cpp
@@ -21,6 +21,10 @@ std::array<const char*, TPL_NY> observableNames = {
     TPL_OBSERVABLE_NAMES_INITIALIZER_LIST
 };
 
+std::array<const char*, TPL_NW> expressionNames = {
+    TPL_EXPRESSION_NAMES_INITIALIZER_LIST
+};
+
 std::array<const char*, TPL_NP> parameterIds = {
     TPL_PARAMETER_IDS_INITIALIZER_LIST
 };
@@ -37,6 +41,9 @@ std::array<const char*, TPL_NY> observableIds = {
     TPL_OBSERVABLE_IDS_INITIALIZER_LIST
 };
 
+std::array<const char*, TPL_NW> expressionIds = {
+    TPL_EXPRESSION_IDS_INITIALIZER_LIST
+};
 
 } // namespace model_TPL_MODELNAME
 
