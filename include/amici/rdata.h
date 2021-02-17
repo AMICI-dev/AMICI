982559aab0c9782fc555da33d2e97359da89f470
FFroehlich <fabian@schaluck.com>
2021-02-17 14:42:38 -0500

    fix

--- include/amici/rdata.h
+++ include/amici/rdata.h
@@ -41,17 +41,17 @@ class ReturnData: public ModelDimensions {
 
     /**
      * @brief ReturnData
-     * @param ts see amici::Model::ts
-     * @param model_dimensions Model dimensions
-     * @param nplist see amici::Model::nplist
-     * @param nmaxevent see amici::Model::nmaxevent
-     * @param nt see amici::Model::nt
-     * @param newton_maxsteps see amici::Solver::newton_maxsteps
-     * @param pscale see amici::Model::pscale
-     * @param o2mode see amici::Model::o2mode
-     * @param sensi see amici::Solver::sensi
-     * @param sensi_meth see amici::Solver::sensi_meth
-     * @param rdrm see amici::Solver::rdata_reporting
+     * @param ts see :cpp:`amici::Model::ts`
+     * @param model_dimensions Model dimensions`
+     * @param nplist see :cpp:`amici::Model::nplist`
+     * @param nmaxevent see :cpp:`amici::Model::nmaxevent`
+     * @param nt see :cpp:`amici::Model::nt`
+     * @param newton_maxsteps see :cpp:`amici::Solver::newton_maxsteps`
+     * @param pscale see :cpp:`amici::Model::pscale`
+     * @param o2mode see :cpp:`amici::Model::o2mode`
+     * @param sensi see :cpp:`amici::Solver::sensi`
+     * @param sensi_meth see :cpp:`amici::Solver::sensi_meth`
+     * @param rdrm see :cpp:`amici::Solver::rdata_reporting`
      * @param quadratic_llh whether model defines a quadratic nllh and computing res, sres and
      * FIM makes sense
      */
@@ -92,11 +92,11 @@ class ReturnData: public ModelDimensions {
                                   ExpData const *edata);
 
     /**
-     * timepoints (shape\: nt)
+     * timepoints (shape : nt)
      */
     std::vector<realtype> ts;
 
-    /** time derivative (dimension: nx) */
+    /** time derivative (shape : nx) */
     std::vector<realtype> xdot;
 
     /**
@@ -108,112 +108,112 @@ class ReturnData: public ModelDimensions {
     /**
      * w data from the model (recurring terms in xdot, for imported SBML models
      * from python, this contains the flux vector)
-     * (dimensions : nt x nw, row major)
+     * (shape : nt x nw, row major)
      */
     std::vector<realtype> w;
 
-    /** event output (dimension: nmaxevent x nz, row-major) */
+    /** event output (shape : nmaxevent x nz, row-major) */
     std::vector<realtype> z;
 
     /**
-     * event output sigma standard deviation (dimension: nmaxevent x nz,
+     * event output sigma standard deviation (shape : nmaxevent x nz,
      * row-major)
      */
     std::vector<realtype> sigmaz;
 
     /**
-     * parameter derivative of event output (dimension: nmaxevent x nz,
+     * parameter derivative of event output (shape : nmaxevent x nz,
      * row-major)
      */
     std::vector<realtype> sz;
 
     /**
-     * parameter derivative of event output standard deviation (dimension:
+     * parameter derivative of event output standard deviation (shape :
      * nmaxevent x nz, row-major)
      */
     std::vector<realtype> ssigmaz;
 
-    /** event trigger output (dimension: nmaxevent x nz, row-major)*/
+    /** event trigger output (shape : nmaxevent x nz, row-major)*/
     std::vector<realtype> rz;
 
     /**
-     * parameter derivative of event trigger output (dimension: nmaxevent x nz
+     * parameter derivative of event trigger output (shape : nmaxevent x nz
      * x nplist, row-major)
      */
     std::vector<realtype> srz;
 
     /**
-     * second order parameter derivative of event trigger output (dimension:
+     * second order parameter derivative of event trigger output (shape :
      * nmaxevent x nztrue x nplist x nplist, row-major)
      */
     std::vector<realtype> s2rz;
 
-    /** state (dimension: nt x nx, row-major) */
+    /** state (shape : nt x nx, row-major) */
     std::vector<realtype> x;
 
     /**
-     * parameter derivative of state (dimension: nt x nplist x nx, row-major)
+     * parameter derivative of state (shape : nt x nplist x nx, row-major)
      */
     std::vector<realtype> sx;
 
-    /** observable (dimension: nt x ny, row-major) */
+    /** observable (shape : nt x ny, row-major) */
     std::vector<realtype> y;
 
-    /** observable standard deviation (dimension: nt x ny, row-major) */
+    /** observable standard deviation (shape : nt x ny, row-major) */
     std::vector<realtype> sigmay;
 
     /**
-     * parameter derivative of observable (dimension: nt x nplist x ny,
+     * parameter derivative of observable (shape : nt x nplist x ny,
      * row-major)
      */
     std::vector<realtype> sy;
 
     /**
-     * parameter derivative of observable standard deviation (dimension: nt x
+     * parameter derivative of observable standard deviation (shape : nt x
      * nplist x ny, row-major)
      */
     std::vector<realtype> ssigmay;
 
-    /** observable (dimension: nt*ny, row-major) */
+    /** observable (shape : nt*ny, row-major) */
     std::vector<realtype> res;
 
     /**
-     * parameter derivative of residual (dimension: nt*ny x nplist, row-major)
+     * parameter derivative of residual (shape : nt*ny x nplist, row-major)
      */
     std::vector<realtype> sres;
 
-    /** fisher information matrix (dimension: nplist x nplist, row-major) */
+    /** fisher information matrix (shape : nplist x nplist, row-major) */
     std::vector<realtype> FIM;
 
-    /** number of integration steps forward problem (dimension: nt) */
+    /** number of integration steps forward problem (shape : nt) */
     std::vector<int> numsteps;
 
-    /** number of integration steps backward problem (dimension: nt) */
+    /** number of integration steps backward problem (shape : nt) */
     std::vector<int> numstepsB;
 
-    /** number of right hand side evaluations forward problem (dimension: nt) */
+    /** number of right hand side evaluations forward problem (shape : nt) */
     std::vector<int> numrhsevals;
 
-    /** number of right hand side evaluations backward problem (dimension: nt) */
+    /** number of right hand side evaluations backward problem (shape : nt) */
     std::vector<int> numrhsevalsB;
 
-    /** number of error test failures forward problem (dimension: nt) */
+    /** number of error test failures forward problem (shape : nt) */
     std::vector<int> numerrtestfails;
 
-    /** number of error test failures backward problem (dimension: nt) */
+    /** number of error test failures backward problem (shape : nt) */
     std::vector<int> numerrtestfailsB;
 
     /**
-     * number of linear solver convergence failures forward problem (dimension:
+     * number of linear solver convergence failures forward problem (shape :
      * nt) */
     std::vector<int> numnonlinsolvconvfails;
 
     /**
-     * number of linear solver convergence failures backward problem (dimension:
+     * number of linear solver convergence failures backward problem (shape :
      * nt) */
     std::vector<int> numnonlinsolvconvfailsB;
 
-    /** employed order forward problem (dimension: nt) */
+    /** employed order forward problem (shape : nt) */
     std::vector<int> order;
 
     /** computation time of forward solve [ms] */
@@ -302,17 +302,17 @@ class ReturnData: public ModelDimensions {
      */
     realtype posteq_wrms = NAN;
 
-    /** initial state (dimension: nx) */
+    /** initial state (shape : nx) */
     std::vector<realtype> x0;
 
-    /** preequilibration steady state found by Newton solver (dimension: nx) */
+    /** preequilibration steady state found by Newton solver (shape : nx) */
     std::vector<realtype> x_ss;
 
-    /** initial sensitivities (dimension: nplist x nx, row-major) */
+    /** initial sensitivities (shape : nplist x nx, row-major) */
     std::vector<realtype> sx0;
 
     /**
-     * preequilibration sensitivities found by Newton solver (dimension: nplist
+     * preequilibration sensitivities found by Newton solver (shape : nplist
      * x nx, row-major)
      */
     std::vector<realtype> sx_ss;
@@ -323,11 +323,11 @@ class ReturnData: public ModelDimensions {
     /** chi2 value */
     realtype chi2 = 0.0;
 
-    /** parameter derivative of loglikelihood (dimension: nplist) */
+    /** parameter derivative of loglikelihood (shape : nplist) */
     std::vector<realtype> sllh;
 
     /**
-     * second order parameter derivative of loglikelihood (dimension: (nJ-1) x
+     * second order parameter derivative of loglikelihood (shape : (nJ-1) x
      * nplist, row-major)
      */
     std::vector<realtype> s2llh;
@@ -372,7 +372,7 @@ class ReturnData: public ModelDimensions {
     RDataReporting rdata_reporting{RDataReporting::full};
 
     /**
-     * @brief Serialize ReturnData (see boost::serialization::serialize)
+     * @brief Serialize ReturnData (see :cpp:`boost::serialization::serialize`)
      * @param ar Archive to serialize to
      * @param r Data to serialize
      * @param version Version number
@@ -404,7 +404,7 @@ class ReturnData: public ModelDimensions {
     AmiVectorArray sx_rdata_;
 
     /** array of number of found roots for a certain event type
-     * (dimension: ne) */
+     * (shape : ne) */
     std::vector<int> nroots_;
 
     /**
