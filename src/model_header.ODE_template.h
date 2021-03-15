17a5ae04eb5f13c682632f113d06cf21543e453b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-03-15 22:32:12 +0100

    fixup

--- src/model_header.ODE_template.h
+++ src/model_header.ODE_template.h
@@ -571,7 +571,7 @@ class Model_TPL_MODELNAME : public amici::Model_ODE {
      * @brief returns the amici version that was used to generate the model
      * @return AMICI git commit hash
      */
-    virtual const std::string getAmiciCommit() const override {
+    virtual std::string getAmiciCommit() const override {
         return "TPL_AMICI_COMMIT_STRING";
     }
 
