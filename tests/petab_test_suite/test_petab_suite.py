e0cdc24cb9154ad02ba64112325fff862b41e654
FFroehlich <fabian@schaluck.com>
2021-03-22 20:40:49 -0400

    fix petab tests

--- tests/petab_test_suite/test_petab_suite.py
+++ tests/petab_test_suite/test_petab_suite.py
@@ -85,6 +85,10 @@ def _test_case(case, model_type):
     gt_chi2 = solution[petabtests.CHI2]
     gt_llh = solution[petabtests.LLH]
     gt_simulation_dfs = solution[petabtests.SIMULATION_DFS]
+    if case == '0006':
+        # account for flattening
+        gt_simulation_dfs[0].loc[:, petab.OBSERVABLE_ID] = ('obs_a__10__c0',
+                                                            'obs_a__15__c0')
     tol_chi2 = solution[petabtests.TOL_CHI2]
     tol_llh = solution[petabtests.TOL_LLH]
     tol_simulations = solution[petabtests.TOL_SIMULATIONS]
