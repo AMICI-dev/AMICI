AMICI-dev/AMICI

commit 88867922309862fdced413667fb050fdf25f1798

PEtab: More informative output for unhandled species overrides (#1519)
Handle sympy expressions that consist only of parameter IDs.
Otherwise show the offending expression.

Partially resolves #1516

python/amici/petab_objective.py
@@ -17,6 +17,7 @@ import libsbml
 import numpy as np
 import pandas as pd
 import petab
+import sympy as sp
 from petab.C import *  # noqa: F403
 
 from . import AmiciModel, AmiciExpData
@@ -346,11 +347,21 @@ def create_parameter_mapping_for_condition(
             value = petab.to_float_if_float(
                 petab_problem.condition_df.loc[condition_id, species_id])
             if pd.isna(value):
-                value = float(
-                    get_species_initial(
-                        petab_problem.sbml_model.getSpecies(species_id)
-                    )
+                value = get_species_initial(
+                    petab_problem.sbml_model.getSpecies(species_id)
                 )
+                try:
+                    value = float(value)
+                except ValueError:
+                    if sp.nsimplify(value).is_Atom:
+                        # Get rid of multiplication with one
+                        value = sp.nsimplify(value)
+                    else:
+                        raise NotImplementedError(
+                            "Cannot handle non-trivial expressions for "
+                            f"species initial for {species_id}: {value}")
+                    # this should be a parameter ID
+                    value = str(value)
                 logger.debug(f'The species {species_id} has no initial value '
                              f'defined for the condition {condition_id} in '
                              'the PEtab conditions table. The initial value is '

commit e3c2a4e61d015c4bb1389abc71239b95b85364ad

PEtab import: Fixup for 888679, handle TypeError
Fixes incomplete exception handling introduced in 88867922309862fdced413667fb050fdf25f1798

python/amici/petab_objective.py
@@ -352,7 +352,7 @@ def create_parameter_mapping_for_condition(
                 )
                 try:
                     value = float(value)
-                except ValueError:
+                except (ValueError, TypeError):
                     if sp.nsimplify(value).is_Atom:
                         # Get rid of multiplication with one
                         value = sp.nsimplify(value)