9ddb859cd6bb492b8f9a35c71f9cedbee45b413e
Daniel Weindl <dweindl@users.noreply.github.com>
2021-09-26 13:32:34 +0200

    Improve checking support for SBML extensions (#1546)
    Ignore enabled extensions that do not affect model equations
    
    Closes #1545
    
    * Plugin 'required' attribute only available in L3; remove unnecessary lists
    
    * ignore layout extension

--- python/amici/sbml_import.py
+++ python/amici/sbml_import.py
@@ -408,28 +408,49 @@ class SbmlImporter:
         Also ensures that the SBML contains at least one reaction, or rate
         rule, or assignment rule, to produce change in the system over time.
         """
-        if hasattr(self.sbml, 'all_elements_from_plugins') \
+
+        # Check for required but unsupported SBML extensions
+        if self.sbml_doc.getLevel() != 3 \
+                and hasattr(self.sbml, 'all_elements_from_plugins') \
                 and self.sbml.all_elements_from_plugins.getSize():
             raise SBMLException('SBML extensions are currently not supported!')
 
-        if any([not rule.isAssignment() and not isinstance(
+        if self.sbml_doc.getLevel() == 3:
+            # the "required" attribute is only available in SBML Level 3
+            for i_plugin in range(self.sbml.getNumPlugins()):
+                plugin = self.sbml.getPlugin(i_plugin)
+                if plugin.getPackageName() in ('layout',):
+                    # 'layout' plugin does not have the 'required' attribute
+                    continue
+                if hasattr(plugin, 'getRequired') and not plugin.getRequired():
+                    # if not "required", this has no impact on model
+                    #  simulation, and we can safely ignore it
+                    continue
+                # Check if there are extension elements. If not, we can safely
+                #  ignore the enabled package
+                if plugin.getListOfAllElements():
+                    raise SBMLException(
+                        f'Required SBML extension {plugin.getPackageName()} '
+                        f'is currently not supported!')
+
+        if any(not rule.isAssignment() and not isinstance(
                     self.sbml.getElementBySId(rule.getVariable()),
                     (sbml.Compartment, sbml.Species, sbml.Parameter)
-                ) for rule in self.sbml.getListOfRules()]):
+                ) for rule in self.sbml.getListOfRules()):
             raise SBMLException('Algebraic rules are currently not supported, '
                                 'and rate rules are only supported for '
                                 'species, compartments, and parameters.')
 
-        if any([not (rule.isAssignment() or rule.isRate())
+        if any(not (rule.isAssignment() or rule.isRate())
                 and isinstance(
                     self.sbml.getElementBySId(rule.getVariable()),
                     (sbml.Compartment, sbml.Species, sbml.Parameter)
-                ) for rule in self.sbml.getListOfRules()]):
+                ) for rule in self.sbml.getListOfRules()):
             raise SBMLException('Only assignment and rate rules are '
                                 'currently supported for compartments, '
                                 'species, and parameters!')
 
-        if any([r.getFast() for r in self.sbml.getListOfReactions()]):
+        if any(r.getFast() for r in self.sbml.getListOfReactions()):
             raise SBMLException('Fast reactions are currently not supported!')
 
         # Check events for unsupported functionality
