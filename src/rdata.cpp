23712633c8f267c9aea3e99dc140ab0392ced1fd
FFroehlich <fabian@schaluck.com>
2021-10-12 09:06:28 -0400

    fix blank throws

--- src/rdata.cpp
+++ src/rdata.cpp
@@ -787,7 +787,7 @@ static realtype fres(realtype y, realtype my, realtype sigma_y,
         case amici::ObservableScaling::log10:
             return (std::log10(y) - std::log10(my)) / sigma_y;
         default:
-            throw;
+            throw std::invalid_argument("only lin, log, log10 allowed.");
     }
 }
 
@@ -845,7 +845,7 @@ static realtype fsres(realtype y, realtype sy, realtype my,
         case amici::ObservableScaling::log10:
             return (sy / (y * std::log(10)) - ssigma_y * res) / sigma_y;
         default:
-            throw;
+            throw std::invalid_argument("only lin, log, log10 allowed.");
     }
 }
 static realtype fsres_error(realtype sigma_y, realtype ssigma_y,
