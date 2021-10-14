1e15477c8a250b9c8d2202baac3361b917f3fb88
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-10-14 11:06:12 +0200

    Update package name

--- documentation/conf.py
+++ documentation/conf.py
@@ -94,7 +94,7 @@ def install_amici_deps_rtd():
     cmd = (f"cd '{os.path.join(amici_dir, 'ThirdParty')}' "
            "&& apt download libatlas-base-dev && mkdir libatlas-base-dev "
            "&& cd libatlas-base-dev "
-           "&& ar x ../libatlas-base-dev_3.10.3-5_amd64.deb "
+           "&& ar x ../libatlas-base-dev_3.10.3-8ubuntu7_amd64.deb "
            "&& tar -xJf data.tar.xz "
            f"&& ln -s {cblas_inc_dir}/cblas-atlas.h {cblas_inc_dir}/cblas.h "
            )
