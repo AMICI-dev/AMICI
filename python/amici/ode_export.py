0b3b6eb1de894174c343f9c93a8b6cf12d5b809f
FFroehlich <fabian@schaluck.com>
2021-11-30 16:25:19 -0500

    fix substitution `w` expression in root functions

--- python/amici/ode_export.py
+++ python/amici/ode_export.py
@@ -1626,7 +1626,7 @@ class ODEModel:
             expr.set_val(self._process_heavisides(expr.get_val(), roots))
 
         # remove all possible Heavisides from roots, which may arise from
-        # the substitution of `'w'` in `_collect_heaviside_roots`
+        # the substitution of `'w'` in `_get_unique_root`
         for root in roots:
             root.set_val(self._process_heavisides(root.get_val(), roots))
 
@@ -2036,6 +2036,7 @@ class ODEModel:
                         raise RuntimeError(
                             'dwdw is not nilpotent. Something, somewhere went '
                             'terribly wrong. Please file a bug report at '
+                            'terribly wrong. Please file a bug report at '
                             'https://github.com/AMICI-dev/AMICI/issues and '
                             'attach this model.'
                         )
@@ -2354,6 +2355,14 @@ class ODEModel:
             unique identifier for root, or `None` if the root is not
             time-dependent
         """
+        # substitute 'w' expressions into root expressions now, to avoid
+        # rewriting '{model_name}_root.cpp' and '{model_name}_stau.cpp' headers
+        # to include 'w.h'
+        w_sorted = toposort_symbols(dict(zip(
+            [expr.get_id() for expr in self._expressions],
+            [expr.get_val() for expr in self._expressions],
+        )))
+        root_found = root_found.subs(w_sorted)
 
         if not self._expr_is_time_dependent(root_found):
             return None
@@ -2394,18 +2403,6 @@ class ODEModel:
                 root_funs.append(arg.args[0])
             elif arg.has(sp.Heaviside):
                 root_funs.extend(self._collect_heaviside_roots(arg.args))
-
-        # substitute 'w' expressions into root expressions now, to avoid
-        # rewriting '{model_name}_root.cpp' and '{model_name}_stau.cpp' headers
-        # to include 'w.h'
-        w_sorted = toposort_symbols(dict(zip(
-            [expr.get_id()  for expr in self._expressions],
-            [expr.get_val() for expr in self._expressions],
-        )))
-        root_funs = [
-            r.subs(w_sorted)
-            for r in root_funs
-        ]
 
         return root_funs
 
