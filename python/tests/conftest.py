7d23311d73f0a2ce04ab987da56ede8c3b46ab21
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-01-11 22:14:50 +0100

    add_dll_directory

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -5,8 +5,14 @@ import os
 import shutil
 import sys
 
-import amici
 import pytest
+
+if sys.platform == 'win32':
+    # GitHub actions OpenBLAS location
+    #  directory needs to be added explicitly in Python>=3.8
+    os.add_dll_directory(r"C:\BLAS\OpenBLAS-0.3.12\OpenBLAS-0.3.12\lib")
+
+import amici
 
 
 @pytest.fixture(scope="session")
