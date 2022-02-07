87dccb771cfa874a947febe4575a19760819ca87
dilpath <dilan.private+github@outlook.com>
2022-02-07 22:19:05 +0100

    use fiddy

--- python/tests/test_petab_benchmark.py
+++ python/tests/test_petab_benchmark.py
@@ -13,10 +13,11 @@ import petab
 from petab.C import NOMINAL_VALUE, LOWER_BOUND, UPPER_BOUND, ESTIMATE
 import pytest
 
-import finite_difference_methods as fdm
-from finite_difference_methods.extensions.amici import (
+import fiddy
+from fiddy.extensions.amici import (
     simulate_petab_to_cached_functions,
 )
+from fiddy.gradient_check import keep_lowest_error
 
 
 # Absolute and relative tolerances for finite difference gradient checks.
@@ -28,12 +29,37 @@ benchmark_yamls = [
     petab_path / (petab_path.stem + ".yaml")
     for petab_path in benchmark_path.glob("*") if petab_path.is_dir()
 ]
-#benchmark_yamls = [benchmark_yamls[1]]  # FIXME remove
+#benchmark_yamls = [benchmark_yamls[0]]  # FIXME remove
+
+debug = True
+if debug:
+    debug_path = Path('debug')
+    debug_full_path = debug_path / 'full'
+    debug_minimal_path = debug_path / 'minimal'
+    debug_full_path.mkdir(exist_ok=True, parents=True)
+    debug_minimal_path.mkdir(exist_ok=True, parents=True)
 
 
 @pytest.mark.parametrize("petab_yaml", benchmark_yamls)
 def test_benchmark_gradient(petab_yaml):
     petab_problem = petab.Problem.from_yaml(petab_yaml)
+
+    sizes = [
+        #1e-1,
+        #1e-2,
+        1e-3,
+        #1e-4,
+        1e-5,
+        #1e-6,
+        1e-7,
+        #1e-8,
+        1e-9,
+        #1e-10,
+        #1e-11,
+        #1e-12,
+        #1e-13,
+    ]
+
 
     # Only compute gradient for estimated parameters.
     index_estimated = petab_problem.parameter_df[ESTIMATE] == 1
@@ -65,19 +91,21 @@ def test_benchmark_gradient(petab_yaml):
         )
 
     gradient_check_partial = partial(
-        fdm.gradient_check,
+        fiddy.gradient_check,
         function=function,
         point=point,
         gradient=gradient,
-        sizes=[
-            1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11,
-            1e-12, 1e-13,
-        ],
+        sizes=sizes,
     )
 
     success, result_df = gradient_check_partial(
         fd_gradient_method='central',
     )
+
+    if debug:
+        result_df.to_csv(debug_full_path / (petab_yaml.stem + ".tsv"), sep='\t')
+        minimal_result_df = keep_lowest_error(result_df, inplace=False)
+        minimal_result_df.to_csv(debug_minimal_path / (petab_yaml.stem + ".tsv"), sep='\t')
 
     # The gradients for all parameters are correct.
     assert success, result_df
