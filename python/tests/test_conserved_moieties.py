67e11bdeb7564ecdf0afbb4fee135c5305be29a4
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-02-11 11:26:20 +0100

    cleanup test; remove obsolete functions

--- python/tests/test_conserved_moieties.py
+++ python/tests/test_conserved_moieties.py
@@ -1,9 +1,9 @@
 """Tests for conservation laws / conserved moieties"""
 
-import time
 from time import perf_counter
 
 import numpy as np
+import pytest
 
 from amici.conserved_moieties import *
 from amici.logging import get_logger, log_execution_time
@@ -11,43 +11,33 @@ from amici.logging import get_logger, log_execution_time
 logger = get_logger(__name__)
 
 
-def GetRemoteInput():
+@pytest.fixture(scope="session")
+def data_demartino2014():
+    """Get tests from DeMartino2014 Suppl. Material"""
     import urllib.request
     import io
     import gzip
+
+    # stoichiometric matrix
     response = urllib.request.urlopen(
-        r'http://chimera.roma1.infn.it/SYSBIO/test-ecoli.dat.gz')
+        r'https://chimera.roma1.infn.it/SYSBIO/test-ecoli.dat.gz')
     data = gzip.GzipFile(fileobj=io.BytesIO(response.read()))
-    return [int(item) for sl in
-            [entry.decode('ascii').strip().split('\t') for entry in
-             data.readlines()] for item in sl]
+    S = [int(item) for sl in
+         [entry.decode('ascii').strip().split('\t')
+          for entry in data.readlines()] for item in sl]
 
-
-def GetRemoteNames():
-    import urllib.request
-    import io
+    # metabolite / row names
     response = urllib.request.urlopen(
-        r'http://chimera.roma1.infn.it/SYSBIO/test-ecoli-met.txt')
-    return [entry.decode('ascii') for entry in io.BytesIO(response.read())]
+        r'https://chimera.roma1.infn.it/SYSBIO/test-ecoli-met.txt')
+    row_names = [entry.decode('ascii')
+                 for entry in io.BytesIO(response.read())]
+
+    return S, row_names
 
 
-def Input():
-    """ Read input from test stoichiometric matrix """
-    with open('matrix.dat', 'r') as f:
-        return [int(item) for sl in [entry.strip().split('\t') for entry in f]
-                for item in sl]
-
-
-def Names():
-    """ Get names of metabolites from list of metabolites example"""
-    with open('metabolites.txt', 'r') as f:
-        return list(f)
-
-
-def Output(
-        intKernelDim, kernelDim, intmatched, NSolutions, NSolutions2,
-        IsRemoteFile=False
-        ):
+def output(
+        intKernelDim, kernelDim, intmatched, NSolutions, NSolutions2, row_names
+):
     """
     Output solution
 
@@ -67,41 +57,39 @@ def Output(
     else:
         print(f"They don't generate all the conservation laws, "
               f"{kernelDim - intKernelDim} of them are not reducible to "
-              "moeities")
+              "moieties")
 
-    names = IsRemoteFile and Names() or GetRemoteNames()
     for i in range(intKernelDim):
         print(f"Moiety number {i + 1} engages {len(NSolutions[i])} "
               "metabolites.")
         print('\t')
         for j in range(len(NSolutions[i])):
-            print(f"{names[NSolutions[i][j]]} \t {NSolutions2[i][j]}")
+            print(f"{row_names[NSolutions[i][j]]} \t {NSolutions2[i][j]}")
 
 
 @log_execution_time("Detecting conservation laws", logger)
-def test_detect_cl():
+def test_detect_cl(data_demartino2014):
     """Invoke test case and benchmarking for De Martino's published results
     for E. coli network"""
-    S = GetRemoteInput()
+    S, row_names = data_demartino2014
     N = 1668
     M = 2381
-    knownValuesFromDeMartino = [53] + [2] * 11 + [6] + [3] * 2 + [2] * 15 + [
-        3] + [2] * 5
+    knownValuesFromDeMartino = \
+        [53] + [2] * 11 + [6] + [3] * 2 + [2] * 15 + [3] + [2] * 5
 
-    if len(S) != N * M:
-        logger.debug("Stoichiometric matrix inconsistent")
+    assert len(S) == N * M, "Unexpected dimension of stoichiometric matrix"
 
     start = perf_counter()
     logger.debug(f"Kernel calculation of S ({N} x {M})...\n")
     kernelDim, engagedMetabolites, intKernelDim, conservedMoieties, \
-    NSolutions, NSolutions2 = kernel(S, N, M)
+        NSolutions, NSolutions2 = kernel(S, N, M)
     logger.debug(f"There are {kernelDim} conservation laws, engaging, "
                  f"{len(engagedMetabolites)} metabolites, {intKernelDim} are "
                  f"integers (conserved moieties), engaging "
                  f"{len(conservedMoieties)} metabolites...""")
     logger.debug("Kernel calc")
-    Output(intKernelDim, kernelDim, engagedMetabolites, NSolutions,
-           NSolutions2, IsRemoteFile=False)
+    output(intKernelDim, kernelDim, engagedMetabolites, NSolutions,
+           NSolutions2, row_names)
     logger.debug("Kernel calc")
 
     # There are 38 conservation laws, engaging 131 metabolites
@@ -131,11 +119,12 @@ def test_detect_cl():
         maxIter = 10
         while finish == 0:
             logger.debug(f"MonteCarlo call #{counter} (maxIter: {maxIter})")
-            yes, intKernelDim, kernelDim, NSolutions, NSolutions2, engagedMetabolites, conservedMoieties = MonteCarlo(
-                engagedMetabolites, J, J2, fields, conservedMoieties,
-                intKernelDim, NSolutions, NSolutions2, kernelDim, N)
-            Output(intKernelDim, kernelDim, engagedMetabolites, NSolutions,
-                   NSolutions2)
+            yes, intKernelDim, kernelDim, NSolutions, NSolutions2, \
+                engagedMetabolites, conservedMoieties = MonteCarlo(
+                    engagedMetabolites, J, J2, fields, conservedMoieties,
+                    intKernelDim, NSolutions, NSolutions2, kernelDim, N)
+            output(intKernelDim, kernelDim, engagedMetabolites, NSolutions,
+                   NSolutions2, row_names)
 
             counter += 1
             if intKernelDim == kernelDim:
@@ -159,15 +148,15 @@ def test_detect_cl():
             assert (set(old2[i]) == set(NSolutions2[i]))
 
     # Assert that each conserved moiety has the correct number of metabolites
-    for i in range(0, intKernelDim - 2):
-        assert (len(NSolutions[i]) == knownValuesFromDeMartino[
-            i]), f"Moiety #{i + 1} failed for test case (De Martino et al.)"
+    for i in range(intKernelDim - 2):
+        assert (len(NSolutions[i]) == knownValuesFromDeMartino[i]),\
+            f"Moiety #{i + 1} failed for test case (De Martino et al.)"
 
     logger.debug("*" * 80)
     logger.debug("Details about conserved moieties:")
     logger.debug("*" * 80)
-    Output(intKernelDim, kernelDim, engagedMetabolites, NSolutions,
-           NSolutions2)
+    output(intKernelDim, kernelDim, engagedMetabolites, NSolutions,
+           NSolutions2, row_names)
     logger.debug("-" * 80)
     logger.debug("-" * 80)
     logger.debug("-" * 80)
@@ -179,7 +168,7 @@ def test_detect_cl():
 
 
 @log_execution_time("Detecting moiety conservation laws", logger)
-def test_cl_detect_execution_time():
+def test_cl_detect_execution_time(data_demartino2014):
     """Test execution time stays within a certain predefined bound"""
     max_tries = 3
     # <5s on modern hardware, but leave some slack
@@ -187,8 +176,7 @@ def test_cl_detect_execution_time():
     runtime = np.Inf
 
     for _ in range(max_tries):
-        runtime = test_detect_cl()
+        runtime = test_detect_cl(data_demartino2014)
         if runtime < max_time_seconds:
             break
     assert runtime < max_time_seconds, "Took too long"
-
