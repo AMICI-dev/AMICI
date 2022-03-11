1084a3b6054435be1885880aa4580f851d72694b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-03-11 16:22:14 +0100

    don't import ext from setup

--- python/amici/__init__.template.py
+++ python/amici/__init__.template.py
@@ -16,7 +16,33 @@ except ModuleNotFoundError:
     # AMICI is not installed, so no problem here
     pass
 
-from TPL_MODELNAME._TPL_MODELNAME import *
-from .core import *
+import os
+
+
+def _imported_from_setup() -> bool:
+    """Check whether this module is imported from `setup.py`"""
+
+    from inspect import getouterframes, currentframe
+
+    # in case we are imported from setup.py, this will be the AMICI package
+    # root directory (otherwise it is most likely the Python library directory,
+    # we are not interested in)
+    package_root = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
+
+    for frame in getouterframes(currentframe(), context=0):
+        # Need to compare the full path, in case a user tries to import AMICI
+        # from a module `*setup.py`. Will still cause trouble if some package
+        # requires the AMICI extension during its installation, but seems
+        # unlikely...
+        frame_path = os.path.realpath(os.path.expanduser(frame.filename))
+        if frame_path == os.path.join(package_root, 'setup.py'):
+            return True
+
+    return False
+
+
+if not _imported_from_setup():
+    from TPL_MODELNAME._TPL_MODELNAME import *
+    from .core import *
 
 __version__ = 'TPL_PACKAGE_VERSION'
