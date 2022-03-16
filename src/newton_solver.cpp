288c346be145d9909ff859e3dafcb3bb4092af62
FFroehlich <fabian@schaluck.com>
2022-03-16 15:46:12 -0400

    add dense implementation

--- src/newton_solver.cpp
+++ src/newton_solver.cpp
@@ -173,7 +173,14 @@ void NewtonSolverDense::solveLinearSystem(AmiVector &rhs) {
 /* ------------------------------------------------------------------------- */
 
 bool NewtonSolverDense::is_singular() const {
-    
+    // dense solver doesn't have any implementation for rcond/condest, so use
+    // sparse solver interface, not the most efficient solution, but who is
+    // concerned about speed and used the dense solver anyways ¯\_(ツ)_/¯
+    auto sparse_solver = new NewtonSolverSparse(t_, x_, model_);
+    sparse_solver->prepareLinearSystem(0,0);
+    auto is_singular = sparse_solver->is_singular();
+    delete sparse_solver;
+    return is_singular;
 }
 
 NewtonSolverDense::~NewtonSolverDense() {
