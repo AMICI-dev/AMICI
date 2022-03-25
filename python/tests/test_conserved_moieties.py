aa55328a4358a1f4ae5c58ab4942aa715450e12c
Stephan Grein <stephan.grein@uni-bonn.de>
2022-03-25 12:05:35 +0100

    Fix test.

--- python/tests/test_conserved_moieties.py
+++ python/tests/test_conserved_moieties.py
@@ -204,6 +204,7 @@ def test_compute_moiety_conservation_laws_demartino2014(
     start = perf_counter()
     cls_state_idxs, cls_coefficients = compute_moiety_conservation_laws(
         stoichiometric_list,
+        True,
         num_species=num_species,
         num_reactions=num_reactions
     )
