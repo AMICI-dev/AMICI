abed58fd6cf0c2a337dc4a3ada5f3a07d54bb800
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-07-18 12:13:42 +0200

    undo

--- scripts/run-valgrind-py.sh
+++ scripts/run-valgrind-py.sh
@@ -10,26 +10,18 @@ if [[ -z "${BNGPATH}" ]]; then
     export BNGPATH=${amici_path}/ThirdParty/BioNetGen-2.7.0
 fi
 
-#cd "${amici_path}"/python/tests
+cd "${amici_path}"/python/tests
+
 source "${amici_path}"/build/venv/bin/activate
-pip install scipy h5py pytest
-# PEtab tests are run separately
-#python -m pytest -vv --ignore-glob=*petab* \
-#  --ignore-glob=test_sbml_import_special_functions.py
-source build/venv/bin/activate \
-&& pip3 install coverage pytest pytest-cov pytest-rerunfailures \
-&& pytest \
-  --ignore-glob=*petab* \
-  ${amici_path}/python/tests
 
+pip install scipy h5py pytest pytest-rerunfailures
 
-#PYTHONMALLOC=malloc valgrind \
-#  --suppressions=valgrind-python.supp \
-#  --show-leak-kinds=definite \
-#  --errors-for-leak-kinds=definite \
-#  --error-exitcode=1 \
-#  --leak-check=full \
-#  --gen-suppressions=all \
-#  -v \
-#  python -m pytest -vv --ignore-glob=*petab* \
-#    -k "not test_sbml2amici_observable_dependent_error"
+PYTHONMALLOC=malloc valgrind \
+  --suppressions=valgrind-python.supp \
+  --show-leak-kinds=definite \
+  --errors-for-leak-kinds=definite \
+  --error-exitcode=1 \
+  --leak-check=full \
+  --gen-suppressions=all \
+  -v \
+  python -m pytest -vv --ignore-glob=*petab*
