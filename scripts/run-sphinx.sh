e25d1055e655ca018ac9750932c0b66535dafd59
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-08-23 14:03:35 +0200

    ..

--- scripts/run-sphinx.sh
+++ scripts/run-sphinx.sh
@@ -7,8 +7,8 @@ AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 python3 -m venv ${AMICI_PATH}/doc-venv --clear
 source ${AMICI_PATH}/doc-venv/bin/activate
 python -m pip install --upgrade --no-cache-dir pip
-python -m pip install git+https://github.com/readthedocs/readthedocs-sphinx-ext
 python -m pip install --exists-action=w --no-cache-dir -r ${AMICI_PATH}/documentation/rtd_requirements.txt
+python -m pip install exhale>=0.3.5
 
 ${AMICI_PATH}/scripts/run-sphinx-hasenv.sh
 
