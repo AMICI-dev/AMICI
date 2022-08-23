f952c14656dc20712fb88493b97507fba50ca66b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-08-23 12:32:57 +0200

    Fix postcondition: numsteps is only sorted if no integration failures occur

--- src/amici.cpp
+++ src/amici.cpp
@@ -246,11 +246,15 @@ AmiciApplication::runAmiciSimulation(Solver& solver,
     gsl_EnsuresDebug(rdata->posteq_cpu_time <= rdata->cpu_time_total);
     gsl_EnsuresDebug(rdata->posteq_cpu_timeB <= rdata->cpu_time_total);
     if (!posteq)
-        gsl_EnsuresDebug(std::is_sorted(rdata->numsteps.begin(),
-                                        rdata->numsteps.end()));
+        gsl_EnsuresDebug(
+            std::is_sorted(rdata->numsteps.begin(), rdata->numsteps.end())
+            || rdata->status != AMICI_SUCCESS
+        );
     if (!preeq)
-        gsl_EnsuresDebug(std::is_sorted(rdata->numstepsB.begin(),
-                                        rdata->numstepsB.end()));
+        gsl_EnsuresDebug(
+            std::is_sorted(rdata->numstepsB.begin(), rdata->numstepsB.end())
+            || rdata->status != AMICI_SUCCESS
+        );
 
     return rdata;
 }
