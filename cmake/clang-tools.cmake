14b7c890b4cfece3da4ee897cc4361ae151fe349
Daniel Weindl <dweindl@users.noreply.github.com>
2022-12-11 15:39:05 +0100

    Update clang-format style, fix clang-format target (#1908)
    Previously, `make clang-format` missed all header files.
    
    * Update .clang-format
    
    * fix missing headers
    
    * fix path, fix cxx standard

--- cmake/clang-tools.cmake
+++ cmake/clang-tools.cmake
@@ -1,7 +1,7 @@
 ######### Add targets for clang-format and clang-tidy ############
 
 # Find all source files
-execute_process(COMMAND sh -c "git ls-tree -r HEAD --name-only src/*.cpp include/*.h | tr '\n' ' '"
+execute_process(COMMAND sh -c "git ls-tree -r HEAD --name-only src/*.cpp include/amici/*.h | tr '\n' ' '"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE ALL_CXX_SOURCE_FILES
     )
@@ -12,7 +12,7 @@ find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
     add_custom_target(
         clang-format
-        COMMAND bash -c "/usr/bin/clang-format -i ${ALL_CXX_SOURCE_FILES}"
+        COMMAND bash -c "${CLANG_FORMAT} -i ${ALL_CXX_SOURCE_FILES}"
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         )
 else()
@@ -26,7 +26,7 @@ find_program(CLANG_TIDY "clang-tidy")
 if(CLANG_TIDY)
     add_custom_target(
         clang-tidy
-        COMMAND sh -c "/usr/bin/clang-tidy ${ALL_CXX_SOURCE_FILES} -- -std=c++11 -I${CMAKE_SOURCE_DIR}"
+        COMMAND sh -c "${CLANG_TIDY} ${ALL_CXX_SOURCE_FILES} -- -std=c++14 -I${CMAKE_SOURCE_DIR}"
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         )
 else()
