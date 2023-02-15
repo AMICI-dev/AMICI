c05f4650d2aaa70470ea8d754e2972f2ed9a7311
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-02-15 21:51:04 +0100

    optomp

--- cmake/AmiciConfig.cmake
+++ cmake/AmiciConfig.cmake
@@ -2,6 +2,7 @@
 
 include(CMakeFindDependencyMacro)
 
+find_package(OpenMP)
 find_package(SUNDIALS REQUIRED PATHS "@CMAKE_SOURCE_DIR@/ThirdParty/sundials/build/lib/cmake/sundials/")
 
 include("${CMAKE_CURRENT_LIST_DIR}/AmiciTargets.cmake")
