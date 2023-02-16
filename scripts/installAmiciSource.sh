00a281bc8362176f9ce90009d080d3915bd57c7a
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-02-16 10:46:39 +0100

    ;

--- scripts/installAmiciSource.sh
+++ scripts/installAmiciSource.sh
@@ -27,7 +27,5 @@ else
     source ${AMICI_PATH}/build/venv/bin/activate
 fi
 
-pip install --upgrade pip pkgconfig scipy matplotlib coverage pytest pytest-cov cmake-build-extension==0.5.1 oldest-supported-numpy
-pip install git+https://github.com/pysb/pysb # pin to develop to fix sympy compatibility
-pip install --verbose -e ${AMICI_PATH}/python/sdist[petab,test] --no-build-isolation
+pip install --verbose -e ${AMICI_PATH}/python/sdist[petab,test,pysb]
 deactivate
