67dbbd889f9a3bfe033fdf1fed69ad3ad55d60a9
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-02-20 17:01:45 +0100

    ncholmod

--- scripts/buildSuiteSparse.sh
+++ scripts/buildSuiteSparse.sh
@@ -8,7 +8,7 @@ script_path=$(dirname "$BASH_SOURCE")
 amici_path=$(cd "$script_path/.." && pwd)
 
 suitesparse_root="${amici_path}/ThirdParty/SuiteSparse"
-export CMAKE_OPTIONS="-DBLA_VENDOR=All -DALLOW_64BIT_BLAS=ON -DENABLE_CUDA=FALSE -DNFORTRAN=TRUE"
+export CMAKE_OPTIONS="-DBLA_VENDOR=All -DALLOW_64BIT_BLAS=ON -DENABLE_CUDA=FALSE -DNFORTRAN=TRUE -DNCHOLMOD=TRUE"
 for subdir in SuiteSparse_config BTF AMD CAMD COLAMD KLU
   do cd "${suitesparse_root}/${subdir}" && make local install
 done
