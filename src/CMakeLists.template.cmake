26e9d74401833278b22b4bcee347f6e860fac173
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-02-22 11:10:45 +0100

    format

--- src/CMakeLists.template.cmake
+++ src/CMakeLists.template.cmake
@@ -8,41 +8,39 @@ set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 include(CheckCXXCompilerFlag)
 set(MY_CXX_FLAGS -Wall -Wno-unused-function -Wno-unused-variable)
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    list(APPEND MY_CXX_FLAGS  -Wno-unused-but-set-variable)
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  list(APPEND MY_CXX_FLAGS -Wno-unused-but-set-variable)
 endif()
 foreach(FLAG ${MY_CXX_FLAGS})
-    unset(CUR_FLAG_SUPPORTED CACHE)
-    CHECK_CXX_COMPILER_FLAG(-Werror ${FLAG} CUR_FLAG_SUPPORTED)
-    if(${CUR_FLAG_SUPPORTED})
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAG}")
-    endif()
+  unset(CUR_FLAG_SUPPORTED CACHE)
+  check_cxx_compiler_flag(-Werror ${FLAG} CUR_FLAG_SUPPORTED)
+  if(${CUR_FLAG_SUPPORTED})
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAG}")
+  endif()
 endforeach(FLAG)
 
-find_package(Amici TPL_AMICI_VERSION REQUIRED HINTS ${CMAKE_CURRENT_LIST_DIR}/../../build)
+find_package(Amici TPL_AMICI_VERSION REQUIRED HINTS
+             ${CMAKE_CURRENT_LIST_DIR}/../../build)
 message(STATUS "Found AMICI ${Amici_DIR}")
 
 set(MODEL_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-set(SRC_LIST_LIB TPL_SOURCES
-${MODEL_DIR}/wrapfunctions.cpp
-)
+set(SRC_LIST_LIB TPL_SOURCES ${MODEL_DIR}/wrapfunctions.cpp)
 
 add_library(${PROJECT_NAME} ${SRC_LIST_LIB})
 add_library(model ALIAS ${PROJECT_NAME})
 
 # This option can be helpful when using the Intel compiler and compilation of
 # wrapfunctions.cpp fails due to insufficient memory.
-option(ENABLE_WRAPFUNCTIONS_OPTIMIZATIONS "Enable compiler optimizations for wrapfunctions.cpp?" ON)
+option(ENABLE_WRAPFUNCTIONS_OPTIMIZATIONS
+       "Enable compiler optimizations for wrapfunctions.cpp?" ON)
 if(NOT ENABLE_WRAPFUNCTIONS_OPTIMIZATIONS)
-    set_source_files_properties(wrapfunctions.cpp PROPERTIES COMPILE_FLAGS -O0)
+  set_source_files_properties(wrapfunctions.cpp PROPERTIES COMPILE_FLAGS -O0)
 endif()
 
 target_include_directories(${PROJECT_NAME} PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}")
 
-target_link_libraries(${PROJECT_NAME}
-    PUBLIC Upstream::amici
-)
+target_link_libraries(${PROJECT_NAME} PUBLIC Upstream::amici)
 
 set(SRC_LIST_EXE main.cpp)
 
@@ -51,31 +49,32 @@ add_executable(simulate_${PROJECT_NAME} ${SRC_LIST_EXE})
 target_link_libraries(simulate_${PROJECT_NAME} ${PROJECT_NAME})
 
 if($ENV{ENABLE_GCOV_COVERAGE})
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0 --coverage")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0 --coverage")
 endif()
 
-## SWIG
+# SWIG
 option(ENABLE_SWIG "Build swig/python library?" ON)
 if(ENABLE_SWIG)
-    if(NOT(${CMAKE_VERSION} VERSION_LESS 3.8))
-        add_subdirectory(swig)
-    else()
-        message(WARNING "Unable to build SWIG interface, upgrade CMake to >=3.8.")
-    endif()
+  if(NOT (${CMAKE_VERSION} VERSION_LESS 3.8))
+    add_subdirectory(swig)
+  else()
+    message(WARNING "Unable to build SWIG interface, upgrade CMake to >=3.8.")
+  endif()
 endif()
-
 
 # <Export cmake configuration>
 include(GNUInstallDirs)
-install(TARGETS ${PROJECT_NAME} EXPORT ${PROJECT_NAME}Targets
-    ARCHIVE  DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    LIBRARY  DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    RUNTIME  DESTINATION ${CMAKE_INSTALL_BINDIR}
-    INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-    PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-)
-export(EXPORT ${PROJECT_NAME}Targets FILE ${PROJECT_NAME}Config.cmake
-    NAMESPACE Upstream::
-    )
+install(
+  TARGETS ${PROJECT_NAME}
+  EXPORT ${PROJECT_NAME}Targets
+  ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+  INCLUDES
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+  PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+export(
+  EXPORT ${PROJECT_NAME}Targets
+  FILE ${PROJECT_NAME}Config.cmake
+  NAMESPACE Upstream::)
 # </Export cmake configuration>
-
