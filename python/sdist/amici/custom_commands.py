8de4d69630c419df51c07a376070a0e5ab432039
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-02-23 21:59:02 +0100

    cleanuo

--- python/sdist/amici/custom_commands.py
+++ python/sdist/amici/custom_commands.py
@@ -6,8 +6,7 @@ import sys
 from pathlib import Path
 from typing import Dict, List, Tuple
 
-import cmake_build_extension
-from setuptools.command.build_ext import build_ext
+from cmake_build_extension import BuildExtension, CMakeExtension
 from setuptools.command.build_py import build_py
 from setuptools.command.develop import develop
 from setuptools.command.install import install
@@ -15,9 +14,6 @@ from setuptools.command.install_lib import install_lib
 from setuptools.command.sdist import sdist
 
 from amici.swig import fix_typehints
-
-# typehints
-Library = Tuple[str, Dict[str, List[str]]]
 
 
 class AmiciInstall(install):
@@ -30,49 +26,14 @@ class AmiciInstall(install):
     ]
 
     def initialize_options(self):
-        install.initialize_options(self)
+        super().initialize_options()
         self.no_clibs = False
 
     def finalize_options(self):
         if self.no_clibs:
             self.no_clibs = True
-        install.finalize_options(self)
+        super().finalize_options()
 
-
-def compile_parallel(self, sources, output_dir=None, macros=None,
-                     include_dirs=None, debug=0, extra_preargs=None,
-                     extra_postargs=None, depends=None):
-    """Parallelized version of distutils.ccompiler.compile"""
-
-    macros, objects, extra_postargs, pp_opts, build = \
-        self._setup_compile(output_dir, macros, include_dirs, sources,
-                            depends, extra_postargs)
-    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
-
-    # parallel compilation
-    num_threads = 1
-    if 'AMICI_PARALLEL_COMPILE' in os.environ:
-        max_threads = int(os.environ['AMICI_PARALLEL_COMPILE'])
-        num_threads = min(len(objects), max_threads)
-        num_threads = max(1, num_threads)
-
-    def _single_compile(obj):
-        try:
-            src, ext = build[obj]
-        except KeyError:
-            return
-        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
-
-    if num_threads > 1:
-        import multiprocessing.pool
-        # convert to list, imap is evaluated on-demand
-        list(multiprocessing.pool.ThreadPool(num_threads).imap(
-            _single_compile, objects))
-    else:
-        for obj in objects:
-            _single_compile(obj)
-
-    return objects
 
 class AmiciDevelop(develop):
     """Custom develop to build clibs"""
@@ -83,13 +44,13 @@ class AmiciDevelop(develop):
     ]
 
     def initialize_options(self):
-        develop.initialize_options(self)
+        super().initialize_options()
         self.no_clibs = False
 
     def finalize_options(self):
         if self.no_clibs:
             self.no_clibs = True
-        develop.finalize_options(self)
+        super().finalize_options()
 
 
 class AmiciInstallLib(install_lib):
@@ -103,62 +64,17 @@ class AmiciInstallLib(install_lib):
         """
         print("running AmiciInstallLib")
 
-        if 'ENABLE_AMICI_DEBUGGING' in os.environ \
-                and os.environ['ENABLE_AMICI_DEBUGGING'] == 'TRUE' \
+        if os.environ.get('ENABLE_AMICI_DEBUGGING') == 'TRUE' \
                 and sys.platform == 'darwin':
             search_dir = os.path.join(os.getcwd(), self.build_dir, 'amici')
             for file in os.listdir(search_dir):
                 if file.endswith('.so'):
                     subprocess.run(['dsymutil', os.path.join(search_dir, file),
                                     '-o',
-                                    os.path.join(search_dir, file + '.dSYM')])
+                                    os.path.join(search_dir, f'{file}.dSYM')])
 
         # Continue with the actual installation
-        install_lib.run(self)
-
-
-# TODO REMOVEME
-class AmiciBuildExt(build_ext):
-    """Custom build_ext to allow keeping otherwise temporary static libs"""
-
-    def run(self):
-        """Copy the generated clibs to the extensions folder to be included in
-        the wheel
-        """
-
-        print("running AmiciBuildExt")
-
-        no_clibs = 'develop' in self.distribution.command_obj \
-                   and self.get_finalized_command('develop').no_clibs
-        no_clibs |= 'install' in self.distribution.command_obj \
-                    and self.get_finalized_command('install').no_clibs
-
-        if no_clibs:
-            # Nothing to build
-            return
-
-        if not self.dry_run and self.distribution.has_c_libraries():
-            # Module build directory where we want to copy the generated
-            # libs to
-            if self.inplace == 0:
-                build_dir = self.build_lib
-            else:
-                build_dir = os.getcwd()
-            target_dir = os.path.join(build_dir, 'amici', 'libs')
-            self.mkpath(target_dir)
-
-            swig_outdir = os.path.join(os.path.abspath(build_dir), "amici")
-            swig_py_module_path = os.path.join(swig_outdir, 'amici.py')
-            print("updating typehints")
-            fix_typehints(swig_py_module_path, swig_py_module_path)
-
-        # Always force recompilation. The way setuptools/distutils check for
-        # whether sources require recompilation is not reliable and may lead
-        # to crashes or wrong results. We rather compile once too often...
-        self.force = True
-
-        # Continue with the actual extension building
-        build_ext.run(self)
+        super().run()
 
 
 class AmiciSDist(sdist):
@@ -166,12 +82,11 @@ class AmiciSDist(sdist):
 
     def run(self):
         """Setuptools entry-point"""
-
-        print("running AmiciSDist")
+        print(f"running {self.__class__.__name__}")
 
         save_git_version()
 
-        sdist.run(self)
+        super().run()
 
 
 def save_git_version():
@@ -179,9 +94,6 @@ def save_git_version():
 
     This requires git. We assume that whoever creates the sdist will work
     inside a valid git repository.
-
-    Returns:
-
     """
     with open(os.path.join("amici", "git_version.txt"), "w") as f:
         try:
@@ -192,53 +104,47 @@ def save_git_version():
             print(e)
 
 
-def set_compiler_specific_library_options(
-        libraries: List[Library],
-        compiler_type: str) -> None:
-    """Set compiler-specific library options.
-
-    C/C++-libraries for setuptools/distutils are provided as dict containing
-    entries for 'sources', 'macros', 'cflags', etc.
-    As we don't know the compiler type at the stage of calling
-    ``setuptools.setup`` and as there is no other apparent way to set
-    compiler-specific options, we elsewhere extend the dict with additional
-    fields ${original_field}_${compiler_class}, and add the additional
-    compiler-specific options here, at a stage when the compiler has been
-    determined by distutils.
-
-    Arguments:
-        libraries:
-            List of libraries as passed as ``libraries`` argument to
-            ``setuptools.setup`` and ``setuptools.build_ext.build_extension``.
-            This is modified in place.
-        compiler_type:
-            Compiler type, as defined in
-            ``distutils.ccompiler.compiler.compiler_class``, (e.g. 'unix',
-            'msvc', 'mingw32').
-    """
-
-    for lib in libraries:
-        for field in ['cflags', 'sources', 'macros']:
-            try:
-                lib[1][field] += lib[1][f'{field}_{compiler_type}']
-                print(f"Changed {field} for {lib[0]} with {compiler_type} "
-                         f"to {lib[1][field]}")
-            except KeyError:
-                # No compiler-specific options set
-                pass
-
-
 class AmiciBuildPy(build_py):
     def run(self):
+        print(f"running {self.__class__.__name__}")
         # We need build_ext before build_py, that all artifacts will be
         # copied from the build dir
-        self.run_command("build_clib")
         self.run_command("build_ext")
         return super().run()
 
 
-class AmiciBuildCMakeExtension(cmake_build_extension.BuildExtension):
-    def build_extension(self, ext: cmake_build_extension.CMakeExtension) -> None:
+class AmiciBuildCMakeExtension(BuildExtension):
+    def run(self):
+        """Copy the generated clibs to the extensions folder to be included in
+        the wheel
+        """
+        print(f"running {self.__class__.__name__}")
+
+        # custom flag to build without extensions
+        no_clibs = 'develop' in self.distribution.command_obj \
+                       and self.get_finalized_command('develop').no_clibs
+        no_clibs |= 'install' in self.distribution.command_obj \
+                        and self.get_finalized_command('install').no_clibs
+
+        if no_clibs:
+            # Nothing to build
+            return
+
+        # Continue with the actual extension building
+        result = super().run()
+
+        if not self.dry_run:
+            # Fix SWIG-generated typehints
+            build_dir = self.build_lib if self.inplace == 0 else os.getcwd()
+            print("updating typehints")
+            swig_py_module_path = Path(build_dir, "amici", "amici.py")
+            fix_typehints(swig_py_module_path, swig_py_module_path)
+
+        return result
+
+    def build_extension(
+            self, ext: CMakeExtension
+    ) -> None:
         # put some structure into CMake output
         print("-" * 30, ext.name, "-" * 30, file=sys.stderr)
 
@@ -251,6 +157,6 @@ class AmiciBuildCMakeExtension(cmake_build_extension.BuildExtension):
             x.replace("${build_dir}", build_dir) for x in
             ext.cmake_configure_options]
 
-        cmake_build_extension.BuildExtension.build_extension(self, ext)
+        super().build_extension(ext)
 
         print("-" * 30, ext.name, "-" * 30, file=sys.stderr)
