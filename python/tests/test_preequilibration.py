f79b9376d20e6d98a64429a9df022b66c883d84b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-05-09 11:06:12 +0200

    rtol

--- python/tests/test_preequilibration.py
+++ python/tests/test_preequilibration.py
@@ -19,6 +19,7 @@ def preeq_fixture(pysb_example_presimulation_module):
     solver = model.getSolver()
     solver.setSensitivityOrder(amici.SensitivityOrder.first)
     solver.setSensitivityMethod(amici.SensitivityMethod.forward)
+    solver.setRelativeTolerance(1e-10)
 
     edata = get_data(model)
     edata.t_presim = 2
