9eedc54d876be7568bff0323da4bd73bd0084ce0
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-05-11 09:39:55 +0200

    Fix deprecation

--- python/tests/splines_utils.py
+++ python/tests/splines_utils.py
@@ -257,7 +257,7 @@ def create_petab_problem(
     folder = os.path.abspath(folder)
     os.makedirs(folder, exist_ok=True)
     problem.to_files(
-        sbml_file=os.path.join(folder, f'{model_name}_model.xml'),
+        model_file=os.path.join(folder, f'{model_name}_model.xml'),
         condition_file=os.path.join(folder, f'{model_name}_conditions.tsv'),
         measurement_file=os.path.join(folder,
                                       f'{model_name}_measurements.tsv'),
