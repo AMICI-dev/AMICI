c6455a1badb855485a1582367dc78443b39fbca0
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-05-22 20:09:22 +0200

    test

--- tests/testSBMLSuite.py
+++ tests/testSBMLSuite.py
@@ -80,11 +80,14 @@ def test_sbml_testsuite_case(
             current_test_path,
             test_id,
             model_dir,
-            generate_sensitivity_code=test_id in sensitivity_check_cases,
+            generate_sensitivity_code=True,
         )
         settings = read_settings_file(current_test_path, test_id)
 
         atol, rtol = apply_settings(settings, solver, model, test_id)
+
+        solver.setSensitivityOrder(amici.SensitivityOrder.first)
+        solver.setSensitivityMethod(amici.SensitivityMethod.forward)
 
         # simulate model
         rdata = amici.runAmiciSimulation(model, solver)
@@ -104,8 +107,6 @@ def test_sbml_testsuite_case(
 
         # check sensitivities for selected models
         if epsilon := sensitivity_check_cases.get(test_id):
-            solver.setSensitivityOrder(amici.SensitivityOrder.first)
-            solver.setSensitivityMethod(amici.SensitivityMethod.forward)
             check_derivatives(model, solver, epsilon=epsilon)
 
     except amici.sbml_import.SBMLException as err:
