00109858f58b91d1f3db3ab49a43c6fb0cf7c061
Daniel Weindl <dweindl@users.noreply.github.com>
2023-06-19 16:06:21 +0200

    Fix sp.Matrix errors with numpy==1.25 (#2124)
    and DeprecationWarnings with older numpy (`venv/lib/python3.11/site-packages/sympy/matrices/matrices.py:995: DeprecationWarning: elementwise comparison failed; this will raise an error in the future.
        if dat in ([], [[]]):`)

--- python/tests/splines_utils.py
+++ python/tests/splines_utils.py
@@ -47,7 +47,6 @@ def integrate_spline(
     params: Union[Dict, None],
     tt: Sequence[float],
     initial_value: float = 0,
-    **kwargs,
 ):
     """
     Integrate the `AbstractSpline` `spline` at timepoints `tt`
@@ -56,7 +55,7 @@ def integrate_spline(
     ispline = [initial_value + spline.integrate(0, t) for t in tt]
     if params is not None:
         ispline = [x.subs(params) for x in ispline]
-    return np.asarray(ispline, **kwargs)
+    return ispline
 
 
 def create_condition_table() -> pd.DataFrame:
@@ -213,10 +212,13 @@ def create_petab_problem(
     dt /= measure_upsample
     n_obs = math.ceil(T / dt) + 1
     tt_obs = np.linspace(0, float(T), n_obs)
-    zz_true = [
-        integrate_spline(spline, params_true, tt_obs, iv, dtype=float)
-        for (spline, iv) in zip(splines, initial_values)
-    ]
+    zz_true = np.array(
+        [
+            integrate_spline(spline, params_true, tt_obs, iv)
+            for (spline, iv) in zip(splines, initial_values)
+        ],
+        dtype=float,
+    )
     zz_obs = [zz + sigma * np.random.randn(len(zz)) for zz in zz_true]
 
     # Create PEtab tables
