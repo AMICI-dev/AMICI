70083c80f161b7ed85337f41dd0ba6edcee994c1
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-06-28 18:28:00 +0200

    nbconvert

--- scripts/runNotebook.sh
+++ scripts/runNotebook.sh
@@ -10,7 +10,7 @@ AMICI_PATH=$(cd $SCRIPT_PATH/.. && pwd)
 runNotebook () {
     set +e
     tempfile=$(mktemp)
-    jupyter nbconvert --debug --stdout --execute --ExecutePreprocessor.timeout=600 --to markdown $@ &> $tempfile
+    nbconvert --debug --stdout --execute --ExecutePreprocessor.timeout=600 --to markdown $@ &> $tempfile
     ret=$?
     if [[ $ret != 0 ]]; then
       cat $tempfile
@@ -26,7 +26,7 @@ if [ $# -eq 0 ]; then
 fi
 
 source ${AMICI_PATH}/build/venv/bin/activate
-pip3 show ipython || (pip3 install --upgrade jupyter jupyter_contrib_nbextensions nbconvert && python3 -m ipykernel install --user --name amici --display-name "Python (amici)")
+pip3 show nbconvert || (pip3 install --upgrade nbconvert && python3 -m ipykernel install --user --name amici --display-name "Python (amici)")
 
 for arg in "$@"; do
     if [ -d $arg ]; then
