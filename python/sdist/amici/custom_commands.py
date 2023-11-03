269910b9a9cfa2d8ebaa7eb0fa43d7d25d508962
Daniel Weindl <dweindl@users.noreply.github.com>
2023-11-03 05:46:29 +0100

    Remove setuptools<64 restriction (#2180)
    We had to require setuptools<64 because it change its temporary build directory from a known path to a randomly named temporary directory. However, setuptools<64 doesn't work with for Python3.12 (#2179). Therefore, this adds an option to change the setuptools temporary build directory via the `AMICI_BUILD_TEMP` environment variable, so we have those temp files in a known path for coverage analysis. Not meant to be used by users.

--- python/sdist/amici/custom_commands.py
+++ python/sdist/amici/custom_commands.py
@@ -126,6 +126,16 @@ class AmiciBuildPy(build_py):
 
 
 class AmiciBuildCMakeExtension(BuildExtension):
+    def finalize_options(self):
+        # Allow overriding the - since setuptools version 64 randomly named -
+        #  setuptools/distutils temporary build directory via environment variable.
+        # This is useful for CI builds where we need the files in this directory
+        #  for code coverage analysis.
+        if os.getenv("AMICI_BUILD_TEMP"):
+            self.build_temp = os.getenv("AMICI_BUILD_TEMP")
+
+        super().finalize_options()
+
     def run(self):
         """Copy the generated clibs to the extensions folder to be included in
         the wheel
