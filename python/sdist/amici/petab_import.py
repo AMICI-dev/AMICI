663efa35e6fb36296ce5b525c0a6a4b569632d57
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2023-11-22 18:23:17 +0100

    ..

--- python/sdist/amici/petab_import.py
+++ python/sdist/amici/petab_import.py
@@ -12,7 +12,6 @@ warnings.warn(
 )
 
 from .petab.import_helpers import (  # noqa # pylint: disable=unused-import
-    get_fixed_parameters,
     get_observation_model,
     petab_noise_distributions_to_amici,
     petab_scale_to_amici_scale,
@@ -24,5 +23,8 @@ from .petab.petab_import import (  # noqa # pylint: disable=unused-import
     import_model,
     import_model_sbml,
     import_petab_problem,
+)
+from .petab.sbml_import import (  # noqa
+    _get_fixed_parameters_sbml as get_fixed_parameters,
 )
 from .petab.sbml_import import species_to_parameters
