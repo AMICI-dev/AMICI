a25cf9627c9a5a947cf1596d811e9f5d54463a4a
Fabian Fröhlich <fabian@schaluck.com>
2024-03-06 12:15:36 +0000

    remove venv creation from installAmiciSource

--- scripts/installAmiciSource.sh
+++ scripts/installAmiciSource.sh
@@ -5,30 +5,12 @@ set -e
 SCRIPT_PATH=$(dirname $BASH_SOURCE)
 AMICI_PATH=$(cd "$SCRIPT_PATH/.." && pwd)
 
-venv_dir="${AMICI_PATH}/venv"
 # Disabled until cmake package is made compatible with updated setup.py
 #make python-wheel
 #pip3 install --user --prefix= `ls -t ${AMICI_PATH}/build/python/amici-*.whl | head -1`
 
 # test install from setup.py
-set +e
-mkdir -p "${venv_dir}"
-python3 -m venv "${venv_dir}" --clear
-# in case this fails (usually due to missing ensurepip, try getting pip
-# manually
-if [[ $? ]]; then
-    set -e
-    python3 -m venv "${venv_dir}" --clear --without-pip
-    source "${venv_dir}/bin/activate"
-    get_pip=${AMICI_PATH}/get-pip.py
-    curl "https://bootstrap.pypa.io/get-pip.py" -o "${get_pip}"
-    python3 "${get_pip}"
-    rm "${get_pip}"
-else
-    set -e
-    source "${venv_dir}/bin/activate"
-fi
-
+source "${AMICI_PATH}/venv/bin/activate"
 python -m pip install --upgrade pip wheel
 python -m pip install --upgrade pip setuptools cmake_build_extension numpy
 python -m pip install git+https://github.com/FFroehlich/pysb@fix_pattern_matching # pin to PR for SPM with compartments
