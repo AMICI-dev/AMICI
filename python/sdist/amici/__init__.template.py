d8d19000051788e38c5e943d4ccc5815ad55be67
Fabian Fröhlich <fabian@schaluck.com>
2024-04-12 08:48:10 +0100

    fix jax imports

--- python/sdist/amici/__init__.template.py
+++ python/sdist/amici/__init__.template.py
@@ -1,13 +1,11 @@
 """AMICI-generated module for model TPL_MODELNAME"""
 
 from pathlib import Path
-
+from typing import TYPE_CHECKING
 import amici
 
-try:
+if TYPE_CHECKING:
     from amici.jax import JAXModel
-except (ModuleNotFoundError, ImportError):
-    JAXModel = object
 
 # Ensure we are binary-compatible, see #556
 if "TPL_AMICI_VERSION" != amici.__version__:
@@ -23,16 +21,11 @@ if "TPL_AMICI_VERSION" != amici.__version__:
 from .TPL_MODELNAME import *  # noqa: F403, F401
 from .TPL_MODELNAME import getModel as get_model  # noqa: F401
 
-try:
+
+def get_jax_model() -> "JAXModel":
     from .jax import JAXModel_TPL_MODELNAME
 
-    def get_jax_model() -> JAXModel:
-        return JAXModel_TPL_MODELNAME()
-except (ModuleNotFoundError, ImportError) as exc:
-    error = str(exc)
-
-    def get_jax_model() -> JAXModel:
-        raise NotImplementedError(error)
+    return JAXModel_TPL_MODELNAME()
 
 
 __version__ = "TPL_PACKAGE_VERSION"
