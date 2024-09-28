76302f4d1df8c1642cf75ce5b398abf2bd1399fc
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2024-09-28 06:11:51 +0200

    klu via config

--- scripts/buildSundials.sh
+++ scripts/buildSundials.sh
@@ -46,9 +46,7 @@ ${cmake} -DCMAKE_INSTALL_PREFIX="${sundials_build_path}" \
   -DEXAMPLES_ENABLE_C=OFF \
   -DEXAMPLES_INSTALL=OFF \
   -DENABLE_KLU=ON \
-  -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}:${suitesparse_root}/install" \
-  -DKLU_LIBRARY_DIR="${suitesparse_root}/install/lib" \
-  -DKLU_INCLUDE_DIR="${suitesparse_root}/install/include/suitesparse" \
+  -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH};${suitesparse_root}/install" \
   ${SuperLUMT} \
   ..
 
