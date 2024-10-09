AMICI-dev/AMICI

commit cd566335f3eb29be132bb085b3c02a1477f8d1e6

Upgrade SUNDIALS to 7.1.1 (#2513)
We've been lagging behind SUNDIALS development. This updates the SUNDIALS library from v5.8.0 to [v7.1.1](https://sundials.readthedocs.io/en/latest/Changelog_link.html#changes-to-sundials-in-release-7-1-1).

Note:
* This includes the post-7.1.1 changes to `cmake/tpl/FindKLU.cmake` from https://github.com/LLNL/sundials/pull/582
* This includes the post-7.1.1 changes to [`sundials/src/sunmatrix/sparse/sunmatrix_sparse.c:SUNMatScaleAddI_Sparse`](https://github.com/LLNL/sundials/blob/736369d543cb80956b1ba87377ffc0c8cf6b342b/src/sunmatrix/sparse/sunmatrix_sparse.c#L625C1-L694C2) from  https://github.com/LLNL/sundials/pull/584, fixing https://github.com/LLNL/sundials/issues/581

The majority of changes inside amici are related to the introduction of [SUNContext](https://sundials.readthedocs.io/en/latest/sundials/SUNContext_link.html#the-suncontext-type), which is required for the construction of all SUNDIALS objects. Other trivial changes are related to renamed types and macros.

Furthermore, the error handling strategy has changed. As a consequence, simulation error messages will look slightly different.

Closes #1565

include/amici/vector.h
@@ -9,7 +9,7 @@
 #include <nvector/nvector_serial.h>
 
 #include <gsl/gsl-lite.hpp>
-
+#include <sundials/sundials_context.hpp>
 namespace amici {
 class AmiVector;
 }
@@ -40,34 +40,42 @@ class AmiVector {
      */
     AmiVector() = default;
 
-    /** Creates an std::vector<realtype> and attaches the
+    /**
+     * @brief Construct empty vector of given size
+     *
+     * Creates an std::vector<realtype> and attaches the
      * data pointer to a newly created N_Vector_Serial.
      * Using N_VMake_Serial ensures that the N_Vector
      * module does not try to deallocate the data vector
      * when calling N_VDestroy_Serial
-     * @brief empty constructor
      * @param length number of elements in vector
+     * @param sunctx SUNDIALS context
      */
-    explicit AmiVector(long int const length)
+    explicit AmiVector(long int const length, SUNContext sunctx)
         : vec_(static_cast<decltype(vec_)::size_type>(length), 0.0)
-        , nvec_(N_VMake_Serial(length, vec_.data())) {}
+        , nvec_(N_VMake_Serial(length, vec_.data(), sunctx)) {}
 
-    /** Moves data from std::vector and constructs an nvec that points to the
+    /**
+     * @brief Constructor from std::vector
+     *
+     * Moves data from std::vector and constructs an nvec that points to the
      * data
-     * @brief constructor from std::vector,
      * @param rvec vector from which the data will be moved
+     * @param sunctx SUNDIALS context
      */
-    explicit AmiVector(std::vector<realtype> rvec)
+    explicit AmiVector(std::vector<realtype> rvec, SUNContext sunctx)
         : vec_(std::move(rvec))
-        , nvec_(N_VMake_Serial(gsl::narrow<long int>(vec_.size()), vec_.data())
-          ) {}
+        , nvec_(N_VMake_Serial(
+              gsl::narrow<long int>(vec_.size()), vec_.data(), sunctx
+          )) {}
 
     /** Copy data from gsl::span and constructs a vector
      * @brief constructor from gsl::span,
      * @param rvec vector from which the data will be copied
+     * @param sunctx SUNDIALS context
      */
-    explicit AmiVector(gsl::span<realtype const> rvec)
-        : AmiVector(std::vector<realtype>(rvec.begin(), rvec.end())) {}
+    explicit AmiVector(gsl::span<realtype const> rvec, SUNContext sunctx)
+        : AmiVector(std::vector<realtype>(rvec.begin(), rvec.end()), sunctx) {}
 
     /**
      * @brief copy constructor
@@ -75,8 +83,13 @@ class AmiVector {
      */
     AmiVector(AmiVector const& vold)
         : vec_(vold.vec_) {
+        if (vold.nvec_ == nullptr) {
+            nvec_ = nullptr;
+            return;
+        }
         nvec_ = N_VMake_Serial(
-            gsl::narrow<long int>(vold.vec_.size()), vec_.data()
+            gsl::narrow<long int>(vold.vec_.size()), vec_.data(),
+            vold.nvec_->sunctx
         );
     }
 
@@ -85,9 +98,9 @@ class AmiVector {
      * @param other vector from which the data will be moved
      */
     AmiVector(AmiVector&& other) noexcept
-        : nvec_(nullptr) {
-        vec_ = std::move(other.vec_);
-        synchroniseNVector();
+        : vec_(std::move(other.vec_))
+        , nvec_(nullptr) {
+        synchroniseNVector(other.get_ctx());
     }
 
     /**
@@ -236,6 +249,27 @@ class AmiVector {
         Archive& ar, AmiVector& s, unsigned int version
     );
 
+    /**
+     * @brief Get SUNContext
+     * @return The current SUNContext or nullptr, if this AmiVector is empty
+     */
+    SUNContext get_ctx() const {
+        return nvec_ == nullptr ? nullptr : nvec_->sunctx;
+    }
+
+    /**
+     * @brief Set SUNContext
+     *
+     * If this AmiVector is non-empty, changes the current SUNContext of the
+     * associated N_Vector. If empty, do nothing.
+     *
+     * @param ctx SUNDIALS context to set
+     */
+    void set_ctx(SUNContext ctx) {
+        if (nvec_)
+            nvec_->sunctx = ctx;
+    }
+
   private:
     /** main data storage */
     std::vector<realtype> vec_;
@@ -245,8 +279,9 @@ class AmiVector {
 
     /**
      * @brief reconstructs nvec such that data pointer points to vec data array
+     * @param sunctx SUNDIALS context
      */
-    void synchroniseNVector();
+    void synchroniseNVector(SUNContext sunctx);
 };
 
 /**
@@ -270,8 +305,11 @@ class AmiVectorArray {
      * @brief empty constructor
      * @param length_inner length of vectors
      * @param length_outer number of vectors
+     * @param sunctx SUNDIALS context
      */
-    AmiVectorArray(long int length_inner, long int length_outer);
+    AmiVectorArray(
+        long int length_inner, long int length_outer, SUNContext sunctx
+    );
 
     /**
      * @brief copy constructor
@@ -425,8 +463,10 @@ namespace gsl {
  * @param nv
  * @return
  */
-inline span<realtype> make_span(N_Vector nv) {
-    return span<realtype>(N_VGetArrayPointer(nv), N_VGetLength_Serial(nv));
+inline span<amici::realtype> make_span(N_Vector nv) {
+    return span<amici::realtype>(
+        N_VGetArrayPointer(nv), N_VGetLength_Serial(nv)
+    );
 }
 
 /**
@@ -434,7 +474,7 @@ inline span<realtype> make_span(N_Vector nv) {
  * @param av
  *
  */
-inline span<realtype const> make_span(amici::AmiVector const& av) {
+inline span<amici::realtype const> make_span(amici::AmiVector const& av) {
     return make_span(av.getVector());
 }
 

src/solver.cpp
@@ -1,13 +1,62 @@
 #include "amici/solver.h"
 
+#include "amici/amici.h"
 #include "amici/exception.h"
 #include "amici/model.h"
 #include "amici/symbolic_functions.h"
 
+#include <sundials/sundials_context.h>
+
 #include <cstdio>
+#include <filesystem>
 #include <memory>
 
 namespace amici {
+
+/* Error handler passed to SUNDIALS. */
+void wrapErrHandlerFn(
+    [[maybe_unused]] int line, char const* func, char const* file,
+    char const* msg, SUNErrCode err_code, void* err_user_data,
+    [[maybe_unused]] SUNContext sunctx
+) {
+    constexpr int BUF_SIZE = 250;
+
+    char msg_buffer[BUF_SIZE];
+    char id_buffer[BUF_SIZE];
+    static_assert(
+        std::is_same<SUNErrCode, int>::value, "Must update format string"
+    );
+    // for debug builds, include full file path and line numbers
+#ifdef NDEBUG
+    snprintf(msg_buffer, BUF_SIZE, "%s:%d: %s (%d)", file, line, msg, err_code);
+#else
+    snprintf(msg_buffer, BUF_SIZE, "%s", msg);
+#endif
+    // we need a matlab-compatible message ID
+    // i.e. colon separated and only  [A-Za-z0-9_]
+    // see https://mathworks.com/help/matlab/ref/mexception.html
+    std::filesystem::path path(file);
+    auto file_stem = path.stem().string();
+
+    // Error code to string. Remove 'AMICI_' prefix.
+    auto err_code_str = simulation_status_to_str(err_code);
+    constexpr std::string_view err_code_prefix = "AMICI_";
+    if (err_code_str.substr(0, err_code_prefix.size()) == err_code_prefix)
+        err_code_str = err_code_str.substr(err_code_prefix.size());
+
+    snprintf(
+        id_buffer, BUF_SIZE, "%s:%s:%s", file_stem.c_str(), func,
+        err_code_str.c_str()
+    );
+
+    if (!err_user_data) {
+        throw std::runtime_error("eh_data unset");
+    }
+
+    auto solver = static_cast<Solver const*>(err_user_data);
+    if (solver->logger)
+        solver->logger->log(LogSeverity::debug, id_buffer, msg_buffer);
+}
 
 Solver::Solver(Solver const& other)
     : ism_(other.ism_)
@@ -49,7 +98,12 @@ Solver::Solver(Solver const& other)
     , max_conv_fails_(other.max_conv_fails_)
     , max_step_size_(other.max_step_size_)
     , maxstepsB_(other.maxstepsB_)
-    , sensi_(other.sensi_) {}
+    , sensi_(other.sensi_) {
+    // update to our own context
+    constraints_.set_ctx(sunctx_);
+}
+
+SUNContext Solver::getSunContext() const { return sunctx_; }
 
 void Solver::apply_max_num_steps() const {
     // set remaining steps, setMaxNumSteps only applies to a single call of
@@ -719,7 +773,7 @@ void Solver::setConstraints(std::vector<realtype> const& constraints) {
         return;
     }
 
-    constraints_ = AmiVector(constraints);
+    constraints_ = AmiVector(constraints, sunctx_);
 }
 
 void Solver::setMaxStepSize(realtype max_step_size) {
@@ -1150,6 +1204,17 @@ void Solver::initializeNonLinearSolverSens(Model const* model) const {
     setNonLinearSolverSens();
 }
 
+void Solver::setErrHandlerFn() const {
+    auto sunerr = SUNContext_PushErrHandler(
+        sunctx_, wrapErrHandlerFn,
+        reinterpret_cast<void*>(const_cast<Solver*>(this))
+    );
+    if (sunerr)
+        throw AmiException(
+            "Error setting error handler function: %s", SUNGetErrMsg(sunerr)
+        );
+}
+
 int Solver::nplist() const { return sx_.getLength(); }
 
 int Solver::nx() const { return x_.getLength(); }
@@ -1210,17 +1275,17 @@ void Solver::resetMutableMemory(int const nx, int const nplist, int const nquad)
     solver_was_called_F_ = false;
     solver_was_called_B_ = false;
 
-    x_ = AmiVector(nx);
-    dx_ = AmiVector(nx);
-    sx_ = AmiVectorArray(nx, nplist);
-    sdx_ = AmiVectorArray(nx, nplist);
+    x_ = AmiVector(nx, sunctx_);
+    dx_ = AmiVector(nx, sunctx_);
+    sx_ = AmiVectorArray(nx, nplist, sunctx_);
+    sdx_ = AmiVectorArray(nx, nplist, sunctx_);
 
-    dky_ = AmiVector(nx);
+    dky_ = AmiVector(nx, sunctx_);
 
-    xB_ = AmiVector(nx);
-    dxB_ = AmiVector(nx);
-    xQB_ = AmiVector(nquad);
-    xQ_ = AmiVector(nx);
+    xB_ = AmiVector(nx, sunctx_);
+    dxB_ = AmiVector(nx, sunctx_);
+    xQB_ = AmiVector(nquad, sunctx_);
+    xQ_ = AmiVector(nx, sunctx_);
 
     solver_memory_B_.clear();
     initializedB_.clear();
@@ -1343,58 +1408,4 @@ AmiVector const& Solver::getQuadrature(realtype t) const {
 }
 
 realtype Solver::gett() const { return t_; }
-
-void wrapErrHandlerFn(
-    int error_code, char const* module, char const* function, char* msg,
-    void* eh_data
-) {
-    constexpr int BUF_SIZE = 250;
-    char buffer[BUF_SIZE];
-    char buffid[BUF_SIZE];
-    snprintf(
-        buffer, BUF_SIZE, "AMICI ERROR: in module %s in function %s : %s ",
-        module, function, msg
-    );
-    switch (error_code) {
-    case 99:
-        snprintf(buffid, BUF_SIZE, "%s:%s:WARNING", module, function);
-        break;
-
-    case AMICI_TOO_MUCH_WORK:
-        snprintf(buffid, BUF_SIZE, "%s:%s:TOO_MUCH_WORK", module, function);
-        break;
-
-    case AMICI_TOO_MUCH_ACC:
-        snprintf(buffid, BUF_SIZE, "%s:%s:TOO_MUCH_ACC", module, function);
-        break;
-
-    case AMICI_ERR_FAILURE:
-        snprintf(buffid, BUF_SIZE, "%s:%s:ERR_FAILURE", module, function);
-        break;
-
-    case AMICI_CONV_FAILURE:
-        snprintf(buffid, BUF_SIZE, "%s:%s:CONV_FAILURE", module, function);
-        break;
-
-    case AMICI_RHSFUNC_FAIL:
-        snprintf(buffid, BUF_SIZE, "%s:%s:RHSFUNC_FAIL", module, function);
-        break;
-
-    case AMICI_FIRST_RHSFUNC_ERR:
-        snprintf(buffid, BUF_SIZE, "%s:%s:FIRST_RHSFUNC_ERR", module, function);
-        break;
-
-    default:
-        snprintf(buffid, BUF_SIZE, "%s:%s:OTHER", module, function);
-        break;
-    }
-
-    if (!eh_data) {
-        throw std::runtime_error("eh_data unset");
-    }
-    auto solver = static_cast<Solver const*>(eh_data);
-    if (solver->logger)
-        solver->logger->log(LogSeverity::debug, buffid, buffer);
-}
-
 } // namespace amici

src/vector.cpp
@@ -7,7 +7,7 @@ namespace amici {
 
 AmiVector& AmiVector::operator=(AmiVector const& other) {
     vec_ = other.vec_;
-    synchroniseNVector();
+    synchroniseNVector(other.get_ctx());
     return *this;
 }
 
@@ -55,10 +55,14 @@ void AmiVector::copy(AmiVector const& other) {
     std::copy(other.vec_.begin(), other.vec_.end(), vec_.begin());
 }
 
-void AmiVector::synchroniseNVector() {
+void AmiVector::synchroniseNVector(SUNContext sunctx) {
     if (nvec_)
         N_VDestroy_Serial(nvec_);
-    nvec_ = N_VMake_Serial(gsl::narrow<long int>(vec_.size()), vec_.data());
+    nvec_ = vec_.empty()
+                ? nullptr
+                : N_VMake_Serial(
+                      gsl::narrow<long int>(vec_.size()), vec_.data(), sunctx
+                  );
 }
 
 AmiVector::~AmiVector() {
@@ -66,8 +70,10 @@ AmiVector::~AmiVector() {
         N_VDestroy_Serial(nvec_);
 }
 
-AmiVectorArray::AmiVectorArray(long int length_inner, long int length_outer)
-    : vec_array_(length_outer, AmiVector(length_inner)) {
+AmiVectorArray::AmiVectorArray(
+    long int length_inner, long int length_outer, SUNContext sunctx
+)
+    : vec_array_(length_outer, AmiVector(length_inner, sunctx)) {
     nvec_array_.resize(length_outer);
     for (int idx = 0; idx < length_outer; idx++) {
         nvec_array_.at(idx) = vec_array_.at(idx).getNVector();

commit e59e4155f8ecc11718721ab69cd889e1e71ae573

Fix AmiVector::nvec_ for size 0 (#2535)
* We still need to create an NVector, even if it's empty.
* Don't pass nullptr as sunctx

Issue introduced in #2513. 

Closes #2534.

include/amici/vector.h
@@ -88,8 +88,7 @@ class AmiVector {
             return;
         }
         nvec_ = N_VMake_Serial(
-            gsl::narrow<long int>(vold.vec_.size()), vec_.data(),
-            vold.nvec_->sunctx
+            gsl::narrow<long int>(vec_.size()), vec_.data(), vold.nvec_->sunctx
         );
     }
 

src/solver.cpp
@@ -769,7 +769,7 @@ void Solver::setConstraints(std::vector<realtype> const& constraints) {
 
     if (!any_constraint) {
         // all-0 must be converted to empty, otherwise sundials will fail
-        constraints_ = AmiVector();
+        constraints_ = AmiVector(0, sunctx_);
         return;
     }
 

src/vector.cpp
@@ -58,11 +58,11 @@ void AmiVector::copy(AmiVector const& other) {
 void AmiVector::synchroniseNVector(SUNContext sunctx) {
     if (nvec_)
         N_VDestroy_Serial(nvec_);
-    nvec_ = vec_.empty()
-                ? nullptr
-                : N_VMake_Serial(
-                      gsl::narrow<long int>(vec_.size()), vec_.data(), sunctx
-                  );
+    if (sunctx) {
+        nvec_ = N_VMake_Serial(
+            gsl::narrow<long int>(vec_.size()), vec_.data(), sunctx
+        );
+    }
 }
 
 AmiVector::~AmiVector() {