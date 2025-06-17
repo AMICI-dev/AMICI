be1eb88f10020734688169181547099a0a5e709f
Daniel Weindl <daniel.weindl@uni-bonn.de>
2025-06-17 11:35:17 +0200

    ..

--- src/forwardproblem.cpp
+++ src/forwardproblem.cpp
@@ -170,7 +170,8 @@ void ForwardProblem::handlePreequilibration() {
     ConditionContext cc2(model, edata, FixedParameterContext::preequilibration);
 
     preeq_problem_.emplace(*solver, *model);
-    preeq_problem_->workSteadyStateProblem(*solver, *model, -1);
+    auto t0 = std::isnan(model->t0_preeq()) ? model->t0() : model->t0_preeq();
+    preeq_problem_->workSteadyStateProblem(*solver, *model, -1, t0);
 
     ws_.x = preeq_problem_->getState();
     ws_.sx = preeq_problem_->getStateSensitivity();
@@ -263,9 +264,9 @@ void ForwardProblem::handleMainSimulation() {
 void ForwardProblem::handlePostequilibration() {
     if (getCurrentTimeIteration() < model->nt()) {
         posteq_problem_.emplace(*solver, *model);
-        posteq_problem_->workSteadyStateProblem(
-            *solver, *model, getCurrentTimeIteration()
-        );
+        auto it = getCurrentTimeIteration();
+        auto t0 = it < 0 ? model->t0() : model->getTimepoint(it);
+        posteq_problem_->workSteadyStateProblem(*solver, *model, it, t0);
     }
 }
 
