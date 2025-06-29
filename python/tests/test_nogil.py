7d9089de18ba62093e473ec7cf63efc844fbaef8
Fabian Fröhlich <fabian@schaluck.com>
2025-06-29 14:47:30 +0100

    update test

---
+++ python/tests/test_nogil.py
@@ -0,0 +1,16 @@
+import os
+
+import pytest
+
+pytestmark = pytest.mark.skipif(
+    not os.environ.get("AMICI_NOGIL"),
+    reason="only run in nogil workflow",
+)
+
+
+def test_import_warns_without_python_gil(monkeypatch):
+    with pytest.raises(
+        RuntimeWarning,
+        match="The global interpreter lock (GIL) has been enabled to load module 'amici._amici'",
+    ):
+        pass
