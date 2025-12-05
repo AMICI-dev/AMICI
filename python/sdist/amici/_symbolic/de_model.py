1742052a72a4c91c79778969a1f12d5d67b707f9
Branwen Snelling <branwen.snelling@crick.ac.uk>
2025-12-05 11:44:38 +0000

    try fix other test cases

--- python/sdist/amici/_symbolic/de_model.py
+++ python/sdist/amici/_symbolic/de_model.py
@@ -1593,7 +1593,7 @@ class DEModel:
                 else:
                     event_eqs.append(state_update)
 
-            self._eqs[name] = sp.Matrix(event_eqs)
+            self._eqs[name] = event_eqs
 
         elif name == "x_old":
             self._eqs[name] = sp.Matrix(
@@ -2640,13 +2640,13 @@ class DEModel:
         """
         return [root for e in self._events for root in e.get_trigger_times()]
 
-    def get_implicit_roots(self) -> list[sp.Expr]:
+    def get_implicit_roots(self) -> set[sp.Expr]:
         """
         Returns implicit equations for all discontinuities (events)
         that have to be located via rootfinding
 
         :return:
-            list of symbolic roots
+            set of symbolic roots
         """
         return [
             e.get_val()
